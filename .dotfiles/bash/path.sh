8d2ac85a4219b1a8f248893b094f61a19565547b
Ben Chatelain <benchatelain@gmail.com>
2013-08-04 05:29:10 -0600

    Check for /opt/boxen and use fall back paths when not present

--- .dotfiles/bash/path.sh
+++ .dotfiles/bash/path.sh
@@ -35,9 +35,11 @@ export NODE_PATH=/usr/local/lib/node_modules
 # Manually setting the original OS X path so that reloadprofile won't keep growing the PATH
 # OSX_PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/X11R6/bin
 
-BOXEN_PATH=/opt/boxen/rbenv/shims:/opt/boxen/rbenv/bin:/opt/boxen/rbenv/plugins/ruby-build/bin:/opt/boxen/bin:/opt/boxen/homebrew/bin:/opt/boxen/homebrew/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin
+if [ -d /opt/boxen ] ; then
+    BOXEN_PATH=/opt/boxen/rbenv/shims:/opt/boxen/rbenv/bin:/opt/boxen/rbenv/plugins/ruby-build/bin:/opt/boxen/bin:/opt/boxen/homebrew/bin:/opt/boxen/homebrew/sbin
+fi
 
 # Prepend user bin dir to PATH
-export PATH=~/bin:/usr/local/bin:${ANDROID_PATH}:${FORTIFY_PATH}/bin:${BOXEN_PATH}
+export PATH=~/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:${ANDROID_PATH}:${FORTIFY_PATH}/bin:${BOXEN_PATH}
 
 # echo $PATH
--- .dotfiles/ruby/rbenv.sh
+++ .dotfiles/ruby/rbenv.sh
@@ -3,7 +3,12 @@
 # RBENV
 #
 #-------------------------------------------------------------------------------
-export RBENV_ROOT=/opt/boxen/rbenv
+if [ -d /opt/boxen ] ; then
+    export RBENV_ROOT=/opt/boxen/rbenv
+else
+    export RBENV_ROOT=/usr/local/var/rbenv
+fi
+
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
 export PATH=${PATH}:${RBENV_ROOT}/versions/$(cat ${RBENV_ROOT}/version)/bin
