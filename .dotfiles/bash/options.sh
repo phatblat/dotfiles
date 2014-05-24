cf9b7c0480a3d3b22c63466837bac82a0463a942
Ben Chatelain <benchatelain@gmail.com>
2014-05-24 09:51:44 -0600

    Wrap bash options with check for bash shell

--- .dotfiles/bash/options.sh
+++ .dotfiles/bash/options.sh
@@ -1,12 +1,19 @@
-# VI-style prompt editing
-set -o vi
+# If the shell is bash, grep will succeed with 0 (false)
+echo $SHELL | grep -qE "bash"
+if [[ ! $? ]] ; then
+	# bash-only options
 
-export HISTCONTROL=ignoredups:erasedups
-export HISTSIZE=10000
-shopt -s histappend
-## reedit a substitution line if it failed
-shopt -s histreedit
-## edit a recalled history line before executing
-shopt -s histverify
+	# VI-style prompt editing
+	set -o vi
+
+	export HISTCONTROL=ignoredups:erasedups
+	export HISTSIZE=10000
+	shopt -s histappend
+	## reedit a substitution line if it failed
+	shopt -s histreedit
+	## edit a recalled history line before executing
+	shopt -s histverify
+
+fi # end bash
 
 export EDITOR='subl -w'
