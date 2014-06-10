53e2a8b291af770f2bb8790b0d9b4ed11663e509
Ben Chatelain <benchatelain@gmail.com>
2014-06-10 00:50:52 -0600

    Fix merge error in options.sh

--- .dotfiles/bash/options.sh
+++ .dotfiles/bash/options.sh
@@ -1,17 +1,7 @@
-# VI-style prompt editing
-set -o vi
-
-export HISTCONTROL=ignoredups:erasedups
-export HISTSIZE=10000
-shopt -s histappend
-## reedit a substitution line if it failed
-shopt -s histreedit
-## edit a recalled history line before executing
-shopt -s histverify
-=======
 # If the shell is bash, grep will succeed with 0 (false)
 echo $SHELL | grep -qE "bash"
 if [ ! $? ]; then
+
 	# bash-only options
 
 	# VI-style prompt editing
