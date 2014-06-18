238d02d3c657839dc2aa7f5afe319502463bc3f6
Ben Chatelain <benchatelain@gmail.com>
2014-06-18 07:46:50 -0600

    Add godark/golight aliases for Yosemite themes

--- .dotfiles/osx/set-defaults.zsh
+++ .dotfiles/osx/set-defaults.zsh
@@ -41,3 +41,7 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Yosemite dark/light themes
+alias godark='sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Dark'
+alias golight='sudo defaults write /Library/Preferences/.GlobalPreferences AppleInterfaceTheme Light'
