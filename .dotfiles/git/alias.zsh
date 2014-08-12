0c2e0daaf5d5d919508ae1fc6a78993d6b89b864
Ben Chatelain <benchatelain@gmail.com>
2014-08-11 21:54:51 -0600

    Add -f force option to rewrite* aliases

--- .dotfiles/git/alias.zsh
+++ .dotfiles/git/alias.zsh
@@ -118,5 +118,5 @@ alias stash='git stash'
 alias snapshot='git stash save "snapshot: $(date)" && git stash apply "stash@{0}"'
 
 # Experimental
-alias rewriteauthor="git filter-branch --env-filter 'if [ \$GIT_AUTHOR_EMAIL = ben.d.chatelain@kp.org ]; then GIT_AUTHOR_EMAIL=benchatelain@gmail.com; fi; export GIT_AUTHOR_EMAIL'"
-alias rewritecommitter="git filter-branch --env-filter 'if [ \$GIT_COMMITTER_EMAIL = ben.d.chatelain@kp.org ]; then GIT_COMMITTER_EMAIL=benchatelain@gmail.com; fi; export GIT_COMMITTER_EMAIL'"
+alias rewriteauthor="git filter-branch -f --env-filter 'if [ \$GIT_AUTHOR_EMAIL = ben.d.chatelain@kp.org ]; then GIT_AUTHOR_EMAIL=benchatelain@gmail.com; fi; export GIT_AUTHOR_EMAIL'"
+alias rewritecommitter="git filter-branch -f --env-filter 'if [ \$GIT_COMMITTER_EMAIL = ben.d.chatelain@kp.org ]; then GIT_COMMITTER_EMAIL=benchatelain@gmail.com; fi; export GIT_COMMITTER_EMAIL'"
