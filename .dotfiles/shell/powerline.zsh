354ddd43bdd68ec1f4c81bd81fec293fbeb847bb
Ben Chatelain <benchatelain@gmail.com>
2014-08-23 10:02:43 -0600

    Set DEFAULT_USER to prevent PS1 from showing user@host

--- .dotfiles/shell/powerline.zsh
+++ .dotfiles/shell/powerline.zsh
@@ -7,6 +7,8 @@
 export POWERLINE_HOME=/usr/local/lib/python2.7/site-packages/powerline
 export POWERLINE_CONFIG_PATHS=( ~/.powerline )
 
+export DEFAULT_USER="phatblat"
+
 # Powerline
 powerline-daemon -q
 . ${POWERLINE_HOME}/bindings/zsh/powerline.zsh
