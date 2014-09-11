49e56fd95e48f9866f0f3b38e8bd972fbf2ed51a
Ben Chatelain <benchatelain@gmail.com>
2014-09-11 10:15:02 -0600

    Copy uncrustify 'uc' function over to zsh

---
+++ .dotfiles/ios/uncrustify.zsh
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+#
+# Objective-C Source Code Formatting
+#
+#-------------------------------------------------------------------------------
+function uc {
+    find . -type f \( -name "*.h" -or -name "*.m" \) -exec \
+    uncrustify -lOC -c ~/.uncrustify/uncrustify_obj_c.cfg --no-backup {} \;
+}
