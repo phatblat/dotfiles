701f87230064f526ac42e171a2754ae244a910cc
Ben Chatelain <benchatelain@gmail.com>
2014-10-09 22:39:53 -0600

    Add "build" and "market" optional args to version function

--- .dotfiles/ios/alias.zsh
+++ .dotfiles/ios/alias.zsh
@@ -10,28 +10,20 @@ alias ow='open *.xcworkspace'
 
 # agvtool
 function version() {
-	# agvtool_path="/usr/bin/agvtool"
-	agvtool_path=$(which agvtool)
+	agvtool_path=$(which agvtool) # "/usr/bin/agvtool"
 	build_version=$(agvtool what-version -terse)
 	market_version=$(agvtool what-marketing-version -terse1)
-	echo "$market_version ($build_version)"
+
+	case "$1" in
+	    build)
+			echo "$build_version"
+	        ;;
+	    market)
+			echo "$market_version"
+	        ;;
+	    *)
+			# No args, output pretty format
+			echo "$market_version ($build_version)"
+	        ;;
+	esac
 }
-
-#-------------------------------------------------------------------------------
-#
-# Examples
-#
-#-------------------------------------------------------------------------------
-
-# function sshnewkey() {
-# 	if (($+1)); then
-# 		ssh-keygen -t rsa -C "$1"
-# 	else
-# 		echo "Usage: sshnewkey user@host"
-# 	fi
-# }
-
-# function fixperms() {
-# 	find "$1" -type f -print -exec chmod 644 {} \;
-# 	find "$1" -type d -print -exec chmod 755 {} \;
-# }
