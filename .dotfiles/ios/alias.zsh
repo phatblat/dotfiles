adc5ad916d030019a4b1976b6f3246ebea70bb05
Ben Chatelain <benchatelain@gmail.com>
2014-10-15 11:31:26 -0600

    tabs -> spaces

--- .dotfiles/ios/alias.zsh
+++ .dotfiles/ios/alias.zsh
@@ -10,29 +10,29 @@ alias ow='open *.xcworkspace'
 
 # agvtool
 function version() {
-	agvtool_path=$(which agvtool) # "/usr/bin/agvtool"
+    agvtool_path=$(which agvtool) # "/usr/bin/agvtool"
 
-	case "$1" in
-	    "build" | "-b")
-			agvtool what-version -terse
-	        ;;
-	    "market" | "-m")
-			agvtool what-marketing-version -terse1
-	        ;;
-	    "set")
-	        agvtool new-marketing-version $2
-	        if (($+3)); then
-		        agvtool new-version -all $3
-			fi
-	        ;;
-	    "next" | "bump")
-	        agvtool next-version -all
-	        ;;
-	    *)
-			# No args, output pretty format
-			build_version=$(agvtool what-version -terse)
-			market_version=$(agvtool what-marketing-version -terse1)
-			echo "$market_version ($build_version)"
-	        ;;
-	esac
+    case "$1" in
+        "build" | "-b")
+            agvtool what-version -terse
+            ;;
+        "market" | "-m")
+            agvtool what-marketing-version -terse1
+            ;;
+        "set")
+            agvtool new-marketing-version $2
+            if (($+3)); then
+                agvtool new-version -all $3
+            fi
+            ;;
+        "next" | "bump")
+            agvtool next-version -all
+            ;;
+        *)
+            # No args, output pretty format
+            build_version=$(agvtool what-version -terse)
+            market_version=$(agvtool what-marketing-version -terse1)
+            echo "$market_version ($build_version)"
+            ;;
+    esac
 }
