719f165064afb60d6d09a7076cd8814fb6360b51
Ben Chatelain <benchatelain@gmail.com>
2015-01-28 23:39:42 -0700

    Suppress git fetch on login/profile reload, unless it's been more than an hour

--- .dotfiles/shell/login.zsh
+++ .dotfiles/shell/login.zsh
@@ -1,8 +1,19 @@
-git fetch -v
-
 # SSH
 sshkeyfingerprint
 if (( $? != 0 )); then
 	echo "No SSH key found"
 	sshnewkey "${USER}@${HOST}"
 fi
+
+# Update .dotfiles
+# Check the time since the last fetch, continue if more than an hour
+function fetch_dotfiles() {
+	local current=`date +%s`
+	local last_modified=$(stat -f "%c" $HOME/.git/FETCH_HEAD)
+
+	# 3600 seconds in an hour
+	if [ "$(($current - $last_modified))" -gt 3600 ]; then
+		git fetch -v
+	fi
+}
+fetch_dotfiles
