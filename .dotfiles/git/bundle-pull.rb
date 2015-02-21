ccf0dbc225532ff8132bd6a75dc7859be6d5f5c1
Ben Chatelain <benchatelain@gmail.com>
2015-02-21 13:00:44 -0700

    Get basic SSH working

--- .dotfiles/git/bundle-pull.rb
+++ .dotfiles/git/bundle-pull.rb
@@ -6,6 +6,7 @@
 #-------------------------------------------------------------------------------
 
 require 'pathname'
+require 'net/ssh' # gem install net-ssh
 
 # bundle-pull
 #
@@ -14,45 +15,85 @@ require 'pathname'
 # Ensure clean work tree before start, abort if dirty.
 #
 # - SSH in
-# - stash changes
+# - stash changes (snapshot)
 # - get HEAD SHA
-# - get stash
+# - git stash
 # - get stash SHA
 # - bundle create (& verify)
 # - end SSH
 # - git fetch OTHER_HOST $USER/REPO_PATH/BUNDLE_FILE bundle_branch
 #   - User dir is used as root (so can't get to system files, less typing)
 def bundle_pull()
-    command = 'bundle-pull'
+  command = 'bundle-pull'
 
-    # Ensure current dir is in a clean git repo
-    if !`git rev-parse --is-inside-work-tree >/dev/null 2>&1`
-        puts "The #{command} command must be run inside a git repo"
-        return
+  # Ensure current dir is in a clean git repo
+  if !`git rev-parse --is-inside-work-tree >/dev/null 2>&1`
+      puts "The #{command} command must be run inside a git repo"
+      return
+  end
+
+  # TODO: Remove ! when done testing
+  if !`git status --porcelain`
+      puts "The work tree is dirty, aborting."
+      return
+  end
+
+  # determine remote_hostname
+  username = ENV['USER']
+  puts "username: #{username}"
+
+  hostname = `hostname`
+  case
+      when 'octoair.local'
+          remote_hostname = 'imac.local'
+      when 'imac.local'
+          remote_hostname = 'octoair.local'
+  end
+  puts "remote_hostname: #{remote_hostname}"
+
+  # puts Dir.pwd
+  if !Dir.pwd.start_with? "/Users/#{username}"
+      puts 'This command can only be ran in your home directory.'
+  end
+
+  # SSH
+  print 'Enter your password: '
+  password = gets.chomp
+
+  # put commands to send to the remote Ruby here...
+  # def CMDs = [
+  #   '-v'
+  # ]
+
+  Net::SSH.start(remote_hostname, username, :password => password) do |ssh|
+
+    # remote_ruby = ssh.exec!('/usr/bin/which ruby').chomp
+    # puts 'Using remote Ruby: "%s"' % remote_ruby
+
+    # CMDs.each do |cmd|
+
+    #   puts 'Sending: "%s"' % cmd
+
+    #   stdout = ''
+    #   ssh.exec!("#{ remote_ruby } #{ cmd }") do |channel, stream, data|
+    #     stdout << data if stream == :stdout
+    #   end
+
+    #   puts 'Got: %s' % stdout
+    #   puts
+    # end
+
+    stdout = ''
+    ssh.exec!("pwd") do |channel, stream, data|
+      stdout << data if stream == :stdout
     end
 
-    if !`git status --porcelain`
-        puts "The work tree is dirty, aborting."
-        return
-    end
+    puts stdout
 
-    # determine remote_hostname
-    username = ENV['USER']
-    puts "username: #{username}"
+  end
 
-    hostname = `hostname`
-    case
-        when 'octoair.local'
-            remote_hostname = 'imac.local'
-        when 'imac.local'
-            remote_hostname = 'octoair.local'
-    end
-    puts "remote_hostname: #{remote_hostname}"
-
-    # puts Dir.pwd
-    if !Dir.pwd.start_with? "/Users/#{username}"
-        puts 'This command can only be ran in your home directory.'
-    end
+  # git snapshot
+  # git stash save "snapshot: $(date)" && git stash apply "stash@{0}"
 
 end
 
