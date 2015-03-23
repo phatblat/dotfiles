c9c5a55706c4c3b1df8e65c651573bc9e4fa1d48
Ben Chatelain <benchatelain@gmail.com>
2015-03-22 19:05:48 -0600

    Revise fauxpas-open alias to use 'active workspace document' to get rid of warning each time profile is loaded.
    http://stackoverflow.com/questions/1100721/how-to-build-run-xcode-with-applescript

--- .dotfiles/ios/alias.zsh
+++ .dotfiles/ios/alias.zsh
@@ -9,7 +9,7 @@ alias ox='open *.xcodeproj'
 alias ow='open *.xcworkspace'
 alias version_build='agvtool what-version -terse'
 alias version_market='agvtool what-marketing-version -terse1'
-alias fauxpas-open="open -a FauxPas ""`osascript -e 'tell application \"Xcode\" to return path of active project document'`"""
+alias fauxpas-open="open -a FauxPas ""`osascript -e 'tell application \"Xcode\" to return path of active workspace document'`"""
 
 function version_current() {
     local build_version market_version
