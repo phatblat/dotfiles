61677158182e5bc07130a263ea9c7be4916f4074
Ben Chatelain <benchatelain@gmail.com>
2015-03-25 07:59:10 -0600

    Add demangle alias

--- .dotfiles/ios/alias.zsh
+++ .dotfiles/ios/alias.zsh
@@ -10,6 +10,7 @@ alias ow='open *.xcworkspace'
 alias version_build='agvtool what-version -terse'
 alias version_market='agvtool what-marketing-version -terse1'
 alias fauxpas-open="open -a FauxPas ""`osascript -e 'tell application \"Xcode\" to return path of active workspace document'`"""
+alias demangle="xcrun swift-demangle"
 
 function version_current() {
     local build_version market_version
