eb926b7e9f0b894882d96c89329968065c30c364
Ben Chatelain <benchatelain@gmail.com>
2015-04-09 13:44:58 -0600

    Create version_enable alias

--- .dotfiles/ios/alias.zsh
+++ .dotfiles/ios/alias.zsh
@@ -7,6 +7,7 @@
 # Xcode
 alias ox='open *.xcodeproj'
 alias ow='open *.xcworkspace'
+alias version_enable='ruby ~/.dotfiles/ios/enable-versioning.rb'
 alias version_build='agvtool what-version -terse'
 alias version_market='agvtool what-marketing-version -terse1'
 alias fauxpas-open="open -a FauxPas ""`osascript -e 'tell application \"Xcode\" to return path of active workspace document'`"""
