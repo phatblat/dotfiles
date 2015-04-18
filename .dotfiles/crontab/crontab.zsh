1182565bb9587a615896bc1797e33d42d7ceea64
Ben Chatelain <benchatelain@gmail.com>
2015-04-18 14:08:43 -0600

    Add crontab.zsh to load crontab.file on local each time a terminal is opened

---
+++ .dotfiles/crontab/crontab.zsh
@@ -0,0 +1,9 @@
+#
+# crontab.zsh
+#
+# Sets up local crontab based on the tracked, shared file. This is automatic
+# so that I don't have to remember to reload the file on each Mac after it
+# is changed
+#
+
+crontab $HOME/.dotfiles/crontab/crontab.file
