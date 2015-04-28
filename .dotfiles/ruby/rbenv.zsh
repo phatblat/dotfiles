ac41377e8f0553d2d85d936b9549fc6df7c35082
Ben Chatelain <benchatelain@gmail.com>
2015-04-28 14:18:46 -0600

    Make RBENV_ROOT PATH addition smarter
    - Check for version file first to prevent errors when a ruby version hasn't been set yet

--- .dotfiles/ruby/rbenv.zsh
+++ .dotfiles/ruby/rbenv.zsh
@@ -7,5 +7,9 @@ export RBENV_ROOT=/usr/local/var/rbenv
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-export PATH=${PATH}:${RBENV_ROOT}/versions/$(cat ${RBENV_ROOT}/version)/bin
-# echo $PATH
+ruby_version_file="${RBENV_ROOT}/version"
+if [[ -f $ruby_version_file ]]; then
+	ruby_version=$(cat $ruby_version_file)
+	# echo "ruby $ruby_version"
+	export PATH=${PATH}:${RBENV_ROOT}/versions/$ruby_version/bin
+fi
