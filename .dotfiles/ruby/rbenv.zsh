ee294927f5c226ba25320b0ef8abce9d0a8dd9e5
Ben Chatelain <ben@octop.ad>
2015-04-28 04:05:29 -0600

    Comment out rbenv addition to PATH

--- .dotfiles/ruby/rbenv.zsh
+++ .dotfiles/ruby/rbenv.zsh
@@ -11,5 +11,5 @@ fi
 
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-export PATH=${PATH}:${RBENV_ROOT}/versions/$(cat ${RBENV_ROOT}/version)/bin
+# export PATH=${PATH}:${RBENV_ROOT}/versions/$(cat ${RBENV_ROOT}/version)/bin
 # echo $PATH
