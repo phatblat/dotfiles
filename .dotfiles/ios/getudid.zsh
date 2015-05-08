40bf6c36f4f0086c0e2ccf4b633436db4df85383
Ben Chatelain <benchatelain@gmail.com>
2015-05-08 13:44:18 -0600

    Add getudid function

---
+++ .dotfiles/ios/getudid.zsh
@@ -0,0 +1,16 @@
+
+# http://www.neglectedpotential.com/2015/03/simple-script-for-getting-a-devices-udid/
+
+function getudid {
+    udid=($(system_profiler SPUSBDataType | grep -A 11 -w "iPad\|iPhone\|iPad" | grep "Serial Number" | awk '{ print $3 }'))
+    if [ -z $udid ]; then
+        echo "No device detected. Please ensure an iOS device is plugged in."
+        exit 1
+    else
+        for i in "${udid[@]}"; do
+            echo -n $i | pbcopy
+            echo "UDID: $i"
+        done
+    fi
+}
+
