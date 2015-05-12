654607551c8ee32ddfd0dccfb6adbd06ebb4ec0b
Ben Chatelain <ben@octop.ad>
2015-05-11 20:05:15 -0600

    Add xcode_plugin_update_uuid function

---
+++ .dotfiles/ios/xcode-plugin.zsh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# xcode-plugin.zsh
+#
+
+# xcode_plugin_update_uuid
+#
+# Blindly adds the DVTPlugInCompatibilityUUID for every version of Xcode found
+# in /Applications to all Xcode plugins.
+#
+
+function xcode_plugin_update_uuid() {
+	for xcode in /Applications/Xcode*.app; do
+		uuid=$(defaults read "$xcode/Contents/Info" DVTPlugInCompatibilityUUID)
+
+		echo "$xcode - $uuid"
+
+		for plugin in ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/*.xcplugin; do
+			echo "Updating $plugin"
+			defaults write "$plugin/Contents/Info" DVTPlugInCompatibilityUUIDs -array-add $uuid
+		done
+
+	done
+}
