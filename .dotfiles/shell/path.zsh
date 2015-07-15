bc23c8459af76e2502505039b4b18aea7f5ab2c9
Ben Chatelain <benchatelain@gmail.com>
2015-07-15 12:23:35 -0600

    Fix PATH

--- .dotfiles/shell/path.zsh
+++ .dotfiles/shell/path.zsh
@@ -12,13 +12,12 @@ export LC_CTYPE=en_US.UTF-8
 # echo $PATH
 
 # Manually setting the original OS X path so that reloadprofile won't keep growing the PATH
-# OSX_PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/X11/bin:/usr/X11R6/bin
+OSX_PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-if [ -d /Users/ben/Library/Python/2.7/bin ] ; then
-	PYTHON_PATH=/Users/ben/Library/Python/2.7/bin
-	export PATH=${PYTHON_PATH}:${PATH}
+if [ -d ${HOME}/Library/Python/2.7/bin ] ; then
+	PYTHON_PATH=:${HOME}/Library/Python/2.7/bin
 fi
 
 # Prepend user bin dir to PATH
 # TODO: Get rbenv to load path in correct order
-export PATH=~/bin:/usr/local/bin:${PATH}:/usr/bin:/bin:/usr/sbin:/sbin
+export PATH=~/bin:${OSX_PATH}${PYTHON_PATH}
