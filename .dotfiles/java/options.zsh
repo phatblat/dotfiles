e9825cdf5a9589ac1d1e8f1c36de483c68c2e27f
Ben Chatelain <benchatelain@gmail.com>
2015-07-18 21:17:16 -0600

    Add java/options.zsh

---
+++ .dotfiles/java/options.zsh
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+#
+# java/options.zsh
+# Java Environment Options
+# http://docs.ubmatrix.com/webhelp/xda/1_0_1/Configure_Java_Environment_Options_(JAVA_OPTS).htm
+#
+#-------------------------------------------------------------------------------
+
+export JAVA_OPTS="-Xms256m -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=256m"
