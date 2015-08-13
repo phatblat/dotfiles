4624220dfed49aa13d6c67b32f424876e252629a
Ben Chatelain <benchatelain@gmail.com>
2015-08-13 09:41:45 -0600

    Add setJdk# aliases

---
+++ .dotfiles/java/alias.zsh
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+#
+# java/alias.zsh
+# Java Aliases
+#
+#-------------------------------------------------------------------------------
+
+alias setJdk6='export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)'
+alias setJdk7='export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)'
+alias setJdk8='export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)'
