4e603c3aae5cbaf91ef8e1e517d24f9a51eeba65
Ben Chatelain <ben@octop.ad>
2015-10-03 09:46:52 -0600

    [Install] Add charles

--- .dotfiles/install/install.sh
+++ .dotfiles/install/install.sh
@@ -67,7 +67,7 @@ brew tap neonichu/formulae && brew install chswift
 # Homebrew Cask
 brew install caskroom/cask/brew-cask
 brew cask install atom
-
+brew cask install charles
 brew cask install dayone-cli
 brew cask install fabric
 brew cask install geekbench
