5060baa24e52b0355c4151eb06a14e8c21356556
Ben Chatelain <ben@octop.ad>
2015-11-03 16:46:17 -0700

    Split out quicklook aliases into quicklook.zsh

---
+++ .dotfiles/shell/quicklook.zsh
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------
+#
+# shell/quicklook.zsh
+# QuickLook command-line aliases
+#
+#-------------------------------------------------------------------------------
+alias qlreload='qlmanage -r'
+alias qllist='qlmanage -m plugins'
