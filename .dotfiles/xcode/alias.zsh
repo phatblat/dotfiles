621ae07ab57fc64a518c58ca69752a522ecddfc1
Ben Chatelain <ben@octop.ad>
2015-11-03 08:29:21 -0700

    Split out xclist command

--- .dotfiles/xcode/alias.zsh
+++ .dotfiles/xcode/alias.zsh
@@ -122,17 +122,4 @@ function release() {
     fi
 }
 
-function xclist() {
-    local xcode filename
-
-    # echo /Applications/Xcode*.app
-    for xcode in /Applications/Xcode*.app; do
-        # echo $xcode
-        filename=$(basename $xcode)
-        echo ${filename%.*};
-    done
-}
-# export -f xclist
-# alias xclist="echo /Applications/Xcode*.app"
-
 alias dsym_uuid="mdls -name com_apple_xcode_dsym_uuids -raw *.dSYM | grep -e \\\" | sed 's/[ |\\\"]//g'"
