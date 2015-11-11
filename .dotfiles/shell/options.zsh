414627d720d50fb869545cf1c64660c5c1f2d14c
Ben Chatelain <ben@octop.ad>
2015-11-10 20:47:53 -0700

    Change EDIT to atom

--- .dotfiles/shell/options.zsh
+++ .dotfiles/shell/options.zsh
@@ -8,7 +8,7 @@ export TERM="xterm-256color"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 
-export EDIT='subl'
+export EDIT='atom'
 export EDITOR="vi"
 
 ### Below here from the oh-my-zsh .zshrc
