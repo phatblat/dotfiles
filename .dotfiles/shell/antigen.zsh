0320b36c72851d0490176a23fbc01e625fb0c6c4
Ben Chatelain <ben@octop.ad>
2015-11-15 22:08:49 -0700

    Split out antigen.zsh

---
+++ .dotfiles/shell/antigen.zsh
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+#
+# shell/antigen.zsh
+# Antigen zsh plugin setup
+# http://antigen.sharats.me
+#
+#-------------------------------------------------------------------------------
+
+local antigen_dir="$(brew --prefix)/Cellar/antigen/1/share"
+source "${antigen_dir}/antigen.zsh"
+antigen use oh-my-zsh
+# Override the oh-my-zsh 'd' alias
+unalias d && alias d='git diff'
+
+# Antigen Bundles
+antigen bundle common-aliases
+antigen bundle milkbikis/powerline-shell
+antigen bundle robbyrussell/oh-my-zsh plugins/ruby
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Antigen Themes
+antigen theme gnzh
+
+antigen apply
--- .dotfiles/shell/z_login.zsh
+++ .dotfiles/shell/z_login.zsh
@@ -15,23 +15,5 @@ if (( $? != 0 )); then
 	sshnewkey "${USER}@${HOST}"
 fi
 
-# Antigen
-local antigen_dir="$(brew --prefix)/Cellar/antigen/1/share"
-source "${antigen_dir}/antigen.zsh"
-antigen use oh-my-zsh
-# Override the oh-my-zsh 'd' alias
-unalias d && alias d='git diff'
-
-# Antigen Bundles
-antigen bundle common-aliases
-antigen bundle milkbikis/powerline-shell
-antigen bundle robbyrussell/oh-my-zsh plugins/ruby
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-# Antigen Themes
-antigen theme gnzh
-
-antigen apply
-
 # Setup prompt, must be called after antigen is configured
 install_powerline_prompt
