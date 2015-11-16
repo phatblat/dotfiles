d0bb0c2fc71ae1d893e808a81ee265f2aa11cc58
Ben Chatelain <ben@octop.ad>
2015-11-15 21:44:58 -0700

    Remove old powerline setup

--- .dotfiles/shell/powerline.zsh
+++ .dotfiles/shell/powerline.zsh
@@ -28,22 +28,4 @@ function install_powerline_prompt() {
 	fi
 }
 
-# Old, manual setup
-
-# if [ -d /usr/local/lib/python2.7/site-packages/powerline ] ; then
-# 	export POWERLINE_HOME=/usr/local/lib/python2.7/site-packages/powerline
-# elif [ -d ${HOME}/Library/Python/2.7/lib/python/site-packages/powerline ] ; then
-# 	export POWERLINE_HOME=${HOME}/Library/Python/2.7/lib/python/site-packages/powerline
-# else
-# 	echo "Powerline is not installed. See http://computers.tutsplus.com/tutorials/getting-spiffy-with-powerline--cms-20740"
-# fi
-#
-# export POWERLINE_CONFIG_PATHS=${HOME}/.powerline
-#
-# export DEFAULT_USER="phatblat"
-#
-# # Powerline
-# powerline-daemon -q
-# . ${POWERLINE_HOME}/bindings/zsh/powerline.zsh
-#
-# alias powerlinetest='echo "⮀ ± ⭠ ➦ ✔ ✘ ⚡"'
+alias powerlinetest='echo "⮀ ± ⭠ ➦ ✔ ✘ ⚡"'
