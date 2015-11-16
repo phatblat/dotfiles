8791896a8ecc3b0d646ece4239a6331e0b7bae05
Ben Chatelain <ben@octop.ad>
2015-11-15 17:58:41 -0700

    Resetup Powerline using milkbikis/powerline-shell

--- .dotfiles/shell/powerline.zsh
+++ .dotfiles/shell/powerline.zsh
@@ -5,20 +5,24 @@
 #
 #-------------------------------------------------------------------------------
 
-if [ -d /usr/local/lib/python2.7/site-packages/powerline ] ; then
-	export POWERLINE_HOME=/usr/local/lib/python2.7/site-packages/powerline
-elif [ -d ${HOME}/Library/Python/2.7/lib/python/site-packages/powerline ] ; then
-	export POWERLINE_HOME=${HOME}/Library/Python/2.7/lib/python/site-packages/powerline
-else
-	echo "Powerline is not installed. See http://computers.tutsplus.com/tutorials/getting-spiffy-with-powerline--cms-20740"
-fi
+# TODO: Relocate powerline setup from shell/z_login.zsh to here
 
-export POWERLINE_CONFIG_PATHS=${HOME}/.powerline
+# Old, manual setup
 
-export DEFAULT_USER="phatblat"
-
-# Powerline
-powerline-daemon -q
-. ${POWERLINE_HOME}/bindings/zsh/powerline.zsh
-
-alias powerlinetest='echo "⮀ ± ⭠ ➦ ✔ ✘ ⚡"'
+# if [ -d /usr/local/lib/python2.7/site-packages/powerline ] ; then
+# 	export POWERLINE_HOME=/usr/local/lib/python2.7/site-packages/powerline
+# elif [ -d ${HOME}/Library/Python/2.7/lib/python/site-packages/powerline ] ; then
+# 	export POWERLINE_HOME=${HOME}/Library/Python/2.7/lib/python/site-packages/powerline
+# else
+# 	echo "Powerline is not installed. See http://computers.tutsplus.com/tutorials/getting-spiffy-with-powerline--cms-20740"
+# fi
+#
+# export POWERLINE_CONFIG_PATHS=${HOME}/.powerline
+#
+# export DEFAULT_USER="phatblat"
+#
+# # Powerline
+# powerline-daemon -q
+# . ${POWERLINE_HOME}/bindings/zsh/powerline.zsh
+#
+# alias powerlinetest='echo "⮀ ± ⭠ ➦ ✔ ✘ ⚡"'
