c30eb89d44b65e704da4847de8b397258429ef55
Ben Chatelain <ben@octop.ad>
2015-11-16 21:22:47 -0700

    Add dev location for POWERLINE_HOME, commented out

--- .dotfiles/shell/powerline.zsh
+++ .dotfiles/shell/powerline.zsh
@@ -9,6 +9,7 @@
 # NOTE: This must be called after anigen is primed in z_login.zsh for $ADOTDIR
 function install_powerline_prompt() {
 	POWERLINE_HOME="${ADOTDIR}/repos/https-COLON--SLASH--SLASH-github.com-SLASH-phatblat-SLASH-powerline-shell.git-PIPE-custom"
+	# POWERLINE_HOME="${HOME}/dev/shell/powerline-shell"
 
 	function powerline_precmd() {
 		PS1="$(${POWERLINE_HOME}/powerline-shell.py $? --colorize-hostname --shell zsh 2> /dev/null)"
