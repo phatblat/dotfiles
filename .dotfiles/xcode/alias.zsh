659ecd077ad0f0dd89cbf96372f872f3179f9151
Ben Chatelain <ben@octop.ad>
2015-11-18 08:41:22 -0700

    Move provisioning aliases into provisioning.zsh

--- .dotfiles/xcode/alias.zsh
+++ .dotfiles/xcode/alias.zsh
@@ -1,4 +1,3 @@
-#!/bin/bash -e
 #-------------------------------------------------------------------------------
 #
 # xcode/alias.zsh
@@ -12,11 +11,6 @@ alias xccheck='~/.dotfiles/xcode/xccheck.sh'
 alias version_enable='ruby ~/.dotfiles/xcode/enable-versioning.rb'
 alias version_build='agvtool what-version -terse'
 alias version_market='agvtool what-marketing-version -terse1'
-alias plcat='plutil -convert xml1 -o /dev/stdout'
-alias list_codesign_identities='security find-identity -v -p codesigning'
-alias entitlements='codesign -d --entitlements :-'
-alias print_profile='security cms -D -i'
-alias codesign_verify="codesign --verify -vvvv -R='anchor apple generic and certificate 1[field.1.2.840.113635.100.6.2.1] exists and (certificate leaf[field.1.2.840.113635.100.6.1.2] exists or certificate leaf[field.1.2.840.113635.100.6.1.4] exists)'"
 alias register_device="~/.dotfiles/xcode/register_device.rb"
 
 # Common project folders
--- .dotfiles/xcode/provisioning.zsh
+++ .dotfiles/xcode/provisioning.zsh
@@ -1,6 +1,13 @@
 #-------------------------------------------------------------------------------
 #
-# Provisioning profiles
+# xcode/provisioning.zsh
+# Command-line aliases for working with provisioning profiles
 #
 #-------------------------------------------------------------------------------
-alias provdir='open "$HOME/Library/MobileDevice/Provisioning Profiles"'
+
+alias provdir='open "${HOME}/Library/MobileDevice/Provisioning Profiles"'
+alias plcat='plutil -convert xml1 -o /dev/stdout'
+alias list_codesign_identities='security find-identity -v -p codesigning'
+alias entitlements='codesign -d --entitlements :-'
+alias print_profile='security cms -D -i'
+alias codesign_verify="codesign --verify -vvvv -R='anchor apple generic and certificate 1[field.1.2.840.113635.100.6.2.1] exists and (certificate leaf[field.1.2.840.113635.100.6.1.2] exists or certificate leaf[field.1.2.840.113635.100.6.1.4] exists)'"
