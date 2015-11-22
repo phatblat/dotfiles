d249c196a07dd601574a5aa23b48523c1dfdb5d7
Ben Chatelain <ben@octop.ad>
2015-11-22 14:46:08 -0700

    Add bak function

--- .dotfiles/shell/alias.zsh
+++ .dotfiles/shell/alias.zsh
@@ -92,3 +92,38 @@ function fixperms() {
 	find "$1" -type f -print -exec chmod 644 {} \;
 	find "$1" -type d -print -exec chmod 755 {} \;
 }
+
+# Renames the first argument, either appending ".bak" or stripping that extension
+# if already present.
+# - $1 - The file or folder to rename
+function bak {
+	if [[ ! -a "$1" ]]; then
+		echo "'$1' does not exist"
+		return 1
+	fi
+
+	# > h
+	# >      Remove a trailing pathname component, leaving the head.  This
+	# >      works like `dirname'.
+	# >
+	# > r
+	# >      Remove a filename extension of the form `.XXX', leaving the root
+	# >      name.
+	# >
+	# > e
+	# >      Remove all but the extension.
+	# >
+	# > t
+	# >      Remove all leading pathname components, leaving the tail.  This
+	# >      works like `basename'.
+	# http://www.zsh.org/mla/users/2006/msg00239.html
+	if [[ "${1:e}" == "bak" ]]; then
+		# Remove the .bak extension
+		mv "$1" "${1:r}"
+		echo "Renamed to '${1:r}'"
+	else
+		# Append a .bak extension
+		mv "$1" "$1.bak"
+		echo "Renamed to '$1.bak'"
+	fi
+}
