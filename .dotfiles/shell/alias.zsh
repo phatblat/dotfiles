d032afa1e371fdd48428f99ce18c009786e83c51
Ben Chatelain <ben@octop.ad>
2015-11-24 15:14:56 -0700

    Split out shell/editors.zsh

--- .dotfiles/shell/alias.zsh
+++ .dotfiles/shell/alias.zsh
@@ -1,39 +1,9 @@
 #-------------------------------------------------------------------------------
 #
 # shell/alias.zsh
-# Command-line aliases
+# Miscellaneous command-line aliases
 #
 #-------------------------------------------------------------------------------
-
-
-# Editors
-
-# edit
-export VISUAL='/usr/local/bin/atom'
-alias edit="${VISUAL}"
-
-# e quick edit alias - with no args, opens editor to the current dir
-function e() {
-	if [ -z "$1" ] ; then
-		edit .
-	else
-		edit "$*"
-	fi
-}
-
-# o quick open alias - with no args, opens finder to the current dir
-function o() {
-	if [ -z "$1" ] ; then
-		open .
-	else
-		# -t  Causes the given path to be opened with the default app, as determined via LaunchServices
-		open -t "$*"
-	fi
-}
-
-# Sublime symlink installation
-alias subl_link='ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl'
-
 
 # ls
 alias lsa="ls -a"
@@ -47,13 +17,6 @@ alias lh="la -d .*"
 # Help
 bashman () { man bash | less -p "^       $1 "; }
 alias bashman=bashman
-
-# Profile
-alias explain="alias | grep"
-alias dotfiles="edit ~/.dotfiles"
-alias viprofile="vi ~/.zshrc && reloadprofile"
-# TODO: Figure out how to prevent PATH from growing when reloadprofile is invoked
-alias reloadprofile="source ~/.zshrc"
 
 # Shell Helpers
 alias h="history"
---
+++ .dotfiles/shell/editors.zsh
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+#
+# shell/editors.zsh
+# Editor aliases
+#
+#-------------------------------------------------------------------------------
+
+# edit
+export VISUAL='/usr/local/bin/atom'
+alias edit="${VISUAL}"
+
+# e quick edit alias - with no args, opens editor to the current dir
+function e() {
+	if [ -z "$1" ] ; then
+		edit .
+	else
+		edit "$*"
+	fi
+}
+
+# o quick open alias - with no args, opens finder to the current dir
+function o() {
+	if [ -z "$1" ] ; then
+		open .
+	else
+		# -t  Causes the given path to be opened with the default app, as determined via LaunchServices
+		open -t "$*"
+	fi
+}
+
+# Sublime symlink installation
+alias subl_link='ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl'
+
+# Profile
+alias explain="alias | grep"
+alias dotfiles="edit ~/.dotfiles"
+alias viprofile="vi ~/.zshrc && reloadprofile"
+# TODO: Figure out how to prevent PATH from growing when reloadprofile is invoked
+alias reloadprofile="source ~/.zshrc"
