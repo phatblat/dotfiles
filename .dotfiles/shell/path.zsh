14ba931ab986d8e22332156f0aa5ed2bcc0a64dc
Ben Chatelain <ben@octop.ad>
2015-12-04 18:55:13 -0700

    Remove custom PYTHON_PATH

--- .dotfiles/shell/path.zsh
+++ .dotfiles/shell/path.zsh
@@ -12,9 +12,5 @@
 # Manually setting the original OS X path so that reloadprofile won't keep growing the PATH
 OSX_PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
-if [ -d ${HOME}/Library/Python/2.7/bin ] ; then
-	PYTHON_PATH=:${HOME}/Library/Python/2.7/bin
-fi
-
 # Prepend user bin dir to PATH
 export PATH=~/bin${PYTHON_PATH}:${PATH}
