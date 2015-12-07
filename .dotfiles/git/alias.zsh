05a650dc0a948912f763672f342486d7b88f1cde
Ben Chatelain <ben@octop.ad>
2015-12-07 11:24:12 -0700

    Add SO link to bdm alias
    This does what the WIP bclan was tryin (and failing) to do

--- .dotfiles/git/alias.zsh
+++ .dotfiles/git/alias.zsh
@@ -104,6 +104,7 @@ alias bd='git branch -d'
 alias bD='git branch -D'
 
 ## Delete local branches merged into master
+# http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged#answer-6127884
 alias bdm='git branch -d $(git branch --merged | grep -v "^*" | grep -v "master" | tr -d "\n")'
 
 # Merge
