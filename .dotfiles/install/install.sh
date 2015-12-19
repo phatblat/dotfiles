0e344f17de4d37f148d4874a4087f4cb929069a2
Ben Chatelain <ben@octop.ad>
2015-12-19 15:47:16 -0700

    [install] Add swiftenv

--- .dotfiles/install/install.sh
+++ .dotfiles/install/install.sh
@@ -58,6 +58,7 @@ brew install hub
 brew install nodejs
 brew install sourcekitten
 brew install speedtest_cli
+brew install kylef/formulae/swiftenv
 brew install swiftgen
 brew install swiftlint
 brew install terminal-notifier
