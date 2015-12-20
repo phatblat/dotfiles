0daf792f6cd7cc3bfe4e56965d2bf91e7e82c109
Ben Chatelain <ben@octop.ad>
2015-12-20 07:15:44 -0700

    Remove chswift references

--- .dotfiles/swift/alias.zsh
+++ .dotfiles/swift/alias.zsh
@@ -7,7 +7,3 @@
 
 alias demangle='xcrun swift-demangle'
 
-source /usr/local/opt/chswift/share/chswift/chswift.sh
-
-# Enables auto-switching of Swifts specified by .swift-version files
-source /usr/local/opt/chswift/share/chswift/auto.sh
