742445ce62e6ae603f9eff480ebef182232fc268
Ben Chatelain <ben@octop.ad>
2015-12-21 17:40:04 -0700

    Extract quick cd aliases into shell/dirs.zsh

---
+++ .dotfiles/shell/dirs.zsh
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+#
+# shell/dirs.zsh
+# Quick dir navigation commands
+#
+#-------------------------------------------------------------------------------
+
+# Common Xcode project folders
+alias op='cd ~/dev/ios/Octopad'
+alias ebw='cd ~/dev/ios/EBW'
+alias itwire='cd ~/dev/bluemix/ITWire'
--- .dotfiles/xcode/alias.zsh
+++ .dotfiles/xcode/alias.zsh
@@ -13,11 +13,6 @@ alias version_build='agvtool what-version -terse'
 alias version_market='agvtool what-marketing-version -terse1'
 alias register_device="~/.dotfiles/xcode/register_device.rb"
 
-# Common project folders
-alias op='cd ~/dev/ios/Octopad'
-alias ebw='cd ~/dev/ios/EBW'
-alias itwire='cd ~/dev/bluemix/ITWire'
-
 function version_current() {
     local build_version market_version
     build_version=$(version_build)
