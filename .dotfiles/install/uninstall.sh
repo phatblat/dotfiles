a5b2519ffc559514058d0c21ea23c4636b0e14de
Ben Chatelain <ben@octop.ad>
2016-01-08 09:08:27 -0700

    Rename install-cleanup.sh -> uninstall.sh

---
+++ .dotfiles/install/uninstall.sh
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+#
+# install/uninstall.sh
+# Cleans up a botched dotfiles install.
+#
+#-------------------------------------------------------------------------------
+
+echo "WARNING: This script will remove dotfiles from your home directory."
+echo "Are you sure you wish to continue?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+
+rm -rf "${HOME}/.git"
+rm -rf "${HOME}/.dotfiles"
+rm -rf "${HOME}/tmp/dotfiles"
