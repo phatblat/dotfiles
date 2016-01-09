692928dfae6faa6405be3e7ea3b7f397696e4c8d
Ben Chatelain <ben@octop.ad>
2016-01-08 19:36:20 -0700

    Centralize admin install aliases

--- .dotfiles/install/alias.zsh
+++ .dotfiles/install/alias.zsh
@@ -5,5 +5,21 @@
 #
 #-------------------------------------------------------------------------------
 
-# install
-alias install='~/.dotfiles/install/install.sh'
+# Only define these aliases for admin users
+if [[ $(dsmemberutil checkmembership -U "${USER}" -G "admin") == "user is a member of the group" ]]; then
+
+  # update
+  # Invokes the admin package update script
+  alias update='~/.dotfiles/install/update.sh'
+
+  # cleanup
+  # Cleans up old gems, checks Homebrew
+  function cleanup {
+    gem cleanup
+
+    # Print any warnings about the current homebrew setup, they will need to be
+    # resolved manually.
+    brew doctor
+  }
+
+fi
