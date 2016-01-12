81f3673dd1c35a30c35e1756273dd68001f68ed0
Ben Chatelain <ben@octop.ad>
2016-01-12 09:35:49 -0700

    Whitespace formatting in shell/editors

--- .dotfiles/shell/editors.zsh
+++ .dotfiles/shell/editors.zsh
@@ -11,27 +11,30 @@ alias edit="${VISUAL}"
 
 # e quick edit alias - with no args, opens editor to the current dir
 function e() {
-	if [ -z "$1" ] ; then
-		edit .
-	else
-		edit "$*"
-	fi
+  if [ -z "$1" ] ; then
+    edit .
+  else
+    edit "$*"
+  fi
 }
 
 # o quick open alias - with no args, opens finder to the current dir
 function o() {
-	if [ -z "$1" ] ; then
-		open .
-	else
-		# -t  Causes the given path to be opened with the default app, as determined via LaunchServices
-		open -t "$*"
-	fi
+  if [ -z "$1" ] ; then
+    open .
+  else
+    # -t  Causes the given path to be opened with the default app, as determined via LaunchServices
+    open -t "$*"
+  fi
 }
 
 # Sublime symlink installation
 alias subl_link='ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl'
 
+
+#-------------------------------------------------------------------------------
 # Profile
+#-------------------------------------------------------------------------------
 alias explain="alias | grep"
 alias dotfiles="edit ~/.dotfiles"
 
