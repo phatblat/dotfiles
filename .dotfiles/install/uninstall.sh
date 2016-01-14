1fce4d963a7c1777dabf8a5c8553c16db22ab64e
Ben Chatelain <ben@octop.ad>
2016-01-13 18:55:13 -0700

    Two space indent

--- .dotfiles/install/uninstall.sh
+++ .dotfiles/install/uninstall.sh
@@ -12,10 +12,10 @@ echo
 echo "WARNING: This script will remove dotfiles from your home directory."
 echo "Are you sure you wish to continue?"
 select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) exit;;
-    esac
+  case $yn in
+    Yes ) break;;
+    No ) exit;;
+  esac
 done
 
 rm -rf "${HOME}/.git"
