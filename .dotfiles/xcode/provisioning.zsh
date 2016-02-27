d635d31b7d5e85be254624c95a00f8c2d0f28bf7
Ben Chatelain <ben@octop.ad>
2016-02-26 17:02:20 -0700

    Add profile_id function

--- .dotfiles/xcode/provisioning.zsh
+++ .dotfiles/xcode/provisioning.zsh
@@ -18,3 +18,12 @@ alias print_profile='security cms -D -i'
 function uuid_from_profile() {
   print_profile $1 | grep UUID -A 1 | tail -n 1 | cut -d ">" -f 2 | cut -d "<" -f 1
 }
+
+function profile_id {
+    if [[ $# -ne 1 ]]; then
+      echo "Usage: profile_id AppProfile.mobileprovision"
+      return 1
+    fi
+
+    egrep -a -A 2 UUID $1 | grep string | sed -e 's/<string>//' -e 's/<\/string>//' -e 's/[ 	]//'
+}
