bc038f8e147c873eda0795cf659316b3fad51b3e
Ben Chatelain <ben@octop.ad>
2016-04-18 18:55:13 -0600

    Add bundler aliases

--- .dotfiles/xcode/cocoapods.zsh
+++ .dotfiles/xcode/cocoapods.zsh
@@ -5,8 +5,15 @@
 #
 #-------------------------------------------------------------------------------
 
+# Bundler
+alias bi='bundle install'
+alias bu='bundle update'
+
 # CocoaPods
 alias pi='pod install --no-repo-update'
+alias bpi='bundle exec pi'
+alias bpii='bundle exec pod install'
 alias piv='pod install --no-repo-update --verbose'
+alias bpiv='bundle exec piv'
 
 alias clearcpcache='rm -rf "${HOME}/Library/Caches/CocoaPods"'
