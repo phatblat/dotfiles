c4331285da00338c917e736c4e718fa65b548cf8
Ben Chatelain <ben@octop.ad>
2016-05-05 20:56:32 -0600

    Split out bundler.zsh

---
+++ .dotfiles/ruby/bundler.zsh
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+#
+# ruby/bundler.zsh
+# Command-line aliases for Bundler
+#
+#-------------------------------------------------------------------------------
+
+# Bundler
+alias bi='bundle install'
+alias bu='bundle update'
+alias be='bundle exec'
+
+# Fastlane
+alias bef='bundle exec fastlane'
