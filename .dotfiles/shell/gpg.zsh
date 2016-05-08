c86aff8b4d310a76b96758cf15ee0c5647c9c153
Ben Chatelain <ben@octop.ad>
2016-05-08 13:53:13 -0600

    Add gpgcopypub function

--- .dotfiles/shell/gpg.zsh
+++ .dotfiles/shell/gpg.zsh
@@ -6,3 +6,16 @@
 #-------------------------------------------------------------------------------
 
 alias gpgshow='gpg --list-keys'
+
+function gpgcopypub {
+  output=$(gpg --list-keys | head -n 3 | tail -n 1)
+  # tokens=${output/\// }
+  # Split on spaces
+  key_info=${output[(ws: :)2]}
+  echo $key_info
+  # Split on slash
+  key=${key_info[(ws:/:)2]}
+  gpg_key_ascii=$(gpg --armor --export $key)
+  echo $gpg_key_ascii | pbcopy $gpg_key_ascii
+  echo "GPG key copied to pasteboard"
+}
