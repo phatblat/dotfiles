1846a74e3baedaadfa545b7629662a33eb6fa4ed
Ben Chatelain <ben@octop.ad>
2016-05-08 20:29:23 -0600

    Disable rbenv

--- .dotfiles/ruby/rbenv.zsh
+++ .dotfiles/ruby/rbenv.zsh
@@ -4,13 +4,13 @@
 # ruby/rbenv.zsh
 #
 #-------------------------------------------------------------------------------
-export RBENV_ROOT=${HOME}/.rbenv
-
-if which -s rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-ruby_version_file="${RBENV_ROOT}/version"
-if [[ -f $ruby_version_file ]]; then
-  ruby_version=$(cat $ruby_version_file)
-  # echo "ruby $ruby_version"
-  export PATH=${PATH}:${RBENV_ROOT}/versions/$ruby_version/bin
-fi
+# export RBENV_ROOT=${HOME}/.rbenv
+#
+# if which -s rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+#
+# ruby_version_file="${RBENV_ROOT}/version"
+# if [[ -f $ruby_version_file ]]; then
+#   ruby_version=$(cat $ruby_version_file)
+#   # echo "ruby $ruby_version"
+#   export PATH=${PATH}:${RBENV_ROOT}/versions/$ruby_version/bin
+# fi
