fed081198584a9b089ccbdfd0cac8cf163dd534d
Ben Chatelain <ben@octop.ad>
2016-05-09 19:59:19 -0600

    Add template files for liftoff

---
+++ .liftoff/templates/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  <%= project_name %>
+//
+//  Created by <%= author %> on <%= Time.now.strftime("%-m/%-d/%y") %>
+//  Copyright (c) <%= Time.now.strftime('%Y') %> <%= company %>. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
