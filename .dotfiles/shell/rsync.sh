837aed60a7a7f6e2954b0114b7c092807d5dcd24
Ben Chatelain <ben@octop.ad>
2016-05-14 12:59:07 -0600

    Don't show rsync progress
    very noisy output in cron emails

--- .dotfiles/shell/rsync.sh
+++ .dotfiles/shell/rsync.sh
@@ -28,7 +28,7 @@ function sync {
     echo "rsyncing ${source_dir} -> ${destination_dir}"
     mkdir -p "${destination_dir}"
     # Run (-aP)
-    rsync --archive --partial --progress "${source_dir}" "${destination_dir}"
+    rsync --archive --partial "${source_dir}" "${destination_dir}"
   else
     echo "rsyncing ${source_dir} -> ${destination_dir} (dry run)"
     # Test (-anv)
