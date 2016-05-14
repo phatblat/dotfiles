e33d6414a4713da26082842fe379a503a46fabd3
Ben Chatelain <ben@octop.ad>
2016-05-13 22:17:40 -0600

    Add test for hostname

--- .dotfiles/shell/testing.sh
+++ .dotfiles/shell/testing.sh
@@ -6,4 +6,11 @@
 #-------------------------------------------------------------------------------
 
 this_host=$(hostname)
-echo "this_host: $this_host"
+echo "this_host: $this_host \c"
+
+if [[ $this_host != "imac.local" ]]; then
+  echo "(not iMac)"
+  exit
+fi
+
+echo "(iMac)"
