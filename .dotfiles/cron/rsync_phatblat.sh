36309f8def2580a2a174438399bd4ff18e14c674
Ben Chatelain <ben@octop.ad>
2016-05-13 22:18:53 -0600

    Configure sync command to only run on iMac

--- .dotfiles/cron/rsync_phatblat.sh
+++ .dotfiles/cron/rsync_phatblat.sh
@@ -8,5 +8,12 @@
 . $HOME/.dotfiles/cron/cron.env
 . $HOME/.dotfiles/shell/rsync.sh      # Defines sync function
 
+this_host=$(hostname)
+
+# Only run this on iMac
+if [[ $this_host != "imac.local" ]]; then
+  exit
+fi
+
 # Sync user dir from iMac -> ThunderBay
 time sync $phatblat_imac $phatblat_external "go"
