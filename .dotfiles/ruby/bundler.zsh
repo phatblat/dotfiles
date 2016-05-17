8223d93f2166a2994ed8b02ea643922781505c06
Ben Chatelain <ben@octop.ad>
2016-05-17 10:03:04 -0600

    Add bv alias

--- .dotfiles/ruby/bundler.zsh
+++ .dotfiles/ruby/bundler.zsh
@@ -10,6 +10,7 @@ config='--clean --path .rubygems --binstubs bin'
 alias bi="bundle install $config"
 alias bu='bundle update'
 alias be='bundle exec'
+alias bv='bundle --verion'
 
 # El Capitan workaround to not being able to install Bundler to /usr/bin
 # sudo gem install bundler --bindir /usr/local/bin
