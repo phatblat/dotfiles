a646f102c38b7c2b8375689ddcab455a76ad1b99
Ben Chatelain <ben@octop.ad>
2016-07-08 13:52:03 -0600

    Split out shell/config.zsh

---
+++ .dotfiles/shell/config.zsh
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+#
+# shell/config.zsh
+# Shell configuration
+#
+#-------------------------------------------------------------------------------
+
+# Enable vi style command editing.
+# This setting doesn't work when applied in shell/options.zsh
+bindkey -v
+
+# Carthage Zsh Completion
+# https://github.com/Carthage/Carthage/blob/master/Documentation/BashZshFishCompletion.md#zsh
+autoload -U compinit
+compinit -u
+
+# Allow [ or ] whereever you want
+# https://robots.thoughtbot.com/how-to-use-arguments-in-a-rake-task
+unsetopt nomatch
--- .zshrc
+++ .zshrc
@@ -30,16 +30,3 @@ do
 done
 
 popd > /dev/null 2>&1
-
-# Enable vi style command editing.
-# This setting doesn't work when applied in shell/options.zsh
-bindkey -v
-
-# Carthage Zsh Completion
-# https://github.com/Carthage/Carthage/blob/master/Documentation/BashZshFishCompletion.md#zsh
-autoload -U compinit
-compinit -u
-
-# Allow [ or ] whereever you want
-# https://robots.thoughtbot.com/how-to-use-arguments-in-a-rake-task
-unsetopt nomatch
