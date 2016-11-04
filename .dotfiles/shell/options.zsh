15a9674c6d2e595bc62684433aa82e69bb9130c2
Ben Chatelain <ben@octop.ad>
2016-11-04 15:26:11 -0600

    Remove duplicate VISUAL definition from shell options

--- .dotfiles/shell/options.zsh
+++ .dotfiles/shell/options.zsh
@@ -8,9 +8,6 @@ export TERM="xterm-256color"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 
-export VISUAL='/usr/bin/vim'
-export EDITOR='vi -e'
-
 # Allow [ or ] whereever you want
 unsetopt nomatch
 
