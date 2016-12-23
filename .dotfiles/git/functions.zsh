d12c8d0458a03a0bfd76b33479bd433a3650e230
Ben Chatelain <ben@octop.ad>
2016-12-23 16:14:55 -0700

    Add list-authors function

--- .dotfiles/git/functions.zsh
+++ .dotfiles/git/functions.zsh
@@ -79,3 +79,35 @@ function rewrite {
 
   git filter-branch -f --env-filter "${filter_command}"
 }
+
+# list-authors
+# Extracts a list of contributors from git commit history.
+function list-authors {
+  local all_authors=()
+  local format
+
+  if [[ $# -eq 0 ]]; then
+    # Email formatting
+    format='%an <%ae>'
+  elif [[ "$1" == "name" ]]; then
+    # Ruby hash
+    format='%an'
+  elif [[ "$1" == "email" ]]; then
+    # Ruby hash
+    format='%ae'
+  elif [[ "$1" == "ruby" ]]; then
+    # Ruby hash
+    format='"%an" => "%ae",'
+  else
+    # Custom format
+    format="$1"
+  fi
+
+  # Iterate over the hash of all commits
+  for commit in $(git rev-list --all)
+  do
+    all_authors+=("`git --no-pager show -s --format=${format} ${commit}`")
+  done
+
+  printf '%s\n' "${all_authors[@]}" | sort | uniq
+}
