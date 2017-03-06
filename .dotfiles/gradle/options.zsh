a34ad26be52bd6a6e1001612cc509b47724e7877
Ben Chatelain <ben@octop.ad>
2017-03-06 16:15:53 -0700

    set GRADLE_OPTS to 1 gig heap space

--- .dotfiles/gradle/options.zsh
+++ .dotfiles/gradle/options.zsh
@@ -5,14 +5,16 @@
 #
 #-------------------------------------------------------------------------------
 
+export GRADLE_OPTS=-Xmx1g
+export GRADLE_HEAP_SPACE=-Xmx1g
 
 function gradledebug {
   if [[ -z "${GRADLE_OPTS}" || $1 == "on" ]]; then
-    export GRADLE_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+    export GRADLE_OPTS="${GRADLE_HEAP_SPACE} -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
     echo "Gradle debugging on"
     echo "GRADLE_OPTS: ${GRADLE_OPTS}"
   else
-    export GRADLE_OPTS=
+    export GRADLE_OPTS="${GRADLE_HEAP_SPACE}"
     echo "Gradle debugging off"
   fi
 }
