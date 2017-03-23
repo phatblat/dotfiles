7771054b50e1cb02791b78b07c97d07e92fa4cb2
Ben Chatelain <ben@octop.ad>
2017-03-22 20:03:11 -0600

    Set InitialKeyRepeat to 15

--- .dotfiles/macos/set-defaults.zsh
+++ .dotfiles/macos/set-defaults.zsh
@@ -13,7 +13,7 @@
 #
 # Keyboard
 #
-defaults write -g InitialKeyRepeat -int 1 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
 #
