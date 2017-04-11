d617c361bd18caa7164d04fed5425829f0211adf
Ben Chatelain <ben@octop.ad>
2017-04-11 12:11:58 -0600

    Output big file sizes in KB

--- .dotfiles/shell/alias.zsh
+++ .dotfiles/shell/alias.zsh
@@ -16,7 +16,7 @@ alias lhd="la -d .*/"
 alias lh="la -d .*"
 
 # File sizes
-alias bigfiles='du -a . | sort -n -r | head -n 10'
+alias bigfiles='echo "File sizes in KB" && du -ka . | sort -n -r | head -n 10'
 
 # Shell Helpers
 alias h='history'
