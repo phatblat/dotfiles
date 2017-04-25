407ac9d0640b6506e7349af8ff0096b7ddf5bebf
Ben Chatelain <ben@octop.ad>
2017-04-25 05:54:41 -0600

    Add current state of zsh options

--- .dotfiles/shell/options.zsh
+++ .dotfiles/shell/options.zsh
@@ -4,6 +4,7 @@
 # Shell options
 #
 #-------------------------------------------------------------------------------
+
 export TERM="xterm-256color"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
@@ -11,3 +12,27 @@ export LANGUAGE="en_US.UTF-8"
 
 # Allow [ or ] whereever you want
 unsetopt nomatch
+
+# Current zsh options
+setopt alwaystoend
+setopt autocd
+setopt autopushd
+setopt completeinword
+setopt extendedhistory
+setopt noflowcontrol
+setopt histexpiredupsfirst
+setopt histignoredups
+setopt histignorespace
+setopt histverify
+setopt incappendhistory
+setopt interactive
+setopt interactivecomments
+setopt login
+setopt longlistjobs
+setopt monitor
+setopt nonomatch
+setopt promptsubst
+setopt pushdignoredups
+setopt pushdminus
+setopt sharehistory
+setopt zle
