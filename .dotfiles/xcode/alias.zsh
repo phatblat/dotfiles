dcff81fe465b672df76e07ad7a7203d1fea43da3
Ben Chatelain <ben@octop.ad>
2017-04-24 20:52:40 -0600

    Add xclinkapp function for switching xcode versions

--- .dotfiles/xcode/alias.zsh
+++ .dotfiles/xcode/alias.zsh
@@ -5,19 +5,43 @@
 #
 #-------------------------------------------------------------------------------
 
+# Quick Open Commands
 alias ox='open *.xcodeproj'
 alias ow='open *.xcworkspace'
 alias bpx='bi && pi && ow'
+
+# Xcode versions
+alias adev='open https://developer.apple.com/news/'
 alias xcodes='ls -ld /Applications/Xcode*'
 alias xv='xcodebuild -version'
 alias xcsp='xcode-select --print-path'
 alias xcss='sudo xcode-select --switch'
 alias xccheck='~/.dotfiles/xcode/xccheck.sh'
+
+function xclinkapp {
+  if [[ $# -ne 1 ]]; then
+    echo "Usage: xclinkapp <Version>"
+    echo "Version format: 8.3.2"
+    return 1
+  fi
+  local VERSION="$1"
+  echo "Activating Xcode Version: ${VERSION}"
+  # return 0
+
+  rm -f "/Applications/Xcode.app"
+  ln -s "/Applications/Xcode-${VERSION}.app" "/Applications/Xcode.app"
+  xcodes
+  xcss "/Applications/Xcode-${VERSION}.app"
+  xcsp
+}
+
+# Project Versioning
 alias version_enable='ruby ~/.dotfiles/xcode/enable-versioning.rb'
 alias version_build='agvtool what-version -terse'
 alias version_market='agvtool what-marketing-version -terse1'
-alias register_device="~/.dotfiles/xcode/register_device.rb"
+
+# Devices
 alias devices='instruments -s devices'
+alias register_device="~/.dotfiles/xcode/register_device.rb"
 alias developer_mode='DevToolsSecurity -status && sudo DevToolsSecurity -enable'
-alias adev='open https://developer.apple.com/news/'
 alias killsim='launchctl list com.apple.CoreSimulator.CoreSimulatorService && ps aux | grep CoreSimulator && launchctl remove com.apple.CoreSimulator.CoreSimulatorService && echo "CoreSimulatorService has been removed from launchctl."'
