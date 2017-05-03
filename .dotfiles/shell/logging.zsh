1607c929abc3494f4fbd4a5e738f885dacfa614e
Ben Chatelain <ben@octop.ad>
2017-05-03 14:08:05 -0600

    Configure lumberjack

---
+++ .dotfiles/shell/logging.zsh
@@ -0,0 +1,9 @@
+#-------------------------------------------------------------------------------
+#
+# shell/logging.zsh
+# Logging for dotfiles
+#
+#-------------------------------------------------------------------------------
+
+mkdir -p /usr/local/var/log/lj
+lj --file /usr/local/var/log/lj/lumberjack.log --level debug
