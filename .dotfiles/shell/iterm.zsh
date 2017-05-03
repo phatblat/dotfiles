5f9ed419deed5c2cb544cbbfa83395c8f433084f
Ben Chatelain <ben@octop.ad>
2017-05-03 14:54:33 -0600

    Add iTerm shell integration

---
+++ .dotfiles/shell/iterm.zsh
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+#
+# shell/iterm.zsh
+# iTerm 2 shell integration
+#
+#-------------------------------------------------------------------------------
+
+lj info 'shell/iterm.zsh'
+
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
