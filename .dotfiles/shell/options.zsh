65ab19d7580e02281096f77e2386110d4a04da8e
Ben Chatelain <ben@octop.ad>
2017-05-03 15:47:15 -0600

    Set LANG

--- .dotfiles/shell/options.zsh
+++ .dotfiles/shell/options.zsh
@@ -8,9 +8,10 @@
 lj info 'shell/options.zsh'
 
 export TERM="xterm-256color"
+export LANGUAGE="en_US.UTF-8"
+export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
-export LANGUAGE="en_US.UTF-8"
 
 #-------------------------------------------------------------------------------
 # Changing Directories
