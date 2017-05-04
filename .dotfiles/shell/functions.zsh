f0e6093b6bf89ccd195a6b7db8f466937058aa01
Ben Chatelain <ben@octop.ad>
2017-05-04 13:02:00 -0600

    Use print builtin in ctitle function to allow % prompt escapes

--- .dotfiles/shell/functions.zsh
+++ .dotfiles/shell/functions.zsh
@@ -27,7 +27,14 @@ function ctitle {
   
   # Disable OMZ auto-title so that it doesn't overwrite the custom title.
   DISABLE_AUTO_TITLE="true"
-  echo -ne "\e]1;$@\a"
+
+  # echo -ne "\e]1;$@\a"
+
+  #  print builtin works like echo, but gives us access to the % prompt escapes.
+  # %n          expands to $USERNAME
+  # %m          expands to hostname up to first '.'
+  # %~          expands to directory, replacing $HOME with '~'
+  print -Pn "\e]1;$@\a"
 }
 
 # Find
