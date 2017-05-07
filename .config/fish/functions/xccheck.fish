b715cc60c2e723516bf166e35cb0810998acbc32
Ben Chatelain <ben@octop.ad>
2017-05-06 23:59:53 -0600

    Update xccheck to fish syntax

--- .config/fish/functions/xccheck.fish
+++ .config/fish/functions/xccheck.fish
@@ -1,5 +1,15 @@
-# 
+# Validates Xcode application binary integrity using `spctl`
+# Migrated from ~/.dotfiles/xcode/xccheck.sh
 function xccheck
-    ~/.dotfiles/xcode/xccheck.sh $argv
-end
+    set app_dirs \
+        "/Applications" \
+        "$HOME/Applications" \
+        "/Volumes/Thunderbay/Applications" \
 
+    for app_dir in $app_dirs
+        for xcode_version in (ls -d $app_dir/Xcode*.app)
+            echo "Checking integrity of $xcode_version"
+            spctl --assess --verbose "$xcode_version"
+        end
+    end
+end
