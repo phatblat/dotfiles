5be8972ae28ace45f0cec5aa18b085b726b471f1
Ben Chatelain <ben@octop.ad>
2017-05-07 09:44:12 -0600

    Update bdm function with updated SO answer

--- .config/fish/functions/bdm.fish
+++ .config/fish/functions/bdm.fish
@@ -1,10 +1,7 @@
-# Delete local branches which have been merged into master.
+# Delete local branches which have been merged into the current branch, always preserving master.
 # http://stackoverflow.com/questions/6127328/how-can-i-delete-all-git-branches-which-have-been-merged#answer-6127884
 function bdm
-    git branch -d ( \
-        git branch --merged \
-        | grep -v "^*" \
-        | grep -v "master" \
-        | tr -d "\n" \
-    ) $argv
+    git branch --merged \
+    | egrep -v "(^\*|master)" \
+    | xargs git branch -d
 end
