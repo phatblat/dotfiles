379efb020968feef05950fb93b37d33165ed1725
Ben Chatelain <ben@octop.ad>
2017-05-07 23:34:07 -0600

    Update sortdiff for fish syntax

--- .config/fish/functions/sortdiff.fish
+++ .config/fish/functions/sortdiff.fish
@@ -1,5 +1,8 @@
-# 
+# Filter and sort a git diff showing only the changed lines.
 function sortdiff
-    git diff "$@" | grep "^[+-]" | sort --key=1.2 | uniq -u -s1 $argv
+    git diff $argv \
+    | egrep --invert-match "^[+-]{3} " \
+    | grep "^[+-]" \
+    | sort --key=1.2 \
+    | uniq -u -s1
 end
-
