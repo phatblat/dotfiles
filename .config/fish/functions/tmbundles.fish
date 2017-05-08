f0f90e8d612ff974656ac6985661100240dbe610
Ben Chatelain <ben@octop.ad>
2017-05-08 09:36:25 -0600

    Refactor tmbundles to install missing bundles

--- .config/fish/functions/tmbundles.fish
+++ .config/fish/functions/tmbundles.fish
@@ -1,6 +1,23 @@
 # Manage TextMate bundles.
 function tmbundles
-    cd ~/Library/Application\ Support/TextMate/Bundle
-    open fish.tmbundle
-    cd -
+    set -l bundle_dir ~/Library/Application\ Support/TextMate/Bundles
+    set -l bundles fish gradle
+
+    pushd
+
+    for bundle in $bundles
+        if not test -e $bundle.tmbundle
+            switch $bundle
+                case fish
+                    git clone git@github.com:l15n/fish-tmbundle.git fish.tmbundle
+                case gradle
+                    git clone git@github.com:alkemist/gradle.tmbundle.git
+            end
+        end
+        open $bundle.tmbundle
+    end
+
+    echo $bundle_dir
+    ls -l
+    popd
 end
