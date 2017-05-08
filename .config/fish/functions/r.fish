b03c497110fb8096f4147875a07dcd3bfe54856d
Ben Chatelain <ben@octop.ad>
2017-05-07 23:38:31 -0600

    Update r function, accept commit count argument

--- .config/fish/functions/r.fish
+++ .config/fish/functions/r.fish
@@ -1,5 +1,10 @@
-# 
+# Interactive rebase for the last few commits, count specified as 1st arg (default: 10).
 function r
-    git rebase --interactive HEAD~10 $argv
-end
+    echo $argv | read -l count
 
+    if test -z $count
+        set count 10
+    end
+
+    git rebase --interactive HEAD~$count $argv
+end
