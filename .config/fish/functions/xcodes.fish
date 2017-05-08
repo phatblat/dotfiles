50e77a826540b4524ac6b14ae99fd0dcef5cb8eb
Ben Chatelain <ben@octop.ad>
2017-05-07 23:43:40 -0600

    Update xcodes

--- .config/fish/functions/xcodes.fish
+++ .config/fish/functions/xcodes.fish
@@ -1,5 +1,13 @@
-# 
+# Print path to all installed copies of Xcode.
 function xcodes
-    ls -ld /Applications/Xcode* $argv
-end
+    set app_dirs \
+        "/Applications" \
+        "$HOME/Applications" \
+        "/Volumes/Thunderbay/Applications"
 
+    for app_dir in $app_dirs
+        for app in $app_dir/Xcode*
+            ls -ld $app
+        end
+    end
+end
