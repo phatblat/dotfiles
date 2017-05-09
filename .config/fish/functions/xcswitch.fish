5ed2270ca6dbfd528c7e781f2a7a903e422af38e
Ben Chatelain <ben@octop.ad>
2017-05-09 15:26:41 -0600

    Convert xcswitch

--- .config/fish/functions/xcswitch.fish
+++ .config/fish/functions/xcswitch.fish
@@ -1,17 +1,26 @@
-# 
-function xcswitch
-      if [[ $# -ne 1 ]]; then
-    echo "Usage: xclinkapp <Version>"
-    echo "Version format: 8.3.2"
-    return 1
-  fi
-  local VERSION="$1"
-  echo "Activating Xcode Version: ${VERSION}"
-  # return 0
+# Switch the active version of Xcode.
+function xcswitch --argument-names version
+    if test -z $version
+        echo "Usage: xcswitch <version>"
+        echo "Version format: 8.3.2"
+        return 1
+    end
 
-  rm -f "/Applications/Xcode.app"
-  ln -s "/Applications/Xcode-${VERSION}.app" "/Applications/Xcode.app"
-  xcodes
-  xcss "/Applications/Xcode-${VERSION}.app"
-  xcsp $argv
+    set -l xcode_path "/Applications/Xcode-$version.app"
+    if not test -e $xcode_path
+        echo $xcode_path does not exist.
+        return 2
+    end
+
+    echo "Activating Xcode Version: $version"
+
+    # Switch the version of Xcode on the system.
+    xcss $xcode_path
+
+    # Creat a symlink so tools, like SwiftLint, that assume Xcode was 
+    # Installed via the MAS use the correct version.
+    ln -fs $xcode_path "/Applications/Xcode.app"
+    xcodes
+
+    echo "Activated "(string sub --length (string length $xcode_path) (xcsp))
 end
