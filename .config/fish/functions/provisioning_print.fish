9e01a7bbfcf19399aab89c50c337c5ddfe0de81b
Ben Chatelain <ben@octop.ad>
2017-05-09 15:44:21 -0600

    Validate input and snuff stderr in provisioning_print

--- .config/fish/functions/provisioning_print.fish
+++ .config/fish/functions/provisioning_print.fish
@@ -1,4 +1,14 @@
 # Prints a text version of a provisioning profile.
-function provisioning_print
-    security cms -D -i $argv
+function provisioning_print --argument-names profile_path
+    if test -z $profile_path
+        echo "Usage: provisioning_print path/to/profile.mobileprovision"
+        return 1
+    end
+
+    if not test -e $profile_path
+        echo $profile_path does not exist
+        return 2
+    end
+
+    security cms -D -i $profile_path ^/dev/null
 end
