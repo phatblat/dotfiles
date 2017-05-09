4171758336c06bfd7016d67b7cbb775456a990fe
Ben Chatelain <ben@octop.ad>
2017-05-08 21:54:46 -0600

    Update syntax of finds

--- .config/fish/functions/finds.fish
+++ .config/fish/functions/finds.fish
@@ -1,22 +1,23 @@
-# 
-function finds
-      if [[ -z $1 ]]; then
-    echo "Missing search term"
-    echo 'usage: finds "search string" /base/dir'
-    return 1
-  fi
-  glob="$1"
+# Finds files under the given base_dir (defaults to PWD)
+# which contain search_term glob. Note that search_term needs to match
+# the entire filename.
+function finds --argument-names search_term base_dir
+    if test -z $search_term
+        echo "Missing search term"
+        echo 'Usage: finds "search string" /base/dir'
+        return 1
+    end
 
-  base_dir="."
-  if [[ -n "$2" ]]; then
-    base_dir="$2"
-  fi
-  if [[ ! -a "${base_dir}" ]]; then
-    echo "'${base_dir}' does not exist"
-    return 2
-  fi
+    if test -z $base_dir
+        set base_dir .
+    end
 
-  echo "glob: ${glob}"
-  echo "base_dir: ${base_dir}"
-  find "${base_dir}" -name "${glob}" -print $argv
+    if not test -e $base_dir
+        echo "'$base_dir' does not exist"
+        return 2
+    end
+
+    echo "search_term: $search_term"
+    echo "base_dir: $base_dir"
+    find $base_dir -name $search_term -print
 end
