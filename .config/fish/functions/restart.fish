87c57b65ad8f78edc9d980771c160c00c9aba2c6
Ben Chatelain <ben@octop.ad>
2017-05-09 09:22:15 -0600

    Update syntax of restart

--- .config/fish/functions/restart.fish
+++ .config/fish/functions/restart.fish
@@ -1,10 +1,10 @@
-# 
+# Restarts the computer without prompt
 function restart
-      if [[ "$(fdesetup isactive)" = "true" ]]; then
-    # FileVault authenticated restart
-    sudo fdesetup authrestart -verbose
-  else
-    # Normal restart
-    sudo shutdown -r now "Rebooting now"
-  fi $argv
+    if test (fdesetup isactive) = "true"
+        # FileVault authenticated restart
+        sudo fdesetup authrestart -verbose
+    else
+        # Normal restart
+        sudo shutdown -r now "Rebooting now"
+    end
 end
