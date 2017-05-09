1dd92628efd1cbf9256f47b88dc9c95be64dd4ad
Ben Chatelain <ben@octop.ad>
2017-05-08 21:10:06 -0600

    Refactor lsym and ldir to use find+ls to preserve coloring

--- .config/fish/functions/ldir.fish
+++ .config/fish/functions/ldir.fish
@@ -1,4 +1,8 @@
 # List dirs
-function ldir
-    ls -ld */ $argv
+function ldir --wraps ls
+    for path in (find . -type d -depth 1)
+        # Strip the preceeding './' off the filename.
+        set -l dir (string split "/" -- $path)[-1]
+        ls -d $dir
+    end
 end
--- .config/fish/functions/lsym.fish
+++ .config/fish/functions/lsym.fish
@@ -1,4 +1,8 @@
-# List symbolic links.
-function lsym
-    la $argv | grep '^l'
+# List symbolic links in the current dir.
+function lsym --wraps ls
+    for path in (find . -type l -depth 1)
+        # Strip the preceeding './' off the link filename.
+        set -l symlink (string split "/" -- $path)[-1]
+        ls -o $symlink
+    end
 end
