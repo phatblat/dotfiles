addcd33d7031e4179eb184dd8841365d2110405e
Ben Chatelain <ben@octop.ad>
2017-05-09 09:24:35 -0600

    Add admin check to restart

--- .config/fish/functions/restart.fish
+++ .config/fish/functions/restart.fish
@@ -1,5 +1,10 @@
 # Restarts the computer without prompt
 function restart
+    if not user_is_admin
+        echo "You must be an admin to run this command."
+        return 1
+    end
+
     if test (fdesetup isactive) = "true"
         # FileVault authenticated restart
         sudo fdesetup authrestart -verbose
