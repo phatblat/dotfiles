9c81e3a1d7b22827425a2432380bc61b10143c54
Ben Chatelain <ben@octop.ad>
2017-05-08 22:10:45 -0600

    Update syntax of gradledebug

--- .config/fish/functions/gradledebug.fish
+++ .config/fish/functions/gradledebug.fish
@@ -1,11 +1,11 @@
-# 
+# Toggle Gradle debugging.
 function gradledebug
-      if [[ -z "${GRADLE_OPTS}" || $1 == "on" ]]; then
-    export GRADLE_OPTS="${GRADLE_HEAP_SPACE} -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
-    echo "Gradle debugging on"
-    echo "GRADLE_OPTS: ${GRADLE_OPTS}"
-  else
-    export GRADLE_OPTS="${GRADLE_HEAP_SPACE}"
-    echo "Gradle debugging off"
-  fi $argv
+    if echo $GRADLE_OPTS | grep debug >/dev/null
+        set --export GRADLE_OPTS $GRADLE_HEAP_SPACE
+        echo "Gradle debugging off"
+    else
+        set --export GRADLE_OPTS "$GRADLE_HEAP_SPACE -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+        echo "Gradle debugging on"
+        echo "GRADLE_OPTS: $GRADLE_OPTS"
+    end
 end
