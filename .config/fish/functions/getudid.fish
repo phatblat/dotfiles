63283e72cf09209fa954b77a801f1c86ef1748f0
Ben Chatelain <ben@octop.ad>
2017-05-08 21:59:39 -0600

    Update syntax of udid

--- .config/fish/functions/getudid.fish
+++ .config/fish/functions/getudid.fish
@@ -1,13 +1,17 @@
-# 
+# Prints and copies the UDID of the connected iOS device.
 function getudid
-      udid=($(system_profiler SPUSBDataType | grep -A 11 -w "iPad\|iPhone\|iPad" | grep "Serial Number" | awk '{ print $3 }'))
-  if [ -z $udid ]; then
-    echo "No device detected. Please ensure an iOS device is plugged in."
-    exit 1
-  else
-    for i in "${udid[@]}"; do
-      echo -n $i | pbcopy
-      echo "UDID: $i"
-    done
-  fi $argv
+    set -l udid (system_profiler SPUSBDataType \
+        | grep -A 11 -w "iPad\|iPhone\|iPad" \
+        | grep "Serial Number" \
+        | awk '{ print $3 }')
+
+    if test -z $udid
+        echo "No device detected. Please ensure an iOS device is plugged in."
+        exit 1
+    else
+        for identifier in $udid
+            echo -n $identifier | pbcopy
+            echo "UDID: $identifier"
+        end
+    end
 end
