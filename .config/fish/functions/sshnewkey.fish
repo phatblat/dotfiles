6a3562d34d8a2ea6c972a5fe059c97799698bbc8
Ben Chatelain <ben@octop.ad>
2017-05-09 15:04:41 -0600

    Convert sshnewkey, use default file path.

--- .config/fish/functions/sshnewkey.fish
+++ .config/fish/functions/sshnewkey.fish
@@ -1,9 +1,12 @@
-# 
-function sshnewkey
-      comment="${USER}@${HOST}"
-  if (($+1)); then
-    comment="$1"
-  fi
+# Generates a new RSA SSH key at ~/.ssh/id_rsa.pub with an optional comment.
+# Comment defaults to $USER@$HOST.
+function sshnewkey --argument-names comment
+    if test -z $comment
+        set comment "$USER@$HOST"
+    end
 
-  ssh-keygen -t rsa -C "${comment}" $argv
+    ssh-keygen \
+        -t rsa \
+        -C $comment \
+        -f ~/.ssh/id_rsa.pub
 end
