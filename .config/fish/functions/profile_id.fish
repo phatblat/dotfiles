d80e7967f7baa8e6db0f6d750d6a80bc92031967
Ben Chatelain <ben@octop.ad>
2017-05-08 22:52:03 -0600

    Update syntax of profile_id

--- .config/fish/functions/profile_id.fish
+++ .config/fish/functions/profile_id.fish
@@ -1,9 +1,11 @@
-# 
-function profile_id
-      if [[ $# -ne 1 ]]; then
-    echo "Usage: profile_id AppProfile.mobileprovision"
-    return 1
-  fi
+# Extracts the UUID from a .mobileprovision profile.
+function profile_id --argument-names profile
+    if test -z $profile
+        echo "Usage: profile_id AppProfile.mobileprovision"
+        return 1
+    end
 
-  egrep -a -A 2 UUID $1 | grep string | sed -e 's/<string>//' -e 's/<\/string>//' -e 's/[ 	]//' $argv
+    egrep -a -A 2 UUID $profile \
+        | grep string \
+        | sed -e 's/<string>//' -e 's/<\/string>//' -e 's/[ 	]//'
 end
