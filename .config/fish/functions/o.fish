5d431c61ab925aa52d75bd5b62434b6725087183
Ben Chatelain <ben@octop.ad>
2017-05-08 22:44:24 -0600

    Update syntax of o

--- .config/fish/functions/o.fish
+++ .config/fish/functions/o.fish
@@ -1,9 +1,9 @@
-# 
-function o
-      if [ -z "$1" ] ; then
-    open .
-  else
-    # -t  Causes the given path to be opened with the default app, as determined via LaunchServices
-    open -t "$*"
-  fi $argv
+# Short alias for open.
+function o --wraps open --argument-names path
+    if test -z $path
+        open .
+    else
+        # -t  Causes the given path to be opened with the default app, as determined via LaunchServices
+        open -t $argv
+    end
 end
