d585820bdc6cf9fdd15ab3d8ec6d42acff6f673c
Ben Chatelain <ben@octop.ad>
2017-05-08 21:28:05 -0600

    Convert bak to fish syntax

--- .config/fish/functions/bak.fish
+++ .config/fish/functions/bak.fish
@@ -1,32 +1,21 @@
-# 
-function bak
-      if [[ ! -a "$1" ]]; then
-    echo "'$1' does not exist"
-    return 1
-  fi
+# Backs up a file by appending .bak extension. When run on a file
+# that already has .bak, the extension is removed.
+function bak --argument-names file
+    if not test -e $file
+        echo "'$file' does not exist"
+        return 1
+    end
 
-  # > h
-  # >      Remove a trailing pathname component, leaving the head.  This
-  # >      works like `dirname'.
-  # >
-  # > r
-  # >      Remove a filename extension of the form `.XXX', leaving the root
-  # >      name.
-  # >
-  # > e
-  # >      Remove all but the extension.
-  # >
-  # > t
-  # >      Remove all leading pathname components, leaving the tail.  This
-  # >      works like `basename'.
-  # http://www.zsh.org/mla/users/2006/msg00239.html
-  if [[ "${1:e}" == "bak" ]]; then
-    # Remove the .bak extension
-    mv "$1" "${1:r}"
-    echo "Renamed to '${1:r}'"
-  else
-    # Append a .bak extension
-    mv "$1" "$1.bak"
-    echo "Renamed to '$1.bak'"
-  fi $argv
+    set -l components (string split "." -- $file)
+
+    if test $components[-1] = "bak"
+        # Remove the .bak extension
+        set new_name (string join "." $components[1..-2])
+        mv $file $new_name
+        echo "Renamed to '$new_name'"
+    else
+        # Append a .bak extension
+        mv $file $file.bak
+        echo "Renamed to '$file.bak'"
+    end
 end
