9c0bd9e3de73a8dce0cd459a572ebc0d4f757328
Ben Chatelain <ben@octop.ad>
2017-05-08 22:02:50 -0600

    Update syntax of gpgcopypub

--- .config/fish/functions/gpgcopypub.fish
+++ .config/fish/functions/gpgcopypub.fish
@@ -1,7 +1,7 @@
-# 
+# Copies the public key for any GPG key found.
 function gpgcopypub
-      keyid=$(gpgkeyid)
-  gpg_key_ascii=$(gpg --armor --export ${keyid})
-  echo ${gpg_key_ascii} | pbcopy
-  echo "GPG key copied to pasteboard (keyid: ${keyid})" $argv
+    set -l keyid (gpgkeyid)
+    set -l gpg_key_ascii (gpg --armor --export $keyid)
+    echo $gpg_key_ascii | pbcopy
+    echo "GPG key copied to pasteboard (keyid: $keyid)" $argv
 end
--- .config/fish/functions/gpgkeyid.fish
+++ .config/fish/functions/gpgkeyid.fish
@@ -1,5 +1,6 @@
-# 
+# Prints the long format key identifiers of all GPG keys found.
 function gpgkeyid
-    gpg --list-keys --keyid-format long | egrep -o "^pub.*/\w+" | cut -d "/" -f 2 $argv
+    gpg --list-keys --keyid-format long \
+        | egrep -o "^pub.*/\w+" \
+        | cut -d "/" -f 2 $argv
 end
-
