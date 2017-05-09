4cabb06302c52b9e3b43518b234a11ff7b4f35c9
Ben Chatelain <ben@octop.ad>
2017-05-09 09:43:36 -0600

    Add 'both' option to rewrite function

--- .config/fish/functions/rewrite.fish
+++ .config/fish/functions/rewrite.fish
@@ -10,6 +10,9 @@ function rewrite --argument-names field old_email new_email
         set attribute "GIT_AUTHOR_EMAIL"
     case "committer"
         set attribute "GIT_COMMITTER_EMAIL"
+    case "both"
+        # Recursive call replacing author first, then committer
+        set attribute "GIT_AUTHOR_EMAIL"
     case '*'
         echo "Usage: rewrite author|committer old@email new@email"
         return 2
@@ -19,4 +22,9 @@ function rewrite --argument-names field old_email new_email
     set -l filter_command "if [[ \$$attribute == $old_email ]]; then $attribute=$new_email; fi; export $attribute"
 
     git filter-branch -f --env-filter $filter_command
+
+    # Recursive call replacing author first, then committer
+    if test $field = "both"
+        rewrite "committer" $old_email $new_email
+    end
 end
