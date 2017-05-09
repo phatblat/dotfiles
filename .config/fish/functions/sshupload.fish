4d40c7f4328b1f3c5b329bcb1d861ba83a591f47
Ben Chatelain <ben@octop.ad>
2017-05-09 15:11:08 -0600

    Convert sshupload

--- .config/fish/functions/sshupload.fish
+++ .config/fish/functions/sshupload.fish
@@ -1,18 +1,22 @@
-# 
-function sshupload
-      keyfile="~/.ssh/id_rsa.pub"
-  if (($+1)); then
-    keyfile="$1"
-  fi
+# Uploads public RSA SSH key to GitHub profile.
+# Requires manual entry of GitHub OTP code..
+function sshupload --argument-names keyfile
+    if test -z $keyfile
+        set keyfile ~/.ssh/id_rsa.pub
+    end
 
-  # Upload default SSH key to GitHub
-  echo "Uploading SSH public key to GitHub [${keyfile}]"
-  echo -n "GitHub OTP code: "
-  read otpcode
-  curl -X POST \
-    --user "phatblat" \
-    --header "X-GitHub-OTP: ${otpcode}" \
-    --data "{\"title\":\"${USER}@${HOST}_$(date +%Y%m%d%H%M%S)\",\"key\":\"$(cat $keyfile)\"}" \
-    --verbose \
-    "https://api.github.com/user/keys" $argv
+    set -l key (cat $keyfile)
+    set -l title $USER@$HOST_(date +%Y%m%d%H%M%S)
+
+    # Upload default SSH key to GitHub
+    echo "Uploading SSH public key to GitHub [$keyfile]"
+    echo -n "GitHub OTP code: "
+    read otpcode
+
+    curl -X POST \
+        --user "phatblat" \
+        --header "X-GitHub-OTP: $otpcode" \
+        --data "{\"title\":\"$title\",\"key\":\"$key\"" \
+        --verbose \
+        "https://api.github.com/user/keys"
 end
