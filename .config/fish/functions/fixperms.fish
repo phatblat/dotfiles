22fe2f86855a80f742711be31ee795929d818864
Ben Chatelain <ben@octop.ad>
2017-05-08 21:56:32 -0600

    Update syntax of fixperms

--- .config/fish/functions/fixperms.fish
+++ .config/fish/functions/fixperms.fish
@@ -1,5 +1,9 @@
-# 
-function fixperms
-      find "$1" -type f -print -exec chmod 644 {} \;
-  find "$1" -type d -print -exec chmod 755 {} \; $argv
+# Sets all file and directory permissions to 644 and 755, respectively.
+function fixperms --argument-names base_dir
+    if test -z $base_dir
+        set base_dir .
+    end
+
+    find $base_dir -type f -print -exec chmod 644 {} \;
+    find $base_dir -type d -print -exec chmod 755 {} \;
 end
