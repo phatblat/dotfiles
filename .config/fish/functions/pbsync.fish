2bc3fbc1f8d144c3c96232cd2e89470ddf171b27
Ben Chatelain <ben@octop.ad>
2017-05-08 22:49:42 -0600

    Update syntax of psync

--- .config/fish/functions/pbsync.fish
+++ .config/fish/functions/pbsync.fish
@@ -1,4 +1,4 @@
-# 
+# Initiates a sync of phatblat user directory between external and internal storage.
 function pbsync
-      psync $phatblat_external $phatblat_imac "go" $argv
+    psync $phatblat_external $phatblat_imac "go"
 end
--- .config/fish/functions/psync.fish
+++ .config/fish/functions/psync.fish
@@ -1,22 +1,21 @@
-# 
-function psync
-      if (( $# < 2 )); then
-    echo "Usage: sync source_dir/ destination_dir/"
-    return 1
-  fi
+# Syncs files between two directories. Without a 3rd argument, only does a dry run.
+function psync --argument-names source_dir destination_dir dry_run
+    if test -z $source_dir -o -z $destination_dir
+        echo "Usage: sync source_dir/ destination_dir/"
+        return 1
+    end
 
-  local source_dir="$1"
-  local destination_dir="$2"
-
-  if [[ $3 == "go" ]]; then
-    # Output is suppressed when not connected to TTY
-    [ -t 0 ] && echo "rsyncing ${source_dir} -> ${destination_dir}"
-    mkdir -p "${destination_dir}"
-    # Run (-aP)
-    rsync --archive --one-file-system --safe-links --partial --delete-after "${source_dir}" "${destination_dir}"
-  else
-    echo "rsyncing ${source_dir} -> ${destination_dir} (dry run)"
-    # Test (-anv)
-    rsync --archive --one-file-system --safe-links --verbose --dry-run "${source_dir}" "${destination_dir}"
-  fi $argv
+    if test $dry_run = "go"
+        # Output is suppressed when not connected to TTY
+        [ -t 0 ]; and echo "rsyncing ${source_dir} -> ${destination_dir}"
+        mkdir -p $destination_dir
+        # Run (-aP)
+        rsync --archive --one-file-system --safe-links --partial --delete-after \
+             $source_dir $destination_dir
+    else
+        echo "rsyncing $source_dir -> $destination_dir (dry run)"
+        # Test (-anv)
+        rsync --archive --one-file-system --safe-links --verbose --dry-run \
+            $source_dir $destination_dir
+    end
 end
