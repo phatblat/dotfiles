099107d73c4b228fb0618999639f1c9fb2403ab4
Ben Chatelain <ben@octop.ad>
2017-05-10 17:31:49 -0600

    Chain fe to fn when function doesn't exist yet

--- .config/fish/functions/fe.fish
+++ .config/fish/functions/fe.fish
@@ -1,5 +1,11 @@
 # Edit a function.
 function fe --argument-names function_name
+    if not functions --query $function_name
+        yn $function_name" doesn't exist. Create?"
+            and fn $function_name
+        return
+    end
+
     set old_VISUAL $VISUAL
     set VISUAL $VISUAL" -w"
 
