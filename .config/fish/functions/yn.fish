c3746f72f09cadd9907e606125cf02f945c92de1
Ben Chatelain <ben@octop.ad>
2017-05-10 15:37:47 -0600

    Add yn function

---
+++ .config/fish/functions/yn.fish
@@ -0,0 +1,15 @@
+# Prompt the user for a yes/no response. Returns 0 (success) for YES and 1 (error) for NO.
+# Plugin Dependency: get
+function yn --argument-names prompt
+    get --prompt $prompt --default="y" \
+        | read -l answer
+
+    switch $answer
+        case "y" "yes" "Y" "YES"
+            echo "YES"
+            return 0
+        case "n" "no" "N" "NO"
+            echo "NO"
+            return 1
+    end
+end
