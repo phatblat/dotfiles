ec51861b50b360b4aa119a8b5677049b255edfc0
Ben Chatelain <ben@octop.ad>
2017-05-10 11:18:06 -0600

    Refactor fs to preserve doc comments, remove tab and print result

--- .config/fish/functions/fs.fish
+++ .config/fish/functions/fs.fish
@@ -12,14 +12,33 @@ function fs --argument-names function_name
                 set comment_lines $comment_lines $line
             end
         end
-        echo comments:\n$comment_lines
+        # echo comments:\n$comment_lines
     end
 
     funcsave $function_name
 
     if test -n $comment_lines
-        set -l new_contents $comment_lines (cat $dest_file)
-        echo $new_contents\n >$dest_file
-        echo "Comments restored"
+        # Read in the new function definition
+        set -l new_contents (cat $dest_file)
+
+        # Wipe out the file with the original comments
+        echo -n $comment_lines\n >$dest_file
+
+        # Filter the new content to fix indentation
+        for line in $new_contents
+            set -l first_char (string sub --length 1 $line)
+            set -l all_after_first_char (string sub --start 1 $line)
+
+            if test \t = $first_char
+                # Skip the tab on the first line of the function body
+                # Somehow the \t takes up 2 chars?
+                echo "    "(string sub --start 2 $line) >>$dest_file
+            else
+                echo $all_after_first_char >>$dest_file
+            end
+        end
+        # echo "Comments restored"
     end
+
+    cat $dest_file
 end
