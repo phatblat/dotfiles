96a9bc3a54c2baff4a2c09640bf912fb8faef734
Ben Chatelain <ben@octop.ad>
2017-05-10 10:43:29 -0600

    Add fd

---
+++ .config/fish/functions/fd.fish
@@ -0,0 +1,12 @@
+# Delete a function
+function fd --argument function_name
+	if not functions --query $function_name
+        echo "Function "$function_name" does not exist."
+        return 1
+    end
+
+    functions --erase $function_name
+    and echo "Function "$function_name" deleted."
+
+    set VISUAL $old_VISUAL
+end
