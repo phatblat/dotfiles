aea2beea75138c73fe5d1a0169c921c420d40071
Ben Chatelain <ben@octop.ad>
2017-05-10 10:06:35 -0600

    Fix fe argument and toggle VISUAL to make shell wait for edits to finish

--- .config/fish/functions/fe.fish
+++ .config/fish/functions/fe.fish
@@ -1,5 +1,9 @@
 # Edit a function.
 function fe --argument-names function_name
+    set old_VISUAL $VISUAL
     set VISUAL $VISUAL" -w"
-    funced function_name
+
+    funced $function_name
+
+    set VISUAL $old_VISUAL
 end
