272a684e67060bcdc20f17dcf8e9fc2c022b7544
Ben Chatelain <ben@octop.ad>
2017-05-09 19:52:54 -0600

    Chain cron_list at the end of cron_reload

--- .config/fish/functions/cron_reload.fish
+++ .config/fish/functions/cron_reload.fish
@@ -1,4 +1,8 @@
 # Reload Cron file.
 function cron_reload
     crontab ~/.dotfiles/cron/cron.file
+
+    # Show the newly loaded job configuartion
+    echo "Cron file reloaded:"
+    cron_list
 end
