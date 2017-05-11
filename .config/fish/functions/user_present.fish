dd5d93bbcd6d50c3bc3470c9529047d04c209c0c
Ben Chatelain <ben@octop.ad>
2017-05-11 00:03:36 -0600

    Add user_present

---
+++ .config/fish/functions/user_present.fish
@@ -0,0 +1,13 @@
+# Indicates whether a user is present
+function user_present
+    if begin status is-login
+            and status is-interactive
+            and status is-interactive-job-control
+            # and not status is-command-substitution
+            # and not status is-block
+        end
+        return 0
+    else
+        return 1
+    end
+end
