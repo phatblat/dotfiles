2c5976a1eb6b24fa56810c008579134c6c6a9a30
Ben Chatelain <ben@octop.ad>
2017-05-11 10:57:16 -0600

    [xccheck] Check for $app_dir existence to prevent errors

--- .config/fish/functions/xccheck.fish
+++ .config/fish/functions/xccheck.fish
@@ -7,6 +7,7 @@ function xccheck
         "/Volumes/Thunderbay/Applications" \
 
     for app_dir in $app_dirs
+        test -e $app_dir; or continue
         for xcode_version in (ls -d $app_dir/Xcode*.app)
             echo "Checking integrity of $xcode_version"
             spctl --assess --verbose "$xcode_version"
