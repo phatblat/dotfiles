8e184e227f1f1c70b2c869120def90c0f9338245
Ben Chatelain <ben@octop.ad>
2017-05-11 17:44:43 -0600

    [fn] Use fish_indent to format self and new functions created

--- .config/fish/functions/fn.fish
+++ .config/fish/functions/fn.fish
@@ -1,29 +1,30 @@
 # Create a new function.
-function fn --argument-names function_name
+function fn --argument function_name
     if functions --query $function_name
         yn $function_name" already exists. Edit?"
-            and fe $function_name
+        and fe $function_name
         return
     end
 
     set -l file ~/.config/fish/functions/$function_name.fish
 
     # Function template
-    echo \
-"# $function_name
-function $function_name --argument-names arg1
-    if test -z \$arg1
-        echo \"Usage: $function_name arg1\"
-        return 1
-    end
-    switch \$arg1
-        case on ON On
-        case off OFF Off
-        case '*'
-    end
-end"\
-    >$file
+    echo "# $function_name
+ function $function_name --argument-names arg1
+     if test -z \$arg1
+         echo \"Usage: $function_name arg1\"
+         return 1
+     end
+     switch \$arg1
+         case on ON On
+         case off OFF Off
+         case '*'
+     end
+ end"\
+ >$file
 
     editw $file
+    fish_indent $file
+
     echo $function_name function created.
 end
