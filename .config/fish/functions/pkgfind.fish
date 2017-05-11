35fbf13edba2d2bae9dd8478057aa219cb58cfb3
Ben Chatelain <ben@octop.ad>
2017-05-11 17:26:51 -0600

    Add pkgfind, pkginfo and pkgfiles

---
+++ .config/fish/functions/pkgfind.fish
@@ -0,0 +1,9 @@
+# Scans through installer package identifiers for the given name.
+function pkgfind --argument-names filter
+    if test -z $filter
+        echo "Usage: pkgfind name"
+        return 1
+    end
+
+    pkgutil --packages | grep $filter
+end
