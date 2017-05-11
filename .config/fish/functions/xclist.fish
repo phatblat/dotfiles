dc3016caf0c362f6e150473c720890e8ecadca4d
Ben Chatelain <ben@octop.ad>
2017-05-11 12:20:11 -0600

    [xclist] Update syntax

--- .config/fish/functions/xclist.fish
+++ .config/fish/functions/xclist.fish
@@ -1,11 +1,13 @@
-# 
+# Prints a clean list of paths for all installed versions of Xcode
 function xclist
-      local xcode filename
+    set -l app_dirs /Applications ~/Applications
+    set -l xcodes
 
-  # echo /Applications/Xcode*.app
-  for xcode in /Applications/Xcode*.app; do
-    # echo $xcode
-    filename=$(basename $xcode)
-    echo ${filename%.*};
-  done $argv
+    for app_dir in $app_dirs
+        set xcodes $xcodes $app_dir/Xcode*.app
+    end
+
+    for xcode_version in $xcodes
+        string replace '.app' '' (basename $xcode_version)
+    end
 end
