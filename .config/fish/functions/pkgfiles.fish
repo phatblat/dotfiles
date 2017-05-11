d3ff467fbc19272597d0963e18f709cd491a2785
Ben Chatelain <ben@octop.ad>
2017-05-11 17:26:51 -0600

    Add pkgfind, pkginfo and pkgfiles

---
+++ .config/fish/functions/pkgfiles.fish
@@ -0,0 +1,9 @@
+# Shows files associated with the given installer package.
+function pkgfiles --argument-names package_identifier
+    if test -z $package_identifier
+        echo "Usage: pkgfiles package_identifier"
+        return 1
+    end
+
+    pkgutil --files $package_identifier
+end
