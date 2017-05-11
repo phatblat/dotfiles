8307a5b9beb437113b65eb23a9c1e1b86d1de714
Ben Chatelain <ben@octop.ad>
2017-05-11 12:17:37 -0600

    [reload] Add optional arg for function to reload

--- .config/fish/functions/reload.fish
+++ .config/fish/functions/reload.fish
@@ -1,4 +1,13 @@
-# Reloads fish configuration.
-function reload
-    source ~/.config/fish/config.fish
+# Reloads fish configuration or a single autoloaded function.
+function reload --argument-names function_name
+    if test -z $function_name
+        source ~/.config/fish/config.fish
+        return
+    else if functions --query $function_name
+        source ~/.config/fish/functions/$function_name.fish
+        func $function_name
+    else
+        echo "$function_name does not exist in function autoload dir."
+        return 1
+    end
 end
