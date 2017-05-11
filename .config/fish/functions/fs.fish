d0f0ec5be3d318f5abfcd1c72846cbb849ecf8fd
Ben Chatelain <ben@octop.ad>
2017-05-11 17:43:39 -0600

    Use fish_indent to fix funcsave output

--- .config/fish/functions/fs.fish
+++ .config/fish/functions/fs.fish
@@ -1,5 +1,5 @@
 # Save a function to user's autoload dir.
-function fs --argument-names function_name
+function fs --argument function_name
     set -l dest_file ~/.config/fish/functions/$function_name.fish
     set -l original_contents
     set -l comment_lines
@@ -12,7 +12,6 @@ function fs --argument-names function_name
                 set comment_lines $comment_lines $line
             end
         end
-        # echo comments:\n$comment_lines
     end
 
     funcsave $function_name
@@ -23,22 +22,11 @@ function fs --argument-names function_name
 
         # Wipe out the file with the original comments
         echo -n $comment_lines\n >$dest_file
-
-        # Filter the new content to fix indentation
-        for line in $new_contents
-            set -l first_char (string sub --length 1 $line)
-            set -l all_after_first_char (string sub --start 1 $line)
-
-            if test \t = $first_char
-                # Skip the tab on the first line of the function body
-                # Somehow the \t takes up 2 chars?
-                echo "    "(string sub --start 2 $line) >>$dest_file
-            else
-                echo $all_after_first_char >>$dest_file
-            end
-        end
-        # echo "Comments restored"
+        echo -n $new_contents\n >>$dest_file
     end
+
+    # Fix the funky indentation (tab on first line of body, extra space on all)
+    fish_indent --write $dest_file
 
     cat $dest_file
 end
