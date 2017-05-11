23eaea9fb96e7d4902282670c958730c68f6ee91
Ben Chatelain <ben@octop.ad>
2017-05-11 17:36:23 -0600

    Remove quotes from empty arg test

--- .config/fish/functions/fn.fish
+++ .config/fish/functions/fn.fish
@@ -12,7 +12,7 @@ function fn --argument-names function_name
     echo \
 "# $function_name
 function $function_name --argument-names arg1
-    if test -z \"\$arg1\"
+    if test -z \$arg1
         echo \"Usage: $function_name arg1\"
         return 1
     end
