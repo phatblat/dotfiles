68f39a26b4088a635461327ba8cfdc67a6c51458
Ben Chatelain <ben@octop.ad>
2017-05-11 15:30:48 -0600

    [xcswitch] Fix issues where symlink was getting created incorrectly
    Was ending up inside Xcode.app bundle, which fouled up xccheck

--- .config/fish/functions/xcswitch.fish
+++ .config/fish/functions/xcswitch.fish
@@ -6,7 +6,7 @@ function xcswitch --argument-names version
         return 1
     end
 
-    set -l xcode_path "/Applications/Xcode-$version.app"
+    set -l xcode_path /Applications/Xcode-$version.app
     if not test -e $xcode_path
         echo $xcode_path does not exist.
         return 2
@@ -19,8 +19,10 @@ function xcswitch --argument-names version
 
     # Creat a symlink so tools, like SwiftLint, that assume Xcode was
     # Installed via the MAS use the correct version.
-    ln -fsv $xcode_path /Applications/Xcode.app
-    xclist
+    pushd /Applications
+    ln -fhs (basename $xcode_path) Xcode.app
+    popd
 
+    xclist
     echo "Activated "(string sub --length (string length $xcode_path) (xcsp))
 end
