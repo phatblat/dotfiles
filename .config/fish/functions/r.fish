00d1bbcdf9fd0aceae24d709b729d8451285288c
Ben Chatelain <ben@octop.ad>
2017-05-10 20:49:22 -0600

    Fix r function

--- .config/fish/functions/r.fish
+++ .config/fish/functions/r.fish
@@ -1,10 +1,8 @@
 # Interactive rebase for the last few commits, count specified as 1st arg (default: 10).
-function r
-    echo $argv | read -l count
-
+function r --argument-names count
     if test -z $count
         set count 10
     end
 
-    git rebase --interactive HEAD~$count $argv
+    git rebase --interactive HEAD~$count
 end
