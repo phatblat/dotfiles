7f8311fc84e219fea6732a61dd12127de107b975
Ben Chatelain <ben@octop.ad>
2017-05-11 15:51:04 -0600

    [bootstrap] Remove * exclusion

--- .dotfiles/install/bootstrap.sh
+++ .dotfiles/install/bootstrap.sh
@@ -38,10 +38,6 @@ pushd dotfiles
 # Change remote URL to use SSH
 git remote set-url origin git@github.com:phatblat/dotfiles.git
 
-# Ignore all files by default - this makes git status output quieter.
-# Adding new files requires --force.
-echo '*' >> .git/info/exclude
-
 # Copy Dotfiles repo into $HOME
 # http://superuser.com/questions/61611/how-to-copy-with-cp-to-include-hidden-files-and-hidden-directories-and-their-con
 shopt -s dotglob
