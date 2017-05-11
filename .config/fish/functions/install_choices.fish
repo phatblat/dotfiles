f3d457ae382b855bb256323537470a0bb5a05637
Ben Chatelain <ben@octop.ad>
2017-05-11 17:05:54 -0600

    Add install_choices

---
+++ .config/fish/functions/install_choices.fish
@@ -0,0 +1,15 @@
+# Prints the choices available in the given installation package.
+function install_choices --argument-names package_file
+    if test -z $package_file
+        echo "Usage: install_choices file.pkg"
+        return 1
+    else if not string match '*.pkg' $package_file >/dev/null
+        echo "This command requires an installer package (.pkg) file."
+        return 2
+    else if not test -e $package_file
+        echo "$package_file does not exist."
+        return 3
+    end
+
+    installer -showChoicesXML -pkg $package_file
+end
