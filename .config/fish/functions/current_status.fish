3eace5b91e20c60c2be8cf11367930e37ee284e9
Ben Chatelain <ben@octop.ad>
2017-05-11 11:07:33 -0600

    Add example status sets to current_status docs

--- .config/fish/functions/current_status.fish
+++ .config/fish/functions/current_status.fish
@@ -1,4 +1,14 @@
 # Debugging for the status function.
+#
+# User:
+# - is-login
+# - is-interactive
+# - is-block
+# - is-interactive-job-control
+#
+# Cron:
+# - is-block
+# - is-interactive-job-control
 function current_status
     set -l flags is-login is-interactive is-block is-command-substitution is-no-job-control is-full-job-control is-interactive-job-control
 
