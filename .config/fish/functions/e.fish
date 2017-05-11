5a90d9268d67e2d94bb2e08150d17cb30bda799f
Ben Chatelain <ben@octop.ad>
2017-05-10 23:59:31 -0600

    FIx errors from e by quoting argv

--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,7 +1,7 @@
 # Short alias for editing a file. Given no args, the current folder
 # will be opened in the default text editor.
 function e
-    if test -z $argv
+    if test -z "$argv"
         edit .
     else
         edit $argv
