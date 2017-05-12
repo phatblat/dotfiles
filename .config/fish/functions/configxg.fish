6d91b2d45cac59917af3b3a98815f629cc4eb1d6
Ben Chatelain <ben@octop.ad>
2017-05-12 16:15:59 -0600

    Add configg and configxg

---
+++ .config/fish/functions/configxg.fish
@@ -0,0 +1,5 @@
+# Manage extended global git configuration ($XDG_CONFIG_HOME/git/config -> ~/.config/git/config).
+function configxg
+    set -l global_config ~/.config/git/config
+    git config --file $global_config $argv
+end
