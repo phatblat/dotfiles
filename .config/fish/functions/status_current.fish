c8079df15f90fde3db6ef7ac32f45d9d26bc001c
Ben Chatelain <ben@octop.ad>
2017-05-12 10:03:12 -0600

    Rename status_current

--- .config/fish/functions/status_current.fish
+++ .config/fish/functions/status_current.fish
@@ -6,10 +6,15 @@
 # - is-block
 # - is-interactive-job-control
 #
+# su user (admin)
+# - is-interactive
+# - is-block
+# - is-full-job-control
+#
 # Cron:
 # - is-block
 # - is-interactive-job-control
-function current_status
+function status_current
     set -l flags is-login is-interactive is-block is-command-substitution is-no-job-control is-full-job-control is-interactive-job-control
 
     for flag in $flags
