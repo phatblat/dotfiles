219bb2abf04018bd83a6cef7de72885bd90653ba
Ben Chatelain <ben@octop.ad>
2017-05-12 16:00:56 -0600

    [upstall] Check global git config user name and email

--- .config/fish/functions/🗄__gitconfig.fish
+++ .config/fish/functions/🗄__gitconfig.fish
@@ -11,7 +11,7 @@ function 🗄__gitconfig --argument-names email name
     set -l global_config ~/.config/git/config
 
     # Only print config when values are set.
-    if test -n (git config user.name); and test -n (git config user.email)
+    if test -n (git config --global user.name); and test -n (git config --global user.email)
         cat $global_config
         return
     end
