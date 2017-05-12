d49c2daf80299b4ac9eaa82db8a073dd4ceb7424
Ben Chatelain <ben@octop.ad>
2017-05-11 20:46:52 -0600

    Chain _gitconfig onto _install

--- .config/fish/functions/_install.fish
+++ .config/fish/functions/_install.fish
@@ -1,6 +1,16 @@
 # Installs and updates system and shell dependencies.
 function _install
     repeatchar -
+
+    # Only prompt for user info when not already set
+    if test -z (git config user.email)
+        _gitconfig
+    else
+        echo "Git configuration"
+        cat ~/.config/git/config
+    end
+
+    repeatchar -
     ♻️_fisherman
     repeatchar -
     ♻️_tmbundles
