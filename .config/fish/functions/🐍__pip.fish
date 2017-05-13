19963c11bda4e23938b995a785c3b2784d5de216
Ben Chatelain <ben@octop.ad>
2017-05-13 16:40:40 -0600

    [upstall] Add 🐍__pip

---
+++ .config/fish/functions/🐍__pip.fish
@@ -0,0 +1,25 @@
+# Manages python packages using pip (https://pip.pypa.io/en/stable/).
+# Python Package Index (PyPI) https://pypi.python.org/pypi
+#
+# Installs Powerline for Vim
+# https://powerline.readthedocs.io/en/latest/
+#
+# Sequencing
+# - After: brew (installed with python)
+function 🐍__pip
+    echo "🐍__  PIP"
+    echo
+
+    set -l global_packages powerline-status Pygments
+
+    # Update pip
+    pip install --upgrade pip
+
+    # TODO: Uninstall packages
+    # TODO: Only install missing packages
+    # Install packages
+    pip install $global_packages
+
+    # List installed packages
+    pip list
+end
