0d0b49d16bdfcd03e3911aeba36201635dd27e26
Ben Chatelain <ben@octop.ad>
2017-05-13 12:39:26 -0600

    [npm] Use update instead of reinstalling packages

--- .config/fish/functions/🕸__npm.fish
+++ .config/fish/functions/🕸__npm.fish
@@ -1,14 +1,31 @@
 # Installs and updates npm modules.
+# https://docs.npmjs.com/cli
 function 🕸__npm
     echo "🕸  NPM"
     echo
 
     set -l global_packages fast-cli n ralio
 
+    set -l installed_packages (npm list -g --depth=0)
+    # Example output:
+    # /usr/local/lib
+    # ├── fast-cli@0.2.1
+    # ├── n@2.1.7
+    # ├── npm@4.5.0
+    # └── ralio@0.6.1
+    # Manual installation check
+    # string match "*ralio*" $installed_packages[5]
+
     echo "Updating NPM"
     npm install npm@latest -g
 
-    npm install --global $global_packages
+    # TODO: Add package uninstall
+
+    # Update
+    npm update -g
+
+    # TODO: Refactor to only install missing packages
+    #npm install --global $global_packages
 
     npm doctor
 end
