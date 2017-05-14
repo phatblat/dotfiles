6f6c29183be575fa83812c31c062bfba148a7386
Ben Chatelain <ben@octop.ad>
2017-05-13 20:02:55 -0600

    Add 🖥__macos

--- .config/fish/functions/⏫__upstall.fish
+++ .config/fish/functions/⏫__upstall.fish
@@ -9,7 +9,7 @@ function ⏫__upstall
     date_iso8601 >$last_ran_file
 
     if contains -- --nothing $argv
-        set argv $argv --no-ruby --no-xcode --no-brew --no-cask --no-fisherman --no-pip --no-npm --no-textmate
+        set argv $argv --no-ruby --no-xcode --no-brew --no-cask --no-fisherman --no-pip --no-npm --no-textmate --no-macos
     end
 
     ⬆️__upmodule 🗄__gitconfig
@@ -21,4 +21,5 @@ function ⏫__upstall
     ⬆️__upmodule 🐍__pip         "🐍  PIP"       --no-pip        --nopy $argv
     ⬆️__upmodule 🕸__npm         "🕸  NPM"       --no-npm        --nojs $argv
     ⬆️__upmodule 📝__textmate    "📝  TextMate"  --no-textmate   --notm $argv
+    ⬆️__upmodule 🖥__macos       "🖥  macOS"     --no-macos      --noos $argv
 end
---
+++ .config/fish/functions/🖥__macos.fish
@@ -0,0 +1,31 @@
+# Manage macOS system updates.
+function 🖥__macos
+    echo "🖥  macOS"
+    echo
+
+    # echo "Updating  system software"
+
+    softwareupdate --list
+
+    # Example output:
+    # Software Update Tool
+    #
+    # Finding available software
+    # Software Update found the following new or updated software:
+    #    * MacBookAirEFIUpdate2.4-2.4
+    #         MacBook Air EFI Firmware Update (2.4), 3817K [recommended] [restart]
+    #    * ProAppsQTCodecs-1.0
+    #         ProApps QuickTime codecs (1.0), 968K [recommended]
+    #    * JavaForOSX-1.0
+    #         Java for OS X 2012-005 (1.0), 65288K [recommended]
+    #-----
+    # Software Update Tool
+    #
+    # Finding available software
+    # No new software available.
+
+    # Combine all switches?
+    # softwareupdate --list --download --all
+    # TODO: Need sudo to install
+    # softwareupdate --install --all
+end
