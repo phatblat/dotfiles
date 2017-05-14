58865bee738835f7167d65e8dea5d2e7b3a5f06d
Ben Chatelain <ben@octop.ad>
2017-05-14 14:41:31 -0600

    Add col1

---
+++ .config/fish/functions/col1.fish
@@ -0,0 +1,10 @@
+# Prints the first column of input (first argument)
+function col1 --argument-names arg1
+    if test -z $arg1
+        echo "Usage: col1 arg1 ..."
+        return 1
+    end
+
+    # cut is used since list values can contain spaces
+    echo $arg1 | cut -f 1 -d ' ' -
+end
