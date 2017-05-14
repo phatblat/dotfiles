53e1f0fa0aaac728ded8f423451d83bcc072ee70
Ben Chatelain <ben@octop.ad>
2017-05-14 12:29:52 -0600

    [fd] Check for file before reporting that function doesn't exist

--- .config/fish/functions/fd.fish
+++ .config/fish/functions/fd.fish
@@ -1,11 +1,11 @@
 # Delete a function
 function fd --argument function_name
-	if not functions --query $function_name
+    set -l file ~/.config/fish/functions/$function_name.fish
+
+    if not functions --query $function_name; and not test -e $file
         echo "Function "$function_name" does not exist."
         return 1
     end
-
-    set -l file ~/.config/fish/functions/$function_name.fish
 
     functions --erase $function_name
     # Delete file if it exists
