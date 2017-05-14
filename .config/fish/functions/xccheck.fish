37698e97f1863984c58a944af94aea0f9acdc55f
Ben Chatelain <ben@octop.ad>
2017-05-14 11:09:30 -0600

    [xccheck] Use xclist to provide paths to Xcode app bundles

--- .config/fish/functions/xccheck.fish
+++ .config/fish/functions/xccheck.fish
@@ -1,13 +1,8 @@
 # Validates Xcode application binary integrity using `spctl`
 # Migrated from ~/.dotfiles/xcode/xccheck.sh
 function xccheck
-    set -l app_dirs /Applications ~/Applications
-
-    for app_dir in $app_dirs
-        test -e $app_dir; or continue
-        for xcode_version in (ls -d $app_dir/Xcode*.app)
-            echo "Checking integrity of $xcode_version"
-            spctl --assess --verbose "$xcode_version"
-        end
+    for xcode_version in (xclist -1)
+        echo "Checking integrity of $xcode_version"
+        spctl --assess --verbose "$xcode_version"
     end
 end
