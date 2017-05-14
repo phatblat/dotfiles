036a19ca8dd9e02f4c938ec0b79848808163734a
Ben Chatelain <ben@octop.ad>
2017-05-14 11:52:55 -0600

    [setjdk] Show downgrade example

--- .config/fish/functions/setjdk.fish
+++ .config/fish/functions/setjdk.fish
@@ -1,7 +1,7 @@
 # Update JAVA_HOME to point to the given JDK version.
 function setjdk --argument-names version
     if test -z $version
-        echo "Usage: setjdk 1.8"
+        echo "Usage: setjdk 1.8 (or) setjdk 1.8.0_111"
         return 1
     end
 
