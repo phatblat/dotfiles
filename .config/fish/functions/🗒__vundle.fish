37a75db1fb705ec5aa0534e9f5d3040daf2d4edc
Ben Chatelain <ben@octop.ad>
2017-05-15 14:35:45 -0600

    Add 🗒__vundle

---
+++ .config/fish/functions/🗒__vundle.fish
@@ -0,0 +1,22 @@
+# Installs and updates Vundle, plugin manager for Vim.
+# https://github.com/VundleVim/Vundle.vim#about
+#
+# Sequencing
+# - After: brew (installed with node)
+function 🗒__vundle
+    echo "🗒  Vundle"
+    echo
+
+    set -l vim_bundle_dir ~/.vim/bundle
+    set -l vundle_dir $vim_bundle_dir/Vundle.vim
+
+    if not test -e $vundle_dir
+        git clone git@github.com:VundleVim/Vundle.vim.git $vundle_dir
+    else
+        pushd $vundle_dir
+        git pull
+        popd
+    end
+
+    vim +PluginInstall +qall
+end
