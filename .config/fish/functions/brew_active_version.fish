947f9e844eb375ed248d7dc566300be925da8f56
Ben Chatelain <ben@octop.ad>
2017-05-15 16:32:39 -0600

    Add brew_active_version

---
+++ .config/fish/functions/brew_active_version.fish
@@ -0,0 +1,9 @@
+# brew_active_version
+function brew_active_version --argument-names formula
+    if test -z "$formula"
+        echo "Usage: brew_active_version formula"
+        return 1
+    end
+
+    string replace --all '"' '' (bq $formula | jq .[0].linked_keg)
+end
