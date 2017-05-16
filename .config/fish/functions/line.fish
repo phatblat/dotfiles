d6381e8f83dd6424945f58c0378b1ea4c73a3c54
Ben Chatelain <ben@octop.ad>
2017-05-15 18:49:55 -0600

    Add line

---
+++ .config/fish/functions/line.fish
@@ -0,0 +1,9 @@
+# Extracts a single line of stdin. With no arguments, the first line is used.
+# https://github.com/fish-shell/fish-shell/issues/206#issuecomment-255232968
+function line --argument-names line_number
+    if test -z "$line_number"
+        set line_number 1
+    end
+
+    cat 1>| tail -n +$line_number | head -1
+end
