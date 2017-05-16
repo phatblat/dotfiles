38201f052d5f097f04f4f78cab03496475ae426f
Ben Chatelain <ben@octop.ad>
2017-05-16 10:14:09 -0600

    [plcat] Check that valid file argument was given

--- .config/fish/functions/plcat.fish
+++ .config/fish/functions/plcat.fish
@@ -1,4 +1,12 @@
 # Print the contents of a property list in XML format.
-function plcat
-    plutil -convert xml1 -o - $argv
+function plcat --argument-names file
+    if test -z "$file"
+        echo "Usage: plcat file"
+        return 1
+    else if not test -e $file
+        echo "$file does not exist"
+        return 2
+    end
+
+    plutil -convert xml1 -o - -- $file
 end
