2bb0120beff75bf1579c97b9a57134f9ba75e74c
Ben Chatelain <ben@octop.ad>
2017-05-16 10:14:14 -0600

    Add pless

---
+++ .config/fish/functions/pless.fish
@@ -0,0 +1,9 @@
+# Use less to view the XML of a property list file.
+function pless --argument-names file
+    if test -z "$file"
+        echo "Usage: pless file"
+        return 1
+    end
+
+    plcat $file | less
+end
