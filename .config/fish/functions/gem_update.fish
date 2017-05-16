bd505688095f4da3cc38e0fe9695344564c59752
Ben Chatelain <ben@octop.ad>
2017-05-15 20:40:22 -0600

    Add gem_update

---
+++ .config/fish/functions/gem_update.fish
@@ -0,0 +1,18 @@
+# Updates a Ruby gem at the system level (requires sudo permissions).
+# Binstubs are installed to /usr/local/bin alongside Homebrew binaries.
+# This command will fail (even with sudo) without --bindir being directed
+# do a user-owned dir.
+function gem_update --argument-names gem_name
+    if test -z gem_name ^/dev/null
+        echo "Usage: gem_update gem_name ..."
+        return 1
+    end
+
+    if test -n "$argv" -a (count $argv) -gt 1
+        set argv $argv[2..-1]
+    else
+        set --erase argv
+    end
+
+    sudo gem update --force $gem_name --bindir (brew_home)/bin $argv
+end
