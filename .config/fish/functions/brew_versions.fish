de561ea222f816082bbc2778c7e86383efd9696c
Ben Chatelain <ben@octop.ad>
2017-05-17 01:50:40 -0600

    [brew_versions] Return error when formula isn't installed
    brew_versions vim
    Array index out of bounds
    ~/.config/fish/functions/brew_versions.fish (line 11):     list (list -s (brew list --versions $formula))[2..-1]
                                                                                                                 ^
    in function 'brew_versions'
    	called on standard input
    	with parameter list 'vim'

--- .config/fish/functions/brew_versions.fish
+++ .config/fish/functions/brew_versions.fish
@@ -1,8 +1,11 @@
 # Lists installed versions of a formula.
 function brew_versions --argument-names formula
-    if test -z $formula
+    if test -z "$formula"
         echo "Usage: brew_versions formula"
         return 1
+    else if not brew list -1 | grep --silent --line-regexp $formula
+        echo "$formula is not installed." >&2
+        return 2
     end
 
     # brew list --versions ruby output:
