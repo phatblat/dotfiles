862cd57787cd69526f9d630b41b36c3b5c667b72
Ben Chatelain <ben@octop.ad>
2017-05-18 14:51:55 -0600

    Spin up powerline-daemon

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -44,10 +44,12 @@ set fish_pager_color_prefix cyan
 set fish_pager_color_progress cyan
 
 # Powerline
+# http://powerline.readthedocs.io/en/latest/commands/daemon.html
+powerline-daemon --quiet
 # https://computers.tutsplus.com/tutorials/getting-spiffy-with-powerline--cms-20740#highlighter_632634
 set -l python_packages /usr/local/lib/python2.7/site-packages
 set fish_function_path $fish_function_path $python_packages/powerline/bindings/fish
-powerline-setup
+powerline-setup # fish function
 
 # GUI and items requiring a user
 if status is-interactive
