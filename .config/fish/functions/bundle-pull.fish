3bfd7d26fe5e42a8c2981cfef14c01cd4ade5bf0
Ben Chatelain <ben@octop.ad>
2017-05-19 13:05:05 -0600

    [bundle-pull] Start rewrite

--- .config/fish/functions/bundle-pull.fish
+++ .config/fish/functions/bundle-pull.fish
@@ -1,4 +1,17 @@
 # Runs bundle-pull.rb ruby script
 function bundle-pull
-    bundle exec ~/.dotfiles/git/bundle-pull.rb $argv
+    # bundle exec ~/.dotfiles/git/bundle-pull.rb $argv
+
+    # New logic
+
+    # Ensure current dir is in a clean git repo
+    if not git_inside_repo
+        echo "This command must be run inside a git repo."
+        return 1
+    end
+
+    if git_repo_dirty
+        echo "The work tree is dirty, aborting."
+        return 2
+    end
 end
