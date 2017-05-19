4f78fffddb152449e95c99aee7115cd3eaa58dcb
Ben Chatelain <ben@octop.ad>
2017-05-19 13:04:31 -0600

    [git_repo_dirty] Fix logic

--- .config/fish/functions/git_repo_dirty.fish
+++ .config/fish/functions/git_repo_dirty.fish
@@ -1,7 +1,5 @@
 # Detects a dirty work tree.
 function git_repo_dirty
     # Count returns non-zero when no arguments are passed to it (clean repo)
-    if count (git status --porcelain) >/dev/null
-        return 1
-    end
+    count (git status --porcelain) >/dev/null
 end
