5ad0164f3295319879767fef67896ef7eaf5f30d
Ben Chatelain <ben@octop.ad>
2017-05-19 18:16:16 -0600

    Add arp-fix

---
+++ .config/fish/functions/arp-fix.fish
@@ -0,0 +1,40 @@
+# arp-fix
+# http://krypted.com/mac-security/disable-unicast-arp-cache-validation-in-os-x/
+# https://gist.github.com/blimmer/7242676
+function arp-fix
+    if not user_is_admin
+        echo "You must be an admin to run this command."
+        return 1
+    end
+
+    sw_vers -productVersion
+
+    set -l arp_status (list -s (sysctl net.link.ether.inet.arp_unicast_lim))[2]
+    echo "net.link.ether.inet.arp_unicast_lim: $arp_status"
+
+    set -l arp_fixed net.link.ether.inet.arp_unicast_lim=0
+
+    if test $arp_status -ne 0
+        sudo sysctl -w $arp_fixed
+        and set arp_status (list -s (sysctl net.link.ether.inet.arp_unicast_lim))[2]
+
+        if test $arp_status -eq 0
+            echo "Fixed ARP issue"
+        else
+            echo "Something went wrong"
+            echo "net.link.ether.inet.arp_unicast_lim: $arp_status"
+            return 1
+        end
+    else
+        echo "Runtime ARP status is correct"
+    end
+
+    set -l sysctl_file /etc/sysctl.conf
+    if not test -e $sysctl_file
+        sudo echo $arp_fixed > $sysctl_file
+    else if not grep --quiet $arp_fixed $sysctl_file
+        sudo echo $arp_fixed > $sysctl_file
+    else
+        echo "$sysctl_file already contains the ARP fix."
+    end
+end
