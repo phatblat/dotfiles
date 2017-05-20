da54b2840afcb74a3f08d2f908993d16712135b4
Ben Chatelain <ben@octop.ad>
2017-05-19 18:03:14 -0600

    🚧 Add and start to convert ssh and scp from ruby

--- .config/fish/functions/bundle-pull.fish
+++ .config/fish/functions/bundle-pull.fish
@@ -15,6 +15,7 @@ function bundle-pull
         return 2
     end
 
+    set -l bundle_name snapshot.bundle
     set -l hostname (hostname)
     set -l remote_hostname
     switch $hostname
@@ -29,5 +30,60 @@ function bundle-pull
 
     set -l repo_path $PWD
     set -l current_branch (current-branch)
-    set -l ssh_url "ssh://$USER@$remote_hostname:$repo_path"
+
+    # Create a git bundle file containing the diff of the working copy
+    # to HEAD.
+    ssh $USER@$remote_hostname \
+        # Move into repo dir
+        cd $repo_path \
+        pwd \
+        # Clean out previous bundle, if necessary
+        #
+        # Snapshot
+        echo "current branch: "(current-branch) \
+        set -l head_sha (git rev-parse HEAD) \
+        echo "HEAD: $head_sha" \
+        git stash list \
+        # git stash list -p \ # verbose diff of stash
+        git stash save "snapshot: "(date) \
+        #
+        # Returns only the SHA of the last stash (will need the next one back in history in order to restore staging area status)
+        set -l snapshot_sha (git show --abbrev-commit --oneline refs/stash@{0} \
+            | head -1 \
+            | awk '{print $1}') \
+        echo "snapshot: ${snapshot_sha}" \
+        #
+        # Restore the dirty work tree
+        git stash apply "stash@{0}" \
+        #
+        # Create bundle
+        git tag -d snapshot_end; or true \
+        git tag snapshot_end $snapshot_sha \
+        #
+        # This requires the HEAD commit to be present in the local repo
+        # TODO: figure out common ancestor
+        git bundle create $bundle_name HEAD..snapshot_end \
+        git bundle verify $bundle_name \
+        git tag -d snapshot_end; or true
+
+    # Download the git bundle file using SCP
+    Net::SCP.download!(remote_hostname, username,
+      "#{repo_path}/#{bundle_name}", "#{repo_path}/#{bundle_name}",
+      # :ssh => { :password => "password" }
+      )
+    puts `git bundle list-heads snapshot.bundle`
+
+    # Extract bundle
+    # puts `git cherry-pick --no-commit refs/tags/snapshot_end`
+    puts `git fetch snapshot.bundle refs/tags/snapshot_end:snapshot`
+    puts `git tag before_bundle_pull`
+    puts `git checkout snapshot`
+    puts `git reset --mixed before_bundle_pull`
+
+    # Cleanup
+    puts `git checkout #{current_branch}`
+    puts `git branch --delete snapshot`
+    puts `git tag -d before_bundle_pull`
+    puts `git tag -d snapshot_end`
+    puts `rm #{bundle_name}`
 end
