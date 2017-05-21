e6acb45fb8d7c2f7d33e196f318a6c4136cd28e7
Ben Chatelain <ben@octop.ad>
2017-05-20 22:19:32 -0600

    Add pllint

---
+++ .config/fish/functions/pllint.fish
@@ -0,0 +1,15 @@
+# Runs powerline-lint on custom config only.
+#
+# powerline-lint options:
+#
+# -p PATH, --config-path PATH
+#                       Paths where configuration should be checked, in order.
+#                       You must supply all paths necessary for powerline to
+#                       work, checking partial (e.g. only user overrides)
+#                       configuration is not supported.
+# -d, --debug           Display additional information. Used for debugging
+#                       `powerline-lint' itself, not for debugging
+#                       configuration.
+function pllint
+    powerline-lint --config-path ~/.config/powerline
+end
