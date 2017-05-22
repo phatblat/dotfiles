5933de057cd15a368f67a3734f1bcfb10602d829
Ben Chatelain <ben@octop.ad>
2017-05-22 16:28:08 -0600

    ➕  Add appcast_url

---
+++ .config/fish/functions/appcast_url.fish
@@ -0,0 +1,9 @@
+# appcast_url
+function appcast_url --argument-names url
+    if test -z "$url"
+        echo "Usage: appcast_url url"
+        return 1
+    end
+
+    brew cask _appcast_checkpoint --calculate $url
+end
