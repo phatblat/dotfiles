f26b2737b76c237f3c342a26c7451b426c6eb6ab
Ben Chatelain <ben@octop.ad>
2017-05-21 20:18:51 -0600

    Install fork of powerline-gitstatus

--- .config/fish/functions/🐍__pip.fish
+++ .config/fish/functions/🐍__pip.fish
@@ -10,7 +10,9 @@ function 🐍__pip
     echo "🐍  PIP"
     echo
 
-    set -l global_packages powerline-gitstatus powerline-status Pygments
+    set -l global_packages powerline-status Pygments \
+        # powerline-status fork
+        'git+https://github.com/phatblat/powerline-gitstatus.git@segment-spacing'
 
     # Update pip
     pip install --upgrade pip
