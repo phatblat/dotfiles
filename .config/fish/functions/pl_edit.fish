ae243bb83a574ce715f0c1e82f1496d930d7a105
Ben Chatelain <ben@octop.ad>
2017-05-22 19:56:05 -0600

    [pl_edit] Use psgrep

--- .config/fish/functions/pl_edit.fish
+++ .config/fish/functions/pl_edit.fish
@@ -1,6 +1,6 @@
 # Edit Powerline config files.
 function pl_edit
     echo "Kill the powerline-daemon (-k) to see changes."
-    ps aux | grep --invert-match 'grep' | grep powerline-daemon
+    psgrep powerline-daemon
     edit ~/.config/powerline
 end
