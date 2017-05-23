dcd0c85c541ffc40e46864e5398a83503987b56c
Ben Chatelain <ben@octop.ad>
2017-05-22 20:07:03 -0600

    ➕  Add index

---
+++ .config/fish/functions/index.fish
@@ -0,0 +1,9 @@
+# Prints the index of a value in a list.
+function index --argument-names value
+    if test -z "$value"
+        echo "Usage: index value \$list"
+        return 1
+    end
+
+    contains --index -- $value $argv[2..-1]
+end
