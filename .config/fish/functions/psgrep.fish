a7ac8c9d989ff008f56cec47c75d75169acb2a9d
Ben Chatelain <ben@octop.ad>
2017-05-22 19:53:46 -0600

    ➕  Add psgrep

---
+++ .config/fish/functions/psgrep.fish
@@ -0,0 +1,9 @@
+# Wrapper for ps which isolates processes containing
+function psgrep --argument-names process
+    if test -z "$process"
+        echo "Usage: psgrep process"
+        return 1
+    end
+
+    ps aux | grep --invert-match 'grep' | grep $process
+end
