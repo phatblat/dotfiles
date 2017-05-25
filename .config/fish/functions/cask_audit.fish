f43e21999d9c3d1baf515e36fcff1e486d01fcac
Ben Chatelain <ben@octop.ad>
2017-05-25 12:53:15 -0600

    ➕ 🍻 Add cask_audit

---
+++ .config/fish/functions/cask_audit.fish
@@ -0,0 +1,20 @@
+# Audits a Homebrew cask.
+function cask_audit --argument-names token
+    if test -z "$token"
+        echo "Usage: cask_audit token"
+        return 1
+    end
+    # switch $arg1
+    #     case on ON On
+    #     case off OFF Off
+    #     case '*'
+    # end
+
+    if brew cask info $token >/dev/null 2>&1
+        brew cask audit $token --download
+        brew cask style $token --fix
+    else
+        echo "Cask $token does not exist."
+        return 2
+    end
+end
