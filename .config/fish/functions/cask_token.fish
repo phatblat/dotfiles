589aaf5e4fc1f75ebf92a69262789f32d5512b67
Ben Chatelain <ben@octop.ad>
2017-05-25 12:48:55 -0600

    ➕ 🍻 Add cask_token

---
+++ .config/fish/functions/cask_token.fish
@@ -0,0 +1,12 @@
+# Generates a cask token for an app.
+function cask_token --argument-names path
+    if test -z "$path"
+        echo "Usage: cask_token path/to/software.app"
+        return 1
+    else if not test -e "$path"
+        echo "$path does not exist"
+        return 2
+    end
+
+    eval (brew --repository)/Library/Taps/caskroom/homebrew-cask/developer/bin/generate_cask_token
+end
