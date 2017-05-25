8438d2c263569693701de80fe421918e30b29dc1
Ben Chatelain <ben@octop.ad>
2017-05-25 12:29:22 -0600

    [dotfiles] Revise to take arg for opening only a single folder

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,20 @@
 # Edit dotfiles.
 function dotfiles
-    edit ~/.dotfiles/ ~/.config/fish/ $argv
+    set -l dotfiles ~/.dotfiles/
+    set -l fishfiles ~/.config/fish/
+
+    if test -z "$argv"
+        edit $dotfiles $fishfiles
+        return
+    end
+
+    switch $argv[1]
+        case . dot dotfiles
+            edit $dotfiles
+        case fish fishfiles
+            edit $fishfiles
+        case '*'
+            echo "Usage: dotfiles [.|fish]"
+            return 1
+    end
 end
