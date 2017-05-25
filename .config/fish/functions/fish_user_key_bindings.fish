7e587e63dc01026225f1845377100a709f1c8f42
Ben Chatelain <ben@octop.ad>
2017-05-25 11:19:49 -0600

    Restore initial vi mode to "insert"

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 # Enables VI-style key bindings. Updates $fish_key_bindings.
 # https://github.com/fish-shell/fish-shell/issues/1435
 function fish_user_key_bindings
-    fish_vi_key_bindings default
+    fish_vi_key_bindings insert
 end
