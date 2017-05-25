4b063c7cc9f87daae97e21a38ce8ffa4c6912fa8
Ben Chatelain <ben@octop.ad>
2017-05-25 16:25:11 -0600

    ➕ Add cdown

---
+++ .config/fish/functions/cdown.fish
@@ -0,0 +1,9 @@
+# Alias for curl_download.
+function cdown --argument-names url
+    if test -z "$url"
+        echo "Usage: cdown url"
+        return 1
+    end
+
+    curl_download $url
+end
--- .config/fish/functions/curl_download.fish
+++ .config/fish/functions/curl_download.fish
@@ -4,7 +4,12 @@
 # -O, --remote-name     Write output to a local file named like the remote file we get.
 # --remote-name-all     Changes the default action for all given URLs to be dealt with as if -O, --remote-name were used for each one.
 # -#, --progress-bar    Display transfer progress as a simple progress bar.
-function curl_download
+function curl_download --argument-names url
+    if test -z "$url"
+        echo "Usage: curl_download url"
+        return 1
+    end
+
     curl \
         --fail \
         --location \
