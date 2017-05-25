01383ec048f831b4f95f4db6134e400758acd507
Ben Chatelain <ben@octop.ad>
2017-05-24 23:11:08 -0600

    ➕  Add lscolors

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,9 @@
 # Configure ls to show colors and trailing slashes on directories.
 function ls --wraps ls
+    # -p      Write a slash (`/') after each filename if that file is a directory.
+
+    # -G      Enable colorized output.  This option is equivalent to defining
+    # CLICOLOR in the environment.  (See lscolors.)
+
     command ls -Gp $argv
 end
