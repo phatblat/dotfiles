f32c3fbd70099166ff51ba72e0d0d6a350c887d1
Ben Chatelain <ben@octop.ad>
2017-05-25 12:38:50 -0600

    ➕ Add cask_cache

---
+++ .config/fish/functions/cask_cache.fish
@@ -0,0 +1,19 @@
+# Manage the Homebrew Cask cache. Without args the contents of the cache are displayed.
+function cask_cache --argument-names command
+    if test -z "$command"
+        set command show
+    end
+
+    switch $command
+        case clean
+            trash -v ~/Library/Caches/Homebrew/Cask
+        case show
+            echo "Homebrew Cask cache is user-specific"
+            ll -d ~/Library/Caches/Homebrew/Cask
+            ll ~/Library/Caches/Homebrew/Cask
+        case '*'
+            # Show usage
+            echo "Usage: cask_cache arg1"
+            return 1
+    end
+end
