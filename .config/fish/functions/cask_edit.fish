fe17d9a5298d7ae3b3daa08bb0fb7fb8d77ac254
Ben Chatelain <ben@octop.ad>
2017-05-26 14:36:36 -0600

    [cask_edit] Add optional version arg
    Version is used in branch name when updating a cask with a new version

--- .config/fish/functions/cask_edit.fish
+++ .config/fish/functions/cask_edit.fish
@@ -1,14 +1,18 @@
 # Manage Homebrew casks.
-function cask_edit --argument-names token
-    if test -z $token
-        echo "Usage: cask_edit token"
+function cask_edit --argument-names token version
+    set -l branch $token
+
+    if test -z "$token"
+        echo "Usage: cask_edit token [version]"
         return 1
+    else if test -n "$version"
+        set branch $token"-"$version
     end
 
     pushd (brew --repository)/Library/Taps/caskroom/homebrew-cask
     git checkout master
     git pull
-    git checkout -b $token
+    git checkout -b $branch
 
     if brew cask info $token >/dev/null 2>&1
         # If token exists, edit it
