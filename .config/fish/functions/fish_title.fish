b4c7efd93569acfd00ca10703f0e05b5365d66c0
Ben Chatelain <ben@octop.ad>
2017-05-27 19:20:33 -0600

    [fish_title] Only show job when not fish. Show only current dir instead of full path

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -6,8 +6,22 @@
 # Set the TAB_TITLE value to override (see title function).
 function fish_title
     if test -z $TAB_TITLE
-        echo $_ ' '
-        pwd
+        set -l job $_
+
+        if test 'fish' != $job
+            echo $job ' '
+        end
+
+        set -l dir (basename $PWD)
+
+        if test $HOME = $PWD
+            # Show ~ when in $HOME
+            echo '~'
+        else
+            # Otherwise just show the current dir name
+            echo $dir
+        end
+
         return
     end
 
