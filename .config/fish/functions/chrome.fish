c5cc945c1d335f9a388499916ba9c9b9dd469bb7
Ben Chatelain <ben@octop.ad>
2017-05-29 14:20:33 -0600

    Add not about 1Password extension

--- .config/fish/functions/chrome.fish
+++ .config/fish/functions/chrome.fish
@@ -1,4 +1,9 @@
 # Launch Google Chrome.
+#
+# When Chrome is launched via this function the 1Password extension reports
+#
+# > Browser could not be verified.
+#
 function chrome
     open -a "Google Chrome" --args --new-window $argv
 end
