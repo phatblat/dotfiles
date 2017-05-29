0aa446ccffc1fac521c7af9b9cb5292df2ed5ef8
Ben Chatelain <ben@octop.ad>
2017-05-28 21:37:44 -0600

    Add pborigin

---
+++ .config/fish/functions/pborigin.fish
@@ -0,0 +1,11 @@
+# Rename the 'origin' remote to 'phatblat'
+function pborigin --argument-names arg1
+    set -l all_remotes (git remote)
+    if contains -- 'phatblat' $all_remotes
+        echo "The phatblat remote is already set up."
+        rv
+        return 1
+    end
+
+    remote rename origin phatblat
+end
