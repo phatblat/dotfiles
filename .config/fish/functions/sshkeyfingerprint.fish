9c9d10183a922e755e7ed86d5e2305acc53a6f3b
Ben Chatelain <ben@octop.ad>
2017-05-31 17:52:57 -0700

    [sshkeyfingerprint] Show md5 fingerprint that can be compared to GitHub fingerprint

--- .config/fish/functions/sshkeyfingerprint.fish
+++ .config/fish/functions/sshkeyfingerprint.fish
@@ -1,10 +1,9 @@
 # Show fingerprint of optional public key file, defaults to ~/.ssh/id_rsa.pub.
 function sshkeyfingerprint --argument-names file
-    echo file $file
     if test -z $file
         set file ~/.ssh/id_rsa.pub
     end
 
     echo -n "sshkeyfingerprint [$file] "
-    ssh-keygen -lf $file
+    ssh-keygen -l -E md5 -f $file
 end
