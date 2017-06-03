6d17248baafb7c364de4f1567655f6660f7debfc
Ben Chatelain <ben@octop.ad>
2017-06-03 14:04:41 -0600

    Add minecraft_server_port function

---
+++ .config/fish/functions/minecraft_server_port.fish
@@ -0,0 +1,13 @@
+# Prints the port that a local Minecraft server is running on.
+# java      32416 phatblat   65u     IPv6 0xcfa44781bf7c86d5        0t0      TCP *:54217 (LISTEN)
+function minecraft_server_port --argument-names arg1
+    set -l port (string replace '*:' '' (java_ports | awk '{print $9}'))
+
+    if test -n "$port"
+        echo "Minecraft server port: "$port
+        return
+    end
+
+    error "There is no Minecraft server running."
+    return 1
+end
