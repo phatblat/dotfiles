f5a5f75164d43fb9a44261dc0e46085450eb9124
Ben Chatelain <ben@octop.ad>
2017-06-03 11:18:04 -0600

    Fix arg handling in new function

--- .config/fish/functions/new.fish
+++ .config/fish/functions/new.fish
@@ -1,12 +1,10 @@
 # List all new commits have been created with the previous command, such as after a pull.
-function new
-    echo $argv | read -l commit
-
-    if test -z commit
+function new --argument-names commit
+    if test -z $commit
         echo "Usage: new <commit>"
         return 1
     end
 
-    git log $commit@{1}..$commit@{0} "$argv" 
+    git log $commit@{1}..$commit@{0} "$argv"
 end
 
