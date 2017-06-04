b2a91f2d5541d03669a381f038b3aedb794007db
Ben Chatelain <ben@octop.ad>
2017-06-04 15:56:04 -0600

    🎨 [lg] Style

--- .config/fish/functions/lg.fish
+++ .config/fish/functions/lg.fish
@@ -1,5 +1,8 @@
-# 
+# Pretty history graph.
 function lg
-    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative $argv
+    git log --graph \
+        --abbrev-commit \
+        --date=relative \
+        --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' \
+        $argv
 end
-
