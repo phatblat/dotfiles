61c1c9747fdae657d4cf8f1ad9b84b32cad1cf65
Ben Chatelain <ben@octop.ad>
2017-06-06 14:28:31 -0600

    🚧 Add gpgtest function

---
+++ .config/fish/functions/gpgtest.fish
@@ -0,0 +1,13 @@
+# gpgtest
+function gpgtest --argument-names key_id passphrase
+    if test -z "$key_id" -o -z "$passphrase"
+        echo "Usage: gpgtest key_id passphrase"
+        return 1
+    end
+
+    echo "$password" | \
+        gpg -o /dev/null \
+            --local-user $key_id \
+            -as - \
+        and echo "The correct passphrase was entered for this key"
+end
