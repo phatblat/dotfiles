ee6fd1ce077c3b85441e1e18491ca45c06d08864
Ben Chatelain <ben@octop.ad>
2017-06-06 11:46:33 -0600

    [gpgkeyid,gpgshow] Use short keyid format

--- .config/fish/functions/gpgkeyid.fish
+++ .config/fish/functions/gpgkeyid.fish
@@ -1,6 +1,7 @@
 # Prints the long format key identifiers of all GPG keys found.
 function gpgkeyid
-    gpg --list-keys --keyid-format long \
+    gpg --list-keys \
+            --keyid-format short \
         | egrep -o "^pub.*/\w+" \
         | cut -d "/" -f 2 $argv
 end
