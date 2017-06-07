affc38d4913d64722f7647241712863c0ace157e
Ben Chatelain <ben@octop.ad>
2017-06-07 16:47:43 -0600

    Check for Powerline installation before trying to use it for prompts

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -6,8 +6,11 @@
 # https://github.com/banga/powerline-shell
 #
 
-set -l powerline_shell_dir  ~/dev/vim/powerline-shell
+set -l powerline_shell_dir ~/dev/vim/powerline-shell
 
-function fish_prompt
-    eval $powerline_shell_dir/powerline-shell.py $status --shell bare ^/dev/null
+# Don't overide stock fish_prompt unless Powerline is installed
+if test -d $powerline_shell_dir
+    function fish_prompt
+        eval $powerline_shell_dir/powerline-shell.py $status --shell bare ^/dev/null
+    end
 end
