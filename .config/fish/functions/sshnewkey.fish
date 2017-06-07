8006054f2414f8bbc5d6590c2c6b31fd9e8d3817
Ben Chatelain <ben@octop.ad>
2017-06-07 16:50:20 -0600

    [sshnewkey] Fix key file path and hostname

--- .config/fish/functions/sshnewkey.fish
+++ .config/fish/functions/sshnewkey.fish
@@ -1,12 +1,14 @@
-# Generates a new RSA SSH key at ~/.ssh/id_rsa.pub with an optional comment.
-# Comment defaults to $USER@$HOST.
+# Generates a new private RSA SSH key at ~/.ssh/id_rsa with an optional comment.
+# Comment defaults to USER@hostname.
 function sshnewkey --argument-names comment
     if test -z $comment
-        set comment "$USER@$HOST"
+        set comment "$USER@"(hostname)
     end
+
+    set -l keyfile ~/.ssh/id_rsa
 
     ssh-keygen \
         -t rsa \
         -C $comment \
-        -f ~/.ssh/id_rsa.pub
+        -f $keyfile
 end
