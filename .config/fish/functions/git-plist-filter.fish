244ce8094f84a2d2f2c135ad2d9d9f9b5b3a146d
Ben Chatelain <ben@octop.ad>
2017-06-11 16:23:08 -0600

    Add git-plist-filter and hook up for converting files on add to repo

---
+++ .config/fish/functions/git-plist-filter.fish
@@ -0,0 +1,17 @@
+#!/usr/local/bin/fish
+function git-plist-filter --description='Converts plist data to XML format (stdin->stdout).'
+    #!/bin/sh
+    # had to do this because git doesn't like attaching stdin and out to plutil (waitpid error)
+
+    #TMPDIR isn't set for ssh logins!
+    set -l TMPDIR (getconf DARWIN_USER_TEMP_DIR)
+    set -l function_name git-plist-filter
+
+    set TMPFILE (mktemp $TMPDIR/$function_name.XXXXXX)
+
+    # Drop stdin to temp file
+    cat >$TMPFILE
+    plutil -convert xml1 $TMPFILE
+    cat $TMPFILE
+    rm $TMPFILE
+end
