f76cb205c3e47cc7babf965d803104d53e5edcab
Ben Chatelain <ben@octop.ad>
2017-06-12 13:25:46 -0600

    Add gw function

---
+++ .config/fish/functions/gw.fish
@@ -0,0 +1,9 @@
+function gw --description='Invokes a build using the Gradle wrapper script.'
+    if test -e ./gradlew
+        ./gradlew $argv
+        return
+    end
+
+    error "There is no Gradle wrapper in the current dir."
+    gradle $argv
+end
