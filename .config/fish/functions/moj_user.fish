1b60c52a8da0a728e8ed1d02b525a3412c482646
Ben Chatelain <ben@octop.ad>
2017-06-12 14:51:27 -0600

    [moj_user] Add emoji for admin and chatelain

--- .config/fish/functions/moj_user.fish
+++ .config/fish/functions/moj_user.fish
@@ -2,10 +2,14 @@ function moj_user \
     --description='Prints an emoji for the current user.'
 
     switch $USER
+        case 'admin'
+            echo 🙆🏻‍♂️
+        case 'ben'
+            echo 🚶
+        case 'chatelain'
+            echo 👨🏻‍💻
         case 'phatblat'
             echo 🎧
-        case 'ben'
-            echo 👨🏻‍💻👨🏻‍
         case '*'
             echo (string sub --length 1 $USER)❓
     end
