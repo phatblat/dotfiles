b89a0bed71974527ce0cc671fa15f3e3e80623c0
Ben Chatelain <ben@octop.ad>
2017-06-13 06:47:40 -0600

    Add md func

---
+++ .config/fish/functions/md.fish
@@ -0,0 +1,9 @@
+function md --argument-names path --description='Make dir and pushd into it.'
+    if test -z $path
+        echo "Usage: md path"
+        return 1
+    end
+
+    mkdir -p $path
+    and pushd $path
+end
