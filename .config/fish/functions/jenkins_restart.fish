a4185b66dd0a070833246cc4e50360250fd28fd1
Ben Chatelain <ben@octop.ad>
2017-08-06 15:25:29 -0600

    Add jenkins_restart function

---
+++ .config/fish/functions/jenkins_restart.fish
@@ -0,0 +1,12 @@
+# Connect to Jenkins Dev using SSH.
+function jenkins_restart
+    set -l jenkins_service_config /Library/LaunchDaemons/jenkins.plist
+    if not test -e $jenkins_service_config
+        error $jenkins_service_config is not present on this Mac.
+        return 1
+    end
+
+    sudo launchctl unload $jenkins_service_config
+    sudo launchctl load $jenkins_service_config
+    sudo launchctl list | grep jenkins
+end
