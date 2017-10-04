9b85fc930eda420334560faa95e61ccd9f050d84
Ben Chatelain <ben.d.chatelain@kp.org>
2017-10-04 15:23:49 -0600

    [gradle_wrapper_add] Rename function, update to 4.2.1

--- .config/fish/functions/gradle_wrapper_add.fish
+++ .config/fish/functions/gradle_wrapper_add.fish
@@ -1,4 +1,4 @@
-function add-gradle-wrapper --description="Updates the build.gradle and runs the wrapper task."
+function gradle_wrapper_add --description="Updates the build.gradle and runs the wrapper task."
 
     # Configure the wrapper task.
     # https://github.com/fish-shell/fish-shell/issues/540#issuecomment-52779637
@@ -7,7 +7,7 @@ function add-gradle-wrapper --description="Updates the build.gradle and runs the
 task removeBatchFile(type: Delete) { delete 'gradlew.bat' }
 
 wrapper {
-    gradleVersion = '4.1'
+    gradleVersion = '4.2.1'
     distributionType = 'ALL'
     finalizedBy removeBatchFile
 }
