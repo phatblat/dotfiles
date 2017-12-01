f56ec562682c3c8cb49a71e75a8e6e2a4577106a
Ben Chatelain <ben@octop.ad>
2017-12-01 16:48:37 -0700

    [kpm] Add platform argument

--- .config/fish/functions/kpm.fish
+++ .config/fish/functions/kpm.fish
@@ -1,3 +1,12 @@
-function kpm --description='Quick dir navigation to kpmobile'
-    pushd ~/dev/kpmobile
+function kpm --argument-names platform --description='Quick dir navigation to kpmobile'
+    set -l path ~/dev/kpmobile
+
+    switch $platform
+    case i ios
+        set path "$path/ios"
+    case a android
+        set path "$path/android"
+    end
+    
+    pushd $path
 end
