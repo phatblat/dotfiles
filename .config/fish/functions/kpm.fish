88dd0e364c187ad6f773ac1d407b059190cf5b1c
Ben Chatelain <ben.d.chatelain@kp.org>
2017-12-04 09:27:03 -0700

    [kpm] Add check for $platform before switch

--- .config/fish/functions/kpm.fish
+++ .config/fish/functions/kpm.fish
@@ -1,12 +1,14 @@
 function kpm --argument-names platform --description='Quick dir navigation to kpmobile'
     set -l path ~/dev/kpmobile
 
-    switch $platform
-    case i ios
-        set path "$path/ios"
-    case a android
-        set path "$path/android"
+    if test -n "$platform"
+        switch $platform
+        case i ios
+            set path "$path/ios"
+        case a android
+            set path "$path/android"
+        end
     end
-    
+
     pushd $path
 end
