7ce2cf6c0b5146bf7ad4ddb306060e38d5e11e10
Ben Chatelain <ben@octop.ad>
2018-01-06 14:09:28 -0800

    [brew_home] Improve performance to reduce fish launch time (variables.fish)

--- .config/fish/functions/brew_home.fish
+++ .config/fish/functions/brew_home.fish
@@ -1,4 +1,14 @@
-# Prints the Homebrew home dir.
-function brew_home
-    brew --prefix $argv
+function brew_home \
+        --description='Prints the Homebrew home dir or the location in the cellar where formula is or would be installed.' \
+        --argument-names formula
+
+    if test -z "$BREW_HOME"
+        set --export BREW_HOME (brew --prefix)
+    end
+
+    if test -n "$formula"
+        echo "/usr/local/opt/$formula"
+    else
+        echo $BREW_HOME
+    end
 end
