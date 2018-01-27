da9f795eef7d2ffd2f48da3f6a52fb8376f4f76d
Ben Chatelain <ben@octop.ad>
2018-01-27 10:23:08 -0700

    [track] Add optional 2nd arg for local branch name

--- .config/fish/functions/track.fish
+++ .config/fish/functions/track.fish
@@ -1,7 +1,13 @@
-function track --argument-names branch --wraps=git --description='Creates a local tracking branch.'
-    if test -z $branch
-        echo "Usage: track branch"
+function track --wraps=git \
+        --description='Creates a local tracking branch.' \
+        --argument-names remote_branch local_name
+    if test -z $remote_branch
+        echo "Usage: track remote_branch [local_name]"
         return 1
     end
-    c -t $branch
+    if test -z $local_name
+        c --track $remote_branch
+        return 0
+    end
+    c --track $remote_branch -b $local_name
 end
