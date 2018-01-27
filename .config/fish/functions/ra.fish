d98927a32d81d3f11ad9a4b9a054cf6c2f366c74
Ben Chatelain <ben@octop.ad>
2018-01-27 10:52:41 -0700

    [ra] Make url optional, build one assuming fork

--- .config/fish/functions/ra.fish
+++ .config/fish/functions/ra.fish
@@ -1,4 +1,22 @@
-function ra --description 'Adds a git remote' --argument name url
+function ra \
+        --description 'Adds a git remote' \
+        --argument-names name url
+    if test -z $name
+        error Remote name required
+        return 1
+    end
+
+    # Add a fork of the project with only the remote name is given.
+    if test -z $url
+        set -l remote_url (git remote get-url (remote-for-current-branch))
+        # Drop scheme and host
+        set -l path (string split ':' $remote_url)[2]
+        # Drop .git
+        set -l path (string split '.' $path)[1]
+        set -l project (string split '/' $path)[2]
+        set url "git@github.com:$name/$project.git"
+    end
+
     git remote add $name $url
     and rv
     and fetch $name
