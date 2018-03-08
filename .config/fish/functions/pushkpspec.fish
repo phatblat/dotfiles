b0025796a7f1e6dca7d717f22d95e9a7be64af11
Ben Chatelain <ben@octop.ad>
2018-03-08 15:16:30 -0700

    ✨ Add pushkpspec function

---
+++ .config/fish/functions/pushkpspec.fish
@@ -0,0 +1,32 @@
+function pushkpspec \
+        --description="Pushes a podspec to kp-internal-pods" \
+        --argument spec_file
+    if test -z $spec_file
+        # Attempt to locate spec in current dir when not file name provided
+        set -l specs (ls -1 *.podspec | paste -sd " " -)
+        set -l spec_count (count $specs)
+
+        # If specs does not contain an array, spec_count will be a char count
+        # if [[ ! $(declare -p specs 2> /dev/null | grep -q '^typeset \-a') ]]; then
+        if test $spec_count -eq 1
+            set spec_file $specs
+        else if test $spec_count -eq 0
+            echo "No podspecs found in the current directory."
+           return 1
+        else
+            echo "Multiple podspecs found in the current directory ($spec_count). Please specify which spec you would like to publish.\n  pushmobilespec Pod.podspec"
+            return 2
+        end
+    end
+
+    set -l spec_repo_name "kp-internal-pods"
+
+    bundle exec "pod repo push \
+        $spec_repo_name \
+        $spec_file \
+        --sources=git@github.kp.org:internal-pods/specs,git@github.kp.org:mirrored-pods/specs \
+        --use-libraries \
+        --private \
+        --allow-warnings \
+        $argv"
+end
