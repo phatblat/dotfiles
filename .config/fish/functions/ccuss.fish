3806c838ab36a34b1685ec266ba5741e3f3e57d2
Ben Chatelain <ben@octop.ad>
2018-04-08 10:50:02 -0600

    🐛 Fix ccuss function

--- .config/fish/functions/ccuss.fish
+++ .config/fish/functions/ccuss.fish
@@ -1,4 +1,5 @@
-# Update depencencies in git submodules without building using Carthage over SSH.
-function ccuss --wraps carthage --description Update depencencies in git submodules without building using Carthage over SSH
+function ccuss \
+        --wraps carthage \
+        --description='Update depencencies in git submodules without building using Carthage over SSH'
     carthage update --no-use-binaries --no-build --use-ssh --use-submodules $argv
 end
