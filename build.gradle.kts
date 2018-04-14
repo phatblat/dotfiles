cf464d5a738d4020290353cc2a1c1bf1b3eb7e1d
Ben Chatelain <ben@octop.ad>
2018-04-14 16:55:46 -0600

    🐘 Gradle init

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+/*
+ * build.gradle.kts
+ * dotfiles
+ */
+
+
+/* -------------------------------------------------------------------------- */
+// 🔌 Plugins
+/* -------------------------------------------------------------------------- */
+
+plugins {
+    // Gradle plugin portal - https://plugins.gradle.org/
+    id("at.phatbl.clamp") version "1.0.0"
+    id("at.phatbl.shellexec") version "1.1.3"
+}
