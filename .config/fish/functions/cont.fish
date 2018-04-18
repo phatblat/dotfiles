2694abc7aeffa1c31fdc637b4af26470f7b66fa6
Ben Chatelain <ben@octop.ad>
2018-04-18 14:44:24 -0700

    🐛 Fix cont function for rebase

--- .config/fish/functions/cont.fish
+++ .config/fish/functions/cont.fish
@@ -1,10 +1,13 @@
-# Commit an in-progress git merge or continue a rebase, cherry-pick or am (apply mail patch).
-function cont
+function cont \
+        --description='Commit an in-progress git merge or continue a rebase, cherry-pick or am (apply mail patch).'
     if test -e .git/MERGE_HEAD
         echo "File exists"
         git commit ^/dev/null
+    else if test -e .git/REBASE_HEAD
+        git rebase --continue ^/dev/null
+    else if test -e .git/CHERRY_PICK_HEAD
+        git cherry-pick --continue $argv
+    else
+        git am --continue ^/dev/null
     end
-    or git rebase --continue ^/dev/null
-    or git cherry-pick --continue $argv
-    or git am --continue ^/dev/null
 end
