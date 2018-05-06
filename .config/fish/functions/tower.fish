1cce5aac189a13bdd94688815501792019d3f173
Ben Chatelain <ben@octop.ad>
2018-05-05 20:08:00 -0600

    Add description and link to docs to tower function

--- .config/fish/functions/tower.fish
+++ .config/fish/functions/tower.fish
@@ -1,4 +1,5 @@
-# Open the current directory in 
-function tower
+# https://www.git-tower.com/help/mac/integration/cli-tool
+function tower \
+        --description='Open the current repo in Tower'
     gittower (root) $argv
 end
