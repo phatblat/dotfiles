f63553ca1ea6b7ef37dd8bef5df4c8f302373bb4
Ben Chatelain <ben@octop.ad>
2018-05-08 17:06:18 -0600

    Add cron arg to dotfiles function

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,6 +1,7 @@
 function dotfiles --description='Edit dotfiles.' \
         --argument-names=type
     set -l dotfiles ~/.dotfiles/
+    set -l cronfiles ~/.dotfiles/cron/
     set -l fishfiles ~/.config/fish/
 
     if test -z "$type"
@@ -11,10 +12,12 @@ function dotfiles --description='Edit dotfiles.' \
     switch $type
         case . dot dotfiles
             edit $dotfiles
+        case cron
+            edit $cronfiles
         case fish fishfiles
             edit $fishfiles
         case '*'
-            echo "Usage: dotfiles [.|fish]"
+            echo "Usage: dotfiles [.|cron|fish]"
             return 1
     end
 end
