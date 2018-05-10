cb2e3e3f333f41eabb1a1594c24eb020b85e0c67
Ben Chatelain <ben@octop.ad>
2018-05-10 13:56:00 -0600

    Move brew bin up in PATH

--- .config/fish/variables.fish
+++ .config/fish/variables.fish
@@ -56,7 +56,7 @@ set --global fish_user_paths \
     $fish_user_paths
 
 # PATH
-set --export --global PATH ./bin ~/bin (brew_home curl)/bin $ANDROID_HOME/tools/bin $PATH
+set --export --global PATH ./bin ~/bin (brew_home)/bin (brew_home curl)/bin $ANDROID_HOME/tools/bin $PATH
 
 # ls color formatting - LS_COLWIDTHS
 #
