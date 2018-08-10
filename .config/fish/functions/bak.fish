c47f9abf65b7ea6d0cde322b615a21b5d0ddb206
Ben Chatelain <ben@octop.ad>
2018-08-10 10:27:40 -0600

    🤺 Add trailing slash handling to bak function

--- .config/fish/functions/bak.fish
+++ .config/fish/functions/bak.fish
@@ -1,9 +1,15 @@
-# Backs up a file by appending .bak extension. When run on a file
-# that already has .bak, the extension is removed.
-function bak --argument-names file
+function bak \
+        --description='Backs up a file by appending .bak extension. When run on a file that already has .bak, the extension is removed.' \
+        --argument-names file
     if not test -e $file
-        echo "'$file' does not exist"
+        echo "🤷🏻‍♂️  '$file' does not exist"
         return 1
+    end
+
+    # Strip off trailing slash from dir autocompletion
+    set -l pattern '\/$'
+    if string match --quiet --regex $pattern $file
+        set file (string replace --regex $pattern '' $file)
     end
 
     set -l components (string split "." -- $file)
@@ -12,10 +18,10 @@ function bak --argument-names file
         # Remove the .bak extension
         set new_name (string join "." $components[1..-2])
         mv $file $new_name
-        echo "Renamed to '$new_name'"
+        echo "🚚  Renamed to '$new_name'"
     else
         # Append a .bak extension
         mv $file $file.bak
-        echo "Renamed to '$file.bak'"
+        echo "🚚  Renamed to '$file.bak'"
     end
 end
