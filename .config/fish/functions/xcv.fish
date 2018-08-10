c72447c74be5245b2d5aa84e9da83f02756ea309
Ben Chatelain <ben@octop.ad>
2018-08-10 11:36:19 -0600

    ⚡🔇 ️Refactor xcv for speed, add -s short option

--- .config/fish/functions/xcv.fish
+++ .config/fish/functions/xcv.fish
@@ -1,27 +1,50 @@
-function xcv --description='Displays the version of the currently selected Xcode.'
-    set -l output (xcodebuild -version)
+function xcv \
+        --description='Displays the version of the currently selected Xcode.' \
+        --argument-names short_flag
+    # xcodebuild -version takes 250ms
+    # Example output:
+    #   Xcode 10.0
+    #   Build version 10L201y
+    # set -l marketing_version (echo $output | awk '{print $2}')
+    # set -l build_version (echo $output | awk '{print $5}')
 
-    set -l marketing_version (echo $output | awk '{print $2}')
-    set -l build_version (echo $output | awk '{print $5}')
+    # Parse for marketing version
+    set -l marketing_version (\
+        plutil -p (xcode-select -p)/../Info.plist \
+        | grep -e CFBundleShortVersionString \
+        | sed 's/[^0-9\.]*//g' \
+    )
+
+    # Print only the marketing version
+    if test -s = $short_flag
+        echo $marketing_version
+        return
+    end
+
+    # Parse for build version
+    set -l build_version (\
+        plutil -p (xcode-select -p)/../Info.plist \
+        | grep -e DTXcodeBuild \
+        | awk '{print $3}' \
+        | sed 's/"//g' \
+    )
+
+    # Check if this is a beta build
     set -l beta_version
-
     switch $build_version
-        #    9.0    9.1  9.2   9.3   9.4
-        case 9M136h 9B37 9C32c 9Q98q 9Q1004a
+        #    10.0
+        case 101
             set beta_version "beta 1 "
-        case 9M137d 9B46 9C34b 9Q107o
+        case 102
             set beta_version "beta 2 "
-        case 9M174d           9Q117m
+        case 10L201x 10L201y
             set beta_version "beta 3 "
-        case 9M189t           9Q127n
+        case 104
             set beta_version "beta 4 "
-        case 9M202q
+        case 105
             set beta_version "beta 5 "
-        case 9M214v
+        case 106
             set beta_version "beta 6 "
-        # 9.0 GA is the same build as the GM seed.
-        # case 9A235
-        #     set beta_version "GM seed "
     end
 
     echo "$marketing_version $beta_version($build_version)"
