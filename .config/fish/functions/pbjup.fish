725ecf8904367bf7869fc3e3940644c8497fb143
Ben Chatelain <ben@octop.ad>
2018-10-13 20:11:28 -0700

    ✨ Add pbjup function

---
+++ .config/fish/functions/pbjup.fish
@@ -0,0 +1,8 @@
+function pbjup --description='Upgrade personal jenkins formula and restart service'
+    brew update
+    brew upgrade pbjenkins
+    brew services list
+    echo "♻️👷🏻‍♂️Restarting jenkins using admin privileges"
+    sudo brew services restart pbjenkins
+end
+
