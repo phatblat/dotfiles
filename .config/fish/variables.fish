d76ae2509823442ad41295ea0d0e3025067c132a
Ben Chatelain <ben@octop.ad>
2018-11-05 21:27:52 -0800

    🚨 Suppress groovy warning when gradle invoked from Java 9+

--- .config/fish/variables.fish
+++ .config/fish/variables.fish
@@ -35,7 +35,10 @@ set --export GRADLE_HEAP_SPACE -Xmx1g
 set --export GROOVY_HOME (brew_home groovy)/libexec
 # Use Java 8 for now until 10 is more stable
 set --export --global JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set --export JAVA_OPTS "-Xms256m -Xmx512m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m"
+# Suppressing Groovy warnings when gradle invoked from Java 9+
+# https://github.com/gradle/gradle/issues/2995
+# https://issues.apache.org/jira/browse/GROOVY-8339
+set --export JAVA_OPTS "-Xms256m -Xmx512m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED"
 set --export ICLOUD_HOME "~/Library/Mobile Documents"
 set --export ICLOUD_DRIVE $ICLOUD_HOME"/com~apple~CloudDocs"
 set --export LANG en_US.UTF-8
