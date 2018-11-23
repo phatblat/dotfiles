a9dc05916e807bcffee5e8f43b442dfdfaf8b1d9
Ben Chatelain <ben@octop.ad>
2018-11-23 15:35:02 -0700

    🐛 Fix setjdk function

--- .config/fish/functions/setjdk.fish
+++ .config/fish/functions/setjdk.fish
@@ -1,9 +1,10 @@
-# Update JAVA_HOME to point to the given JDK version.
-function setjdk --argument-names version
+function setjdk \
+        --argument-names version \
+        --description "Updates JAVA_HOME to point to the given JDK version."
     if test -z $version
         echo "Usage: setjdk 1.8 (or) setjdk 1.8.0_111"
         return 1
     end
 
-    set --export JAVA_HOME (/usr/libexec/java_home --version $version)
+    set --export --global JAVA_HOME (/usr/libexec/java_home --version $version)
 end
