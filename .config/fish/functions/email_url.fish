9ac17563f5a16137d171fa2761ee61f7c8419005
Ben Chatelain <ben@octop.ad>
2018-11-29 11:50:23 -0700

    ✨ Add email_url function

---
+++ .config/fish/functions/email_url.fish
@@ -0,0 +1,19 @@
+function email_url \
+        --description='Determines an appropriate contact for a given url' \
+        --argument-names url
+
+    set -l user ben
+    set -l work_domain kp.org
+    set -l default $user@octop.ad
+
+    if test -z "$url"
+        echo $default
+        return
+    end
+
+    if string match --quiet --entire $work_domain $url
+        echo $user.d.chatelain@$work_domain
+    else
+        echo $default
+    end
+end
