d7ec75bb67fae4e10444a7c692717ba7ef533d75
Ben Chatelain <ben@octop.ad>
2018-11-29 11:17:22 -0700

    ♻️ Combine clone and cloned

--- .config/fish/functions/clone.fish
+++ .config/fish/functions/clone.fish
@@ -1,4 +1,14 @@
-# Perform a git clone.
-function clone
-    git clone $argv
+function clone \
+        --description="Performs a git clone, then configures repo user." \
+        --argument-names url dir
+    if test -z "$url"
+        echo "Usage: clone url [dir]"
+        return 1
+    end
+    if test -z "$dir"
+        set dir (string split '.' (string split '/' $url)[2])[1]
+    end
+
+    git clone -- $url $dir
+    and pushd $dir
 end
