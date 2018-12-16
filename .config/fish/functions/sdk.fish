bcb3695c0e4d1beba311ab3fc55ea4d31d16d316
Ben Chatelain <ben@octop.ad>
2018-12-16 16:36:02 -0700

    ♻️🧰 Convert sdk to custom wrapper function to work around timing issues on shell startup

---
+++ .config/fish/functions/sdk.fish
@@ -0,0 +1,8 @@
+# Copied from omf. Using from there gets sourced before config/variables, yielding error:
+# > bash: /bin/sdkman-init.sh: No such file or directory
+# Defined in /Volumes/ThunderBay/Users/phatblat/.local/share/omf/pkg/sdk/functions/sdk.fish @ line 1
+function sdk \
+        --wraps=sdk \
+        --description 'Software Development Kit Manager'
+    bash -c "source $sdkman_prefix/bin/sdkman-init.sh && sdk $argv"
+end
