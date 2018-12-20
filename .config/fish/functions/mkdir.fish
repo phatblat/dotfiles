43883793dbb65ca0c1aa4e4d3497d763596a71c8
Ben Chatelain <ben.d.chatelain@kp.org>
2018-12-20 10:44:43 -0700

    ✨🗂 Add mkdir function

---
+++ .config/fish/functions/mkdir.fish
@@ -0,0 +1,14 @@
+function mkdir --description='Create a directory and set CWD'
+
+    command mkdir -p $argv
+    if test $status = 0
+        set -l last_arg $argv[(count $argv)]
+        switch $last_arg
+            case '-*'
+
+            case '*'
+                cd $last_arg
+                return
+        end
+    end
+end
