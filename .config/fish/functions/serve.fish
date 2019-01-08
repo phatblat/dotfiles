f174a59b3c4e7845544fcf6c330f8ffa474580f7
Ben Chatelain <ben@octop.ad>
2019-01-08 16:48:17 -0700

    Revise serve function, only pushding if necessary and auto-opening local site in browser

--- .config/fish/functions/serve.fish
+++ .config/fish/functions/serve.fish
@@ -1,8 +1,10 @@
 function serve \
         --description='Runs Jekyll server in the foreground with the given environment variables.'
     set reflog_content ~/dev/www/reflog/www
-    pushd $reflog_content
+    if test "$pwd" != "$reflog_content"
+        pushd $reflog_content
+    end
 
+    open http://0.0.0.0:4000/
     env (cat .env | xargs) ../bin/jekyll serve $argv
-    popd
 end
