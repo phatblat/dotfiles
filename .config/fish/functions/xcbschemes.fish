8b3ab884e3825383263c3a7e317e1decd572d304
Ben Chatelain <ben@octop.ad>
2019-01-11 22:30:01 -0700

    ✨🛠️ Add xcbschemes function

---
+++ .config/fish/functions/xcbschemes.fish
@@ -0,0 +1,16 @@
+function xcbschemes \
+        --description='Displays schemes for an Xcode project'
+    
+    # Parse the xcodebuild -list output for schemes
+    set --local schemes (xcblist \
+        | grep Schemes: -A 10 \
+        | tail -n +2 \
+        | sed -e 's/^[ \t]*//')
+
+    # Drop the last (empty) line
+    set schemes $schemes[1..-2]
+
+    for scheme in $schemes
+        echo $scheme
+    end
+end
