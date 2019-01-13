85b8b29665fd9cd4dec95e21d2f80fdb367e9212
Ben Chatelain <ben@octop.ad>
2019-01-13 00:02:36 -0700

    ✨ Add masrm function

--- .config/fish/functions/fileowner.fish
+++ .config/fish/functions/fileowner.fish
@@ -1,5 +1,7 @@
-# Displays the owner of a file.
-function fileowner --argument-names file
+function fileowner \
+        --description='Displays the owner of a file.' \
+        --argument-names file
+
     if test -z "$file"
         echo "Usage: fileowner file"
         return 1
---
+++ .config/fish/functions/masrm.fish
@@ -0,0 +1,33 @@
+function masrm \
+        --description='Uninstall mas package'
+
+    if not which -s mas
+        error mas is not installed
+        return 1
+    end
+
+    set --local binary /usr/local/bin/mas
+    set --local framework /usr/local/Frameworks/MasKit.framework
+
+    if not test -f $binary
+        error No mas found at $binary
+        return 2
+    else if not test -d $framework
+        error No MasKit.framework found at $framework
+        return 3
+    end
+
+    # Packges are installed by root
+    set --local owner (fileowner $binary)
+    if test root = $owner
+        trash $binary
+    else if test $USER = $owner
+        brew uninstall mas
+    end
+
+    if which -s mas
+        error Another copy of mas is still installed
+        masshow
+        return 4
+    end
+end
