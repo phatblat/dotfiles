d8676f16c28102f1834a8b1d635461fce5b09180
Ben Chatelain <ben@octop.ad>
2019-01-12 23:43:05 -0700

    🔪 Split tail of brew_edit out into separate brew_test

---
+++ .config/fish/functions/brew_test.fish
@@ -0,0 +1,24 @@
+function brew_test \
+        --description='Installs and tests a Homebrew formula' \
+        --argument-names token formula_version
+
+    if test -z "$token"
+        echo "Usage: brew_test token [formula_version]"
+        return 1
+    end
+
+    # Special handling for certain formulae
+    switch $token
+        case mas
+            brew tap --list-pinned | grep mas-cli/tap
+            and brew tap-unpin mas-cli/tap
+    end
+
+    if brew ls --versions $token > /dev/null
+        brew uninstall $token
+    end
+
+    brew install --build-from-source $token
+    and brew test $token
+    and brew audit --strict $token
+end
