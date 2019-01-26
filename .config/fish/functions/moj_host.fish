31b9b5aa7c345cb19190c5a741648e71f0ccef8e
Ben Chatelain <ben@octop.ad>
2019-01-25 21:49:11 -0700

    💻 Add bens-mbp-wifi to moj_host

--- .config/fish/functions/moj_host.fish
+++ .config/fish/functions/moj_host.fish
@@ -7,7 +7,7 @@ function moj_host --description='Prints an emoji for the current host.'
     end
 
     switch $host
-        case 'greymatter*' 'Bens-MacBook-Pro*' 'mcoe-am*'
+        case 'greymatter*' 'Bens-MacBook-Pro*' 'bens-mbp-wifi*' 'mcoe-am*'
             echo 💻
         case 'hi-c-era*'
             echo 🌋
