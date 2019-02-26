34ffcfbfae6ea4175ed2bb2556dc03ab05f46257
Ben Chatelain <ben.d.chatelain@kp.org>
2019-02-26 11:24:55 -0700

    ✨ Add mvn_local function

---
+++ .config/fish/functions/mvn_local.fish
@@ -0,0 +1,8 @@
+# https://stackoverflow.com/a/16218772
+function mvn_local \
+    --description='Displays path to Maven local repo'
+
+    mvn help:evaluate \
+        -Dexpression=settings.localRepository \
+        | grep -v '\[INFO\]'
+end
