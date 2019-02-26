44672ae53a46646d49efd368a5faca64b6ad7262
Ben Chatelain <ben@octop.ad>
2019-02-26 05:48:27 -0700

    ✨🛣 Add nav function

---
+++ .config/fish/functions/nav.fish
@@ -0,0 +1,15 @@
+function nav \
+        --description='Quick nav to ApplePlatformVersions dir.' \
+        --argument-names dir
+    if test -z "$dir"
+        error "Usage: nav dir"
+        return 1
+    end
+
+    if test -d "$dir"
+        pushd $dir
+    else
+        # Depends on custom mkdir function which includes pushd
+        mkdir "$dir"
+    end
+end
