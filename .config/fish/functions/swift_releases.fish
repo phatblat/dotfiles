dbaf44999b40a8b7c0c7443516580f7ff03364b1
Ben Chatelain <ben@octop.ad>
2019-04-19 17:11:11 -0600

    ♻️ Refactor swift_releases, add arguments for version and number of days

--- .config/fish/functions/swift_releases.fish
+++ .config/fish/functions/swift_releases.fish
@@ -1,4 +1,21 @@
-function swift_releases --description='Check for Swift releases'
+function swift_releases \
+        --description='Check for Swift releases' \
+        --argument-names swift_version days
+
+    set -l branch_name development
+
+    if test -n "$swift_version"
+        set swift_version "-$swift_version"
+        set branch_name "swift$swift_version-branch"
+    end
+
+    if test -z "$days"
+        set days 7
+    end
+
+    echo "Checking for Swift $branch_name snapshot releases ($days days)"
+    echo
+
     set -l year (date "+%Y")
     set -l month (date "+%m")
     set -l current_day (date "+%d")
@@ -11,8 +28,8 @@ function swift_releases --description='Check for Swift releases'
         set -l date $year-$month-$day
         echo $date
 
-        set -l release_slug "swift-DEVELOPMENT-SNAPSHOT-$date-a"
-        set -l url "https://swift.org/builds/development/xcode/$release_slug/$release_slug-osx.pkg"
+        set -l release_slug "swift$swift_version-DEVELOPMENT-SNAPSHOT-$date-a"
+        set -l url "https://swift.org/builds/$branch_name/xcode/$release_slug/$release_slug-osx.pkg"
         echo $url
 
         curl \
@@ -20,5 +37,10 @@ function swift_releases --description='Check for Swift releases'
             --location \
             --write-out "%{http_code}\n\n" \
             $url
+
+        set days (math "$days - 1")
+        if test $days -le 0
+            return
+        end
     end
 end
