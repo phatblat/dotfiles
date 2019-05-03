d17c9d8cc496ea191d22e7fb828dc06ddb463e9d
Ben Chatelain <ben@octop.ad>
2019-05-02 20:10:02 -0600

    Fix tests in console_user

--- .config/fish/functions/console_user.fish
+++ .config/fish/functions/console_user.fish
@@ -1,8 +1,8 @@
 function console_user \
         --description="Tests whether the current user logged into the console (GUI)"
-    if test is_mac
+    if is_mac
         test $USER = (stat -f '%Su' /dev/console)
-    else if test is_linux
+    else if is_linux
         test $USER = (stat --format='%U' /tmp/.X11-unix/X0)
     else
         error Unknown kernel: $KERNEL
