edf2fca4f8c2cd7ccfb8de6a23d347af0c0414b8
Ben Chatelain <ben@octop.ad>
2019-05-06 14:34:07 -0600

    Fix test errors on terminal open

--- .config/fish/variables.fish
+++ .config/fish/variables.fish
@@ -5,6 +5,8 @@
 # Exported variables in fish shell.
 #
 
+set --export KERNEL (uname)
+
 # Upper case
 set --export ANDROID_HOME (brew_home)/share/android-sdk
 set --export ANDROID_SDK_ROOT (brew_home)/share/android-sdk
@@ -16,7 +18,6 @@ set --export GROOVY_HOME (brew_home groovy)/libexec
 set --export ICLOUD_HOME "~/Library/Mobile Documents"
 set --export ICLOUD_DRIVE $ICLOUD_HOME"/com~apple~CloudDocs"
 set --export JABBA_HOME ~/.jabba
-set --export KERNEL (uname)
 set --export LANG en_US.UTF-8
 set --export LANGUAGE en_US.UTF-8
 set --export LC_ALL en_US.UTF-8
