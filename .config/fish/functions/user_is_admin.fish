b5f0fa391b4c19970362bea08cedcfd7e090ac20
Ben Chatelain <ben@octop.ad>
2019-05-06 14:42:17 -0600

    Add linux support to user_is_admin

--- .config/fish/functions/user_is_admin.fish
+++ .config/fish/functions/user_is_admin.fish
@@ -1,4 +1,8 @@
-# Tests whether USER is a member of the admin group.
-function user_is_admin
-    test "user is a member of the group" = (dsmemberutil checkmembership -U $USER -G "admin")
+function user_is_admin \
+        --description='Tests whether USER is a member of the admin group.'
+    if is_mac
+        test "user is a member of the group" = (dsmemberutil checkmembership -U $USER -G "admin")
+    else if is_linux
+        groups | grep -q adm
+    end
 end
