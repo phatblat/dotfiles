761ad370dc949020ca4c75e5535002bd2ce06d98
Ben Chatelain <ben@octop.ad>
2019-05-12 08:29:40 -0700

    Clone APV when not present. Moved to ~/dev

--- .config/fish/functions/apv.fish
+++ .config/fish/functions/apv.fish
@@ -1,6 +1,16 @@
 function apv \
         --description='Quick nav to ApplePlatformVersions dir.'
-    nav ~/dev/markdown/ApplePlatformVersions
-    pull
+
+    set -l apv_dir ~/dev/ApplePlatformVersions
+
+    if not test -d $apv_dir
+        nav ~/dev
+        clone git@github.com:phatblat/ApplePlatformVersions.git
+    else
+        nav $apv_dir
+        pull
+    end
+
     lg10
 end
+
