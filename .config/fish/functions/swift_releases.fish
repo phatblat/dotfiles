ae5f39d1d0f5fe0339ee43a5b823e68aa1fdf502
Ben Chatelain <ben@octop.ad>
2019-07-02 20:05:15 -0600

    Add wrap around to previous month for swift releases

--- .config/fish/functions/swift_releases.fish
+++ .config/fish/functions/swift_releases.fish
@@ -4,7 +4,7 @@ function swift_releases \
 
     set -l branch_name development
 
-    if test $swift_version = "trunk"
+    if test "$swift_version" = "trunk"
         # use default branch name
         # clear swift version
         set -e swift_version
@@ -14,6 +14,7 @@ function swift_releases \
     end
 
     if test -z "$days"
+        # Default to a week
         set days 7
     end
 
@@ -21,35 +22,69 @@ function swift_releases \
     echo
 
     set -l year (date "+%Y")
-    set -l month (date "+%m")
-    set -l current_day (date "+%d")
+    set -l months (date "+%m")
+    set -l current_day (date "+%e") # (1-31); single digits are preceded by a blank.
 
-    # jot generates a decending sequence of integers
-    for day in (jot $days $current_day 1 -1)
-        # Zero pad month and day
-        set month (printf "%02d" $month)
-        set day (printf "%02d" $day)
+    # Detect month wrap around
+    if test $days -gt $current_day
+        # Add previous month
+        set -l prev_month
+        if test $months = 1
+            set prev_month 12
+        else
+            set prev_month (math -- $months - 1)
+        end
 
-        set -l date $year-$month-$day
-        echo -n $date
+        set months $months $prev_month
+    end
 
-        set -l release_slug "swift$swift_version-DEVELOPMENT-SNAPSHOT-$date-a"
-        set -l url "https://swift.org/builds/$branch_name/xcode/$release_slug/$release_slug-osx.pkg"
-        #echo $url
+    # This is typically a single iteration unless more days were requested than
+    # have passed in the current month.
+    for month in $months
+        # Start on the last day of the month in the 2nd iteration
+        set -l days_in_month 31
+        switch $month
+            case 4 6 9 11
+                set days_in_month 30
+            case 2
+                set days_in_month 28
+        end
+        if test $current_day -gt $days_in_month
+            set current_day $days_in_month
+        end
 
-        set -l status_code (
-            curl \
-                --head \
-                --location \
-                --silent \
-                --output /dev/null \
-                --write-out "%{http_code}" \
-                $url
-       )
+        # jot generates a decending sequence of integers
+        for day in (jot $days $current_day 1 -1)
+            # Zero pad month and day
+            set month (printf "%02d" $month)
+            set day (printf "%02d" $day)
 
-       echo " - $status_code"
-       if test $status_code = 200
-           echo $url
-       end
+            set -l date $year-$month-$day
+            echo -n $date
+
+            set -l release_slug "swift$swift_version-DEVELOPMENT-SNAPSHOT-$date-a"
+            set -l url "https://swift.org/builds/$branch_name/xcode/$release_slug/$release_slug-osx.pkg"
+
+            set -l status_code (
+                curl \
+                    --head \
+                    --location \
+                    --silent \
+                    --output /dev/null \
+                    --write-out "%{http_code}" \
+                    $url
+            )
+
+            echo " - $status_code"
+            if test $status_code -eq 200
+                echo $url
+            end
+
+            # Decrement days count for previous month
+            set days (math -- $days - 1)
+        end
+
+        # Start at the end of the previous month
+        set current_day 31
     end
 end
