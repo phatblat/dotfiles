298c13500e4e84b728c35b3a94589ac8e12cdf4f
Ben Chatelain <ben@octop.ad>
2019-09-05 22:00:08 -0600

    🎨 Switch to bobthefish omf theme

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/phatblat/.local/share/omf/themes/bobthefish/fish_prompt.fish
--- .config/fish/variables.fish
+++ .config/fish/variables.fish
@@ -41,6 +41,43 @@ set --export github_user phatblat
 set --export powerline_enabled 0
 set --global --export sdkman_prefix ~/.sdkman
 
+# OMF bobthefish theme options
+# https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md#bobthefish
+
+set -g theme_display_git yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_untracked yes
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_master_branch yes
+set -g theme_git_worktree_support yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+set -g theme_display_ruby no
+set -g theme_display_user ssh
+set -g theme_display_hostname ssh
+set -g theme_display_vi yes
+set -g theme_display_nvm yes
+set -g theme_display_date no
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user phatblat
+set -g theme_color_scheme gruvbox
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_length 1
+set -g theme_newline_cursor no
+
 # Editor
 # After variables which depend on functions that define variables
 set --export EDITOR_CLI "vim" # vi vim
