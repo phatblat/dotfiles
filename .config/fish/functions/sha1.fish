1112bce028d011b1aa4543d6bc7f2fad9e2dba25
Ben Chatelain <ben.d.chatelain@kp.org>
2019-10-10 17:14:27 -0600

    ✨ Add sha1 function

---
+++ .config/fish/functions/sha1.fish
@@ -0,0 +1,10 @@
+function sha1 \
+    --description='Displays the SHA1 hash of files'
+
+    if test -z "$argv"
+        echo "Usage: sha1 file1 file2..."
+        return 1
+    end
+
+    shasum --algorithm 1 $argv
+end
