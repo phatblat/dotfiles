41a5990f85ce4a31569a35eb230281a7cd7cbc2b
Ben Chatelain <ben@octop.ad>
2019-12-19 11:42:58 -0700

    ✨ Add signing_cert_details function

---
+++ .config/fish/functions/signing_cert_details.fish
@@ -0,0 +1,14 @@
+function signing_cert_details \
+    --description='Prints signing certificate details' \
+    --argument-names cert_name
+
+    if test -z $cert_name
+        error "Usage: signing_cert_details \"iOS Dev: Me\""
+        echo "Use the list_codesign_identities function to find the certificate name."
+    end
+
+    security find-certificate \
+        -c "$cert_name" \
+        -p \
+        | openssl x509 -text
+end
