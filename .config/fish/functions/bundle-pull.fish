2dc59e3437d3a2afbbb0c9a26f443b6aa13a75b7
Ben Chatelain <ben@octop.ad>
2020-01-17 18:42:15 -0700

    ♻️ Use rsync instead of scp, update hostnames in bundle-pull function

--- .config/fish/functions/bundle-pull.fish
+++ .config/fish/functions/bundle-pull.fish
@@ -1,8 +1,6 @@
-# Runs bundle-pull.rb ruby script
-function bundle-pull
-    # bundle exec ~/.dotfiles/git/bundle-pull.rb $argv
-
-    # New logic
+# Migrated from bundle-pull.rb ruby script
+function bundle-pull \
+        --description='Copies a dirty working copy from one host to another.'
 
     # Ensure current dir is in a clean git repo
     if not git_inside_repo
@@ -10,43 +8,82 @@ function bundle-pull
         return 1
     end
 
-    # if git_repo_dirty
-    #     echo "The work tree is dirty, aborting."
-    #     return 2
-    # end
+    if git_repo_dirty
+        echo "The work tree is dirty, aborting."
+        return 2
+    end
 
     set -l bundle_name snapshot.bundle
-    set -l hostname (hostname)
+    set -l local_username $USER
+    set -l local_hostname (hostname)
+    set -l remote_username
     set -l remote_hostname
-    switch $hostname
+    switch $local_hostname
         case greymatter.local
-            set remote_hostname imac.local
-        case imac.local
-            set remote_hostname greymatter.local
+            set remote_hostname octodec.local
+            set remote_username phatblat
+        case mcoe-am5012227
+            set remote_hostname octodec.local
+            set remote_username phatblat
+        case octodec.local
+            set remote_hostname mcoe-am5012227.local
+            set remote_username ben
         case '*'
-            echo "Unknown hostname: "$hostname
+            echo "Unknown hostname: $local_hostname"
             return 3
     end
 
-    repeatchar -
-    echo remote_hostname $remote_hostname
-
-    set -l username $USER
-    set -l repo_path $PWD
+    # Use a path relative to HOME to avoid user mismatches
+    set -l repo_path (realpath --relative-to=$HOME $PWD)
     set -l current_branch (current-branch)
 
+    repeatchar -
+
+    # Remote
+    echo "Creating bundle on $remote_hostname:$repo_path/$bundle_name"
+    echo
+
     # Create a git bundle file containing the diff of the working copy to HEAD.
-    ssh $username@$remote_hostname 'cd '$repo_path'; and git_bundle_create'
+    ssh $remote_username@$remote_hostname "cd '$repo_path'; and git_bundle_create"
+
+    if test $status -ne 0
+        return $status
+    end
 
     repeatchar -
 
-    # Download the git bundle file using SCP
-    scp $username@$remote_hostname:$repo_path/$bundle_name \
-        $repo_path/$bundle_name
+    echo "Downloading bundle to $local_hostname:$repo_path/$bundle_name"
+    echo
 
+    set -l source $remote_username@$remote_hostname:$repo_path/$bundle_name
+    set -l destination ~/$repo_path/$bundle_name
+
+    # rync preserves paths, scp doesn't
+    rsync --archive \
+        --recursive \
+        --compress \
+        --verbose \
+        $source \
+        $destination
+
+    if test $status -ne 0
+        return $status
+    end
+
+    # Delete the bundle from the remote after successful transfer
+    ssh $remote_username@$remote_hostname "rm --verbose '$repo_path/$bundle_name'"
     git bundle list-heads snapshot.bundle
 
-    # Extract bundle
+    if test $status -ne 0
+        return $status
+    end
+
+    repeatchar -
+
+    # Local
+    echo "Extracting bundle on $local_hostname"
+    echo
+
     # git cherry-pick --no-commit refs/tags/snapshot_end \
     git fetch snapshot.bundle refs/tags/snapshot_end:snapshot
     git tag before_bundle_pull
