99db379f428a444cafa28c42f088d7d8302f14b5
Ben Chatelain <ben@octop.ad>
2020-01-17 18:01:19 -0700

    🐛 Fix logic in git_repo_ functions

--- .config/fish/functions/git_repo_clean.fish
+++ .config/fish/functions/git_repo_clean.fish
@@ -1,4 +1,6 @@
-# Detects a clean work tree.
-function git_repo_clean
-    not git_repo_dirty
+function git_repo_clean \
+        --description='Detects a clean work tree.'
+
+    # Count returns non-zero when no arguments are passed to it (clean repo)
+    not count (git status --porcelain) >/dev/null
 end
--- .config/fish/functions/git_repo_dirty.fish
+++ .config/fish/functions/git_repo_dirty.fish
@@ -1,5 +1,5 @@
-# Detects a dirty work tree.
-function git_repo_dirty
-    # Count returns non-zero when no arguments are passed to it (clean repo)
-    count (git status --porcelain) >/dev/null
+function git_repo_dirty \
+        --description='Detects a dirty work tree.'
+
+    not git_repo_clean
 end
