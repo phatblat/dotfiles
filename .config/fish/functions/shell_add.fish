8e70123ba57548ea7b1a8d42869b9301851b08bd
Ben Chatelain <ben@octop.ad>
2020-02-01 14:09:17 -0700

    ✨ Add linux support to shell_* functions

--- .config/fish/functions/shell_add.fish
+++ .config/fish/functions/shell_add.fish
@@ -1,5 +1,7 @@
-# Register a new shell in /etc/shells
-function shell_add --argument-names new_shell
+function shell_add \
+    --argument-names new_shell \
+    --description='Register a new shell in /etc/shells'
+
     if test -z $new_shell
         echo "Usage: shell_add new_shell"
         return 1
@@ -8,6 +10,11 @@ function shell_add --argument-names new_shell
     set -l system_shells_file /etc/shells
     set -l brew_binaries (brew_home)/bin
     set -l new_shell_path $brew_binaries/$new_shell
+
+    if ! test -f $new_shell_path
+        error "No such file: $new_shell_path"
+        return 2
+    end
 
     # Only care about Homebrew added shells.
     set -l system_shells (grep "^$brew_binaries" $system_shells_file)
