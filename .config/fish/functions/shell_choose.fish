0ff27bfa0849df4da0c351a43e75241448bf0b5c
Ben Chatelain <ben@octop.ad>
2020-02-01 14:09:17 -0700

    ✨ Add linux support to shell_* functions

--- .config/fish/functions/shell_choose.fish
+++ .config/fish/functions/shell_choose.fish
@@ -1,16 +1,28 @@
-# Interactive prompting for choosing a new default shell.
-function shell_choose
+function shell_choose \
+    --description='Interactive prompting for choosing a new default shell.'
+
     set -l shells bash zsh fish
     set -l current_shell (basename $SHELL)
 
     echo "Your default shell is $current_shell, would you like to change it?"
 
-    get --prompt "New shell ($shells):" --default=$current_shell | read -l new_shell
+    # Input
+    read \
+        --prompt-str "New shell ($shells): " \
+        --local new_shell
 
-    if test $new_shell != $current_shell
-        echo "Changing default shell to: "$new_shell
-        shell_switch $new_shell
-    else
+    if test $new_shell = $current_shell
         echo "No changes."
+        return 1
     end
+
+    set -l brew_binaries (brew_home)/bin
+    set -l new_shell_path $brew_binaries/$new_shell
+
+    if ! grep -q $new_shell_path /etc/shells
+        shell_add $new_shell
+    end
+
+    echo "Changing default shell to: $new_shell"
+    shell_switch $new_shell
 end
--- .config/fish/functions/shell_switch.fish
+++ .config/fish/functions/shell_switch.fish
@@ -1,12 +1,21 @@
-# Changes the current $USER's shell using dscl. Outputs only the command to run for non-admins.
-function shell_switch --argument-names new_shell
+function shell_switch \
+    --argument-names new_shell \
+    --description='Changes the current $USER\'s shell using dscl. Outputs only the command to run for non-admins.'
+
     if test -z $new_shell ^/dev/null
         echo "Usage: shell_switch bash|zsh|fish"
         return 1
     end
 
-    set -l shell_path (brew_home)"/bin/$new_shell"
-    set -l cmd "sudo dscl . -change $HOME UserShell $SHELL $shell_path"
+    set -l brew_binaries (brew_home)/bin
+    set -l new_shell_path $brew_binaries/$new_shell
+
+    if is_linux
+        chsh --shell $new_shell_path
+        return $status
+    end
+
+    set -l cmd "sudo dscl . -change $HOME UserShell $SHELL $new_shell_path"
 
     # Get the last path component
     set -l shell_last_path_component (basename $SHELL)
@@ -18,7 +27,7 @@ function shell_switch --argument-names new_shell
             echo
 
             # Switch to new shell and prime the environment
-            eval $shell_path
+            eval $new_shell_path
         else
             echo "Have an admin run the following command:"
             echo "    $cmd"
@@ -28,5 +37,4 @@ function shell_switch --argument-names new_shell
         echo "No changes."
         return
     end
-
 end
