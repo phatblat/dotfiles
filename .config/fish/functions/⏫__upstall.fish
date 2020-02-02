b6229edcd9a32889f0bcd77fee361a1cb15f5994
Ben Chatelain <ben@octop.ad>
2020-02-02 12:57:00 -0700

    ✨ Change upstall to only run requested module(s) using args
    💡 Document upstall functions

--- .config/fish/functions/⏫__upstall.fish
+++ .config/fish/functions/⏫__upstall.fish
@@ -1,6 +1,6 @@
-# Updates and installs system and shell dependencies (utilities, libraries, plugins, apps).
-# Sometimes these are custom forks or configuration to tweak any of these.
-function ⏫__upstall
+function ⏫__upstall \
+    --description='Updates and installs system and shell dependencies such as utilities, libraries, plugins and apps. Sometimes these are custom forks or configuration to tweak any of these.'
+
     createdirs ~/.config/upstall
 
     set -l last_ran_file ~/.config/upstall/last_run.(machine_id)
@@ -30,34 +30,49 @@ function ⏫__upstall
             apt
     end
 
+    # Log the default modules for the os
     echo "all_modules: $all_modules"
+
+    set -l modules_to_run
 
     if contains -- --nothing $argv
         # Smoke test
-        set argv --norb --noxc --nobr --noca --noomf --nofm --nosdk --nopy --nojs ---nopl -novi --notm --novs --noos
+        set modules_to_run \
+            --norb --noxc \
+            --nobr --noca \
+            --noomf --nofm \
+            --nosdk --nopy  \
+            --nojs --nopl \
+            --novi --notm \
+            --novs --noos
     else if test -z "$argv"
         # No args means run all
-        set argv $all_modules
+        set modules_to_run $all_modules
     else if test '--' = (string sub --length 2 -- $argv[1])
-        # Skip flag passed
-        set argv $argv $all_modules
+        # Skip flag passed (upstall --something)
+        set modules_to_run $argv $all_modules
+    else
+        # Run only the requested modules
+        set modules_to_run $argv
     end
 
-    ⬆️__upmodule 🗄__gitconfig
-    ⬆️__upmodule 💎__rubygems    "💎  Ruby Gems"  ruby       --norb $argv
-    ⬆️__upmodule 🔨__xcode       "🔨  Xcode"      xcode      --noxc $argv
-    ⬆️__upmodule 📦__apt         "📦  APT"        apt        --noapt $argv
-    ⬆️__upmodule 🍺__brew        "🍺  Homebrew"   brew       --nobr $argv
-    ⬆️__upmodule 🍻__cask        "🍻  Cask"       cask       --noca $argv
-    ⬆️__upmodule 🐠__omf         "🐠  oh-my-fish" omf        --noomf $argv
-    ⬆️__upmodule 🐟__fisherman   "🐟  Fisherman"  fisherman  --nofm $argv
-    ⬆️__upmodule 🧰__sdkman      "🧰  SDKman"     sdk        --nosdk $argv
-    ⬆️__upmodule 🐍__pip         "🐍  PIP"        pip        --nopy $argv
-    ⬆️__upmodule 🕸__npm         "🕸  NPM"        npm        --nojs $argv
-    ⬆️__upmodule ▶️__powerline    "▶️  Powerline"   powerline  --nopl $argv
-    ⬆️__upmodule 🗒__vundle      "🗒  Vundle"     vundle     --novi $argv
-    ⬆️__upmodule 📝__textmate    "📝  TextMate"   textmate   --notm $argv
-    ⬆️__upmodule ⚛️__apm          "⚛️  APM"         apm        --noap $argv
-    ⬆️__upmodule 🆚__vscode      "🆚  VS Code"    vscode     --novs $argv
-    ⬆️__upmodule 🖥__macos       "🖥  macOS"      macos      --noos $argv
+    echo "modules_to_run: $modules_to_run"
+
+    ⬆️__upmodule 🗄__gitconfig "🗄  Git config" git        --nog $modules_to_run
+    ⬆️__upmodule 💎__rubygems  "💎  Ruby Gems"  ruby       --norb $modules_to_run
+    ⬆️__upmodule 🔨__xcode     "🔨  Xcode"      xcode      --noxc $modules_to_run
+    ⬆️__upmodule 📦__apt       "📦  APT"        apt        --noapt $modules_to_run
+    ⬆️__upmodule 🍺__brew      "🍺  Homebrew"   brew       --nobr $modules_to_run
+    ⬆️__upmodule 🍻__cask      "🍻  Cask"       cask       --noca $modules_to_run
+    ⬆️__upmodule 🐠__omf       "🐠  oh-my-fish" omf        --noomf $modules_to_run
+    ⬆️__upmodule 🐟__fisherman "🐟  Fisherman"  fisherman  --nofm $modules_to_run
+    ⬆️__upmodule 🧰__sdkman    "🧰  SDKman"     sdk        --nosdk $modules_to_run
+    ⬆️__upmodule 🐍__pip       "🐍  PIP"        pip        --nopy $modules_to_run
+    ⬆️__upmodule 🕸__npm       "🕸  NPM"        npm        --nojs $modules_to_run
+    ⬆️__upmodule ▶️__powerline  "▶️  Powerline"   powerline  --nopl $modules_to_run
+    ⬆️__upmodule 🗒__vundle    "🗒  Vundle"     vundle     --novi $modules_to_run
+    ⬆️__upmodule 📝__textmate  "📝  TextMate"   textmate   --notm $modules_to_run
+    ⬆️__upmodule ⚛️__apm        "⚛️  APM"         apm        --noap $modules_to_run
+    ⬆️__upmodule 🆚__vscode    "🆚  VS Code"    vscode     --novs $modules_to_run
+    ⬆️__upmodule 🖥__macos     "🖥  macOS"      macos      --noos $modules_to_run
 end
--- .config/fish/functions/⬆️__upmodule.fish
+++ .config/fish/functions/⬆️__upmodule.fish
@@ -1,39 +1,50 @@
-# Optionally invokes an upstall module, provided the "skip" flag(s) are not given.
-# Requires either 1 arg (no include/skip options), or 4+ args (include/skip
-# flags & title)
-function ⬆️__upmodule --argument-names module_function display_name include_flag skip_flag
-    if test -z "$argv"
-        echo "Usage: ⬆️__upmodule module_function [display_name include_flag skip_flag original_args]"
-        return 1
-    else if test (count $argv) -eq 1
-        if not functions --query $module_function
-            echo "Unknown function: "$module_function
-            return 3
-        end
+function ⬆️__upmodule \
+    --description='Optionally invokes an upstall module, provided the "skip" flag(s) are not given. Requires either 1 arg (no include/skip options), or 4+ args (include/skip flags & title)' \
+    --argument-names \
+        module_function \
+        display_name \
+        include_flag \
+        skip_flag
 
+    # no args
+    if test -z "$argv"
+        error "Usage: ⬆️__upmodule module_function [display_name include_flag skip_flag original_args]"
+        return 1
+    end
+
+    if not functions --query $module_function
+        echo "Unknown function: "$module_function
+        return 2
+    end
+
+    # 1 arg
+    if test (count $argv) -eq 1
         repeatchar -
         eval $module_function
+
+    # 4+ args
     else if test (count $argv) -ge 4
         if not functions --query $module_function
             echo "Unknown function: "$module_function
             return 4
         end
 
+        # args 5+ are the original args passed to upstall
         if test (count $argv) -ge 5
             set original_args $argv[5..-1]
         end
 
-        repeatchar -
-
         if contains -- $skip_flag $original_args
             # Skip module if skip flag was given
+            repeatchar -
             echo $display_name" (skipped)"
         else if contains -- $include_flag $original_args
             # Run module if asked for
+            repeatchar -
             eval $module_function
         else
             # Otherwise, skip
-            echo $display_name" (skipped)"
+            # echo $display_name" (skipped)"
         end
     else
         echo "Usage: ⬆️__upmodule module_function [display_name include_flag skip_flag original_args]"
