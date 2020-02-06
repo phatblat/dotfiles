18e476f8ba481e7614803ac314a8c55ff61bdb7c
Ben Chatelain <ben@octop.ad>
2020-02-05 22:59:01 -0700

    i✨ Add swift_pgp_key_import and swift_verify functions 🕊 Add swift 5 release signing key

---
+++ .config/fish/functions/swift_verify.fish
@@ -0,0 +1,11 @@
+# https://swift.org/download/#installation-1
+function swift_verify \
+    --description='swift_verify' \
+    --argument-names tarball
+
+    # # https://swift.org/download/#installation-1
+    gpg --keyserver hkp://pool.sks-keyservers.net --refresh-keys Swift
+
+    # Then, use the signature file to verify that the archive is intact:
+    gpg --verify $tarball.sig
+end
