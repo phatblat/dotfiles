3f6a101c2310fe02696d819dc20aac86117e64a1
Ben Chatelain <ben@octop.ad>
2020-02-08 16:04:25 -0700

    ♻️🗄 Change rewrite to use git-filter-repo command
    Functionality limited to email only. That's all I've been using this for lately

--- .config/fish/functions/rewrite.fish
+++ .config/fish/functions/rewrite.fish
@@ -1,49 +1,25 @@
-function rewrite --argument-names field old_value new_value \
---description='Rewrite commits changing author/committer name/email/time environment variables.'
+function rewrite \
+    --argument-names field old_value new_value \
+    --description='Rewrite commits changing author/committer name/email/time environment variables.'
+
     if test (count $argv) -ne 3
-        echo "Usage: rewrite author|committer|both|date|commit_date|dates old|commit new"
+        error "Usage: rewrite email old_value new_value"
         return 1
     end
 
-    switch $field
-    case author
-        set find_attribute GIT_AUTHOR_EMAIL
-        set modify_attributes GIT_AUTHOR_EMAIL
-    case committer
-        set find_attribute GIT_COMMITTER_EMAIL
-        set modify_attributes GIT_COMMITTER_EMAIL
-    case both
-        set find_attribute GIT_AUTHOR_EMAIL
-        set modify_attributes GIT_AUTHOR_EMAIL GIT_COMMITTER_EMAIL
-    case 'date'
-        set find_attribute GIT_COMMIT
-        set modify_attributes GIT_AUTHOR_DATE
-    case commit_date
-        set find_attribute GIT_COMMIT
-        set modify_attributes GIT_COMMITTER_DATE
-    case dates
-        set find_attribute GIT_COMMIT
-        set modify_attributes GIT_AUTHOR_DATE GIT_COMMITTER_DATE
-    case '*'
-        echo "Usage: rewrite author|committer|both|date|commit_date|dates old|commit new"
+    if test $field != "email"
+        error "Only email replacements are currently supported."
+        error "Usage: rewrite email old_value new_value"
         return 2
     end
 
-    # Check if a commit was specified
-    if git rev-parse "$old_value" ^/dev/null
-        set find_attribute GIT_COMMIT
+    if not command --search git-filter-repo > /dev/null
+        error "git-filter-repo is installed"
+        return 3
     end
 
-    # Filter command is eval'd using sh
-    set -l filter_command "if [[ \$$find_attribute == '$old_value' ]]; then export"
-    for attribute in $modify_attributes
-        set filter_command "$filter_command $attribute='$new_value';"
-    end
-    set filter_command "$filter_command fi"
-
-    echo "Filter command:"
-    echo $filter_command
-
-    # Modify commits
-    git filter-branch --force --env-filter $filter_command
+    # https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#CALLBACKS
+    git filter-repo \
+        --email-callback \
+        "return email.replace(b\"$old_value\", b\"$new_value\")"
 end
