471d2a03c9a2e6c3c8a901c53c262a561588982c
Ben Chatelain <ben.d.chatelain@kp.org>
2020-02-12 06:49:29 -0700

    🎨 Formatting for fish prompt functions

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,4 +1,6 @@
-function fish_mode_prompt --description 'Displays the current mode'
-	# To reuse the mode indicator use this function instead
+function fish_mode_prompt \
+    --description 'Displays the current VI mode'
+
+    # To reuse the mode indicator use this function instead
     # fish_default_mode_prompt
 end
