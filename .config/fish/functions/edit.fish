c34f641aea7f43377580b10b3e3e5ce5f9130aef
Ben Chatelain <ben.d.chatelain@kp.org>
2020-03-15 09:52:57 -0600

    💡 Add description to edit function

--- .config/fish/functions/edit.fish
+++ .config/fish/functions/edit.fish
@@ -1,6 +1,6 @@
-# Edit using the configured VISUAL editor (TextMate) for GUI terminal sessions
-# or EDITOR for CLI (SSH) sessions.
-function edit
+function edit \
+    --description="Edit using the configured VISUAL editor (TextMate) for GUI terminal sessions or EDITOR for CLI (SSH) sessions."
+
     if test -z $VISUAL
         eval "$EDITOR '$argv'"
     else
