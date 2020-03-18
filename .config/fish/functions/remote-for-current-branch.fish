ab5e5c6b55f70b88e46c7ccaf9e25ab6bd104879
Ben Chatelain <ben.d.chatelain@kp.org>
2020-03-18 09:50:38 -0600

    🚚 Rename remote_for_current_branch function

--- .config/fish/functions/remote-for-current-branch.fish
+++
@@ -1,5 +0,0 @@
-# Displays the name of the remote for the current branch.
-function remote-for-current-branch
-    set current_branch (git rev-parse --abbrev-ref HEAD)
-    config branch.$current_branch.remote
-end
