74dbd7cff80666167083df5c66549644d32e563c
Ben Chatelain <ben.d.chatelain@kp.org>
2020-03-20 13:29:44 -0600

    🌱 Add mint to upstall

---
+++ .config/fish/functions/🌱_mint.fish
@@ -0,0 +1,53 @@
+# Sequencing
+# - Requires mint.
+function 🌱_mint \
+    --description='Updates Mint and installed packages.'
+
+    echo "🌱 Mint - https://github.com/yonaskolb/Mint"
+    echo
+
+    # List of packages to upstall
+    set -l packages \
+        JamitLabs/Accio \
+
+    set -l uninstall
+
+    # --------------------------------------------------------------------------
+    #
+    # Preflight Checks
+    #
+    # --------------------------------------------------------------------------
+
+    # Ensure Mint is installed.
+    if not type -q mint
+        error "Mint is not installed"
+        return 1
+    end
+
+    mint list
+
+    # --------------------------------------------------------------------------
+    #
+    # Uninstall
+    #
+    # --------------------------------------------------------------------------
+
+    # Uninstall unwanted packages
+    set -l to_uninstall
+    for package in $uninstall
+        echo 🗑️ Uninstalling $to_uninstall
+        mint uninstall $to_uninstall
+    end
+
+    # --------------------------------------------------------------------------
+    #
+    # Upstall
+    #
+    # --------------------------------------------------------------------------
+
+    # Mint has no update command, so force install all packages
+    for package in $packages
+        echo 🆕 Installing: $package
+        mint install --force --verbose $package
+    end
+end
