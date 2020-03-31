65968c14e045e3b7bf9dcc24f46f714e8dd82a37
Ben Chatelain <ben.d.chatelain@kp.org>
2020-03-31 11:32:32 -0600

    🎨 Configure review to use the same options as d

--- .config/fish/functions/d.fish
+++ .config/fish/functions/d.fish
@@ -6,13 +6,13 @@ function d \
     # --no-indent-heuristic: Disable the default heuristic that shifts diff hunk boundaries to make patches easier to read.
     git diff \
         --unified=1 \
+        --no-prefix \
         $argv
 
         # word diff conflicts with diff-so-fancy
         # --word-diff=color \
         # --word-diff-regex='[^[:space:]]' \
         # --diff-algorithm=default \
-        # --no-prefix \
         # --no-indent-heuristic \
         # --ignore-cr-at-eol \
 end
