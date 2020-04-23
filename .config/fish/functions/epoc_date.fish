84781ea1fc287c99b34543e11bd5f4bf863b384a
Ben Chatelain <ben@octop.ad>
2020-04-23 12:23:28 -0600

    ✨ Add epoc_date function

---
+++ .config/fish/functions/epoc_date.fish
@@ -0,0 +1,10 @@
+function epoc_date \
+    --description='Converts epoc timestamps to a date' \
+    --argument-names epoc_timestamp
+
+    # macos format
+    # date -r 1587570452
+
+    # coreutils format
+    date -d @$epoc_timestamp --rfc-3339=date
+end
