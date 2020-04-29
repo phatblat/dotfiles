f1696bb476fedb36d290134de22b24225a07b8c8
Ben Chatelain <ben@octop.ad>
2020-04-29 13:30:54 -0600

    🛠 Add CLI tools version to xcvallc

--- .config/fish/functions/xcvall.fish
+++ .config/fish/functions/xcvall.fish
@@ -1,3 +1,7 @@
 function xcvall --description='Lists versions of all installed copies of Xcode.'
     mdfind kMDItemCFBundleIdentifier=com.apple.dt.Xcode
+
+    echo
+    echo "CLI tools"
+    pkgutil --pkg-info=com.apple.pkg.CLTools_Executables
 end
