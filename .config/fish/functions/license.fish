d151289bd64218d7591f861b8648a4906a1565fe
Ben Chatelain <ben@octop.ad>
2020-05-06 06:53:03 -0600

    🎨 Use H2 header for License block of readme

--- .config/fish/functions/license.fish
+++ .config/fish/functions/license.fish
@@ -27,7 +27,7 @@ SOFTWARE.
 
     # Append license info to README.md
     echo -n "
-# 📄 License
+## 📄 License
 
 This repo is licensed under the MIT License. See the [LICENSE](LICENSE.md) file for rights and limitations.
 " >> README.md
