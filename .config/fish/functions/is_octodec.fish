dc65e51432243abc52e7d1663aeffa7b1c3a09d4
Ben Chatelain <ben@octop.ad>
2020-05-25 11:56:22 -0600

    🐛 Change is_octodec to check for substring

--- .config/fish/functions/is_octodec.fish
+++ .config/fish/functions/is_octodec.fish
@@ -1,5 +1,5 @@
 function is_octodec \
     --description="Tests whether the current computer is octodec"
 
-    test (hostname) = "octodec.local"
+    string match --entire --quiet -- "octodec" (hostname)
 end
