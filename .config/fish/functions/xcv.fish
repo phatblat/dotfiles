4509ce53e8bf46ceced3f72cdb1d9ba2fa994992
Ben Chatelain <ben@octop.ad>
2020-08-25 19:22:26 -0600

    🛠 Add Xcode 12 beta 6

--- .config/fish/functions/xcv.fish
+++ .config/fish/functions/xcv.fish
@@ -145,6 +145,8 @@ function xcv \
             set beta_version "beta 4 "
         case 12A8189h
             set beta_version "beta 5 "
+        case 12A8189n
+            set beta_version "beta 6 "
     end
 
     echo "$marketing_version $beta_version($build_version)"
