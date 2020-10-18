70b8ce7bcf16f6afd6f816900f25fc4b2230a5d8
Ben Chatelain <ben@octop.ad>
2020-10-18 14:20:01 -0600

    ✨ Add pkgexpand function

---
+++ .config/fish/functions/pkgexpand.fish
@@ -0,0 +1,16 @@
+function pkgexpand \
+    --description='Expands a pkg file' \
+    --argument-names package dest_dir
+
+    if test -z $package
+        echo 'Usage: pkgexpand package dest_dir'
+        return 1
+    end
+
+    if test -z $dest_dir
+        echo 'Usage: pkgexpand package dest_dir'
+        return 2
+    end
+
+    pkgutil --expand "$package" "$dest_dir"
+end
