7e300e04e6430eb9abd462b25a81a27b34b99459
Ben Chatelain <ben@octop.ad>
2020-10-18 13:17:52 -0600

    ✨ Add 57 and 114 sizes to appicon

--- .config/fish/functions/appicon.fish
+++ .config/fish/functions/appicon.fish
@@ -10,7 +10,7 @@ function appicon \
         return 2
     end
 
-    set -l all_sizes 20 29 40 58 60 80 76 87 120 152 167 180 1024
+    set -l all_sizes 20 29 40 57 58 60 80 76 87 114 120 152 167 180 1024
 
     for size in $all_sizes
         set -l new_file AppIcon-$size.png
