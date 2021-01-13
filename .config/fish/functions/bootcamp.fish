df2be3842bfa8bed99af5d82bdbe9718cb580299
Ben Chatelain <ben@octop.ad>
2021-01-13 12:17:15 -0700

    ✨ Add bootcamp function

---
+++ .config/fish/functions/bootcamp.fish
@@ -0,0 +1,7 @@
+function bootcamp \
+    --description='Restarts to Windows bootcamp'
+
+    # https://apple.stackexchange.com/questions/9492/how-to-quickly-reboot-from-osx-to-windows-and-back
+    /usr/sbin/bless -mount /Volumes/BOOTCAMP --setBoot --nextonly
+    and restart
+end
