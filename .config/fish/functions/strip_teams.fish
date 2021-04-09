c868223062ef4e0c91b517b1a6e2cd8e26d1308e
Ben Chatelain <ben@octop.ad>
2021-04-08 20:01:57 -0700

    ✨ Add strip_teams function

---
+++ .config/fish/functions/strip_teams.fish
@@ -0,0 +1,16 @@
+function strip_teams \
+    --description='Strip codesign from the Teams app' \
+    --argument-names path
+
+    set -l teams_path "/Applications/Microsoft Teams.app"
+
+    # 1st arg overrides default path
+    if test -n "$path"
+        set teams_path $path
+    end
+
+    # https://support.ecamm.com/en/articles/4343963-virtual-camera-missing-after-microsoft-teams-update
+    sudo codesign \
+        --verbos \
+        --remove-signature "$teams_path/Contents/Frameworks/Microsoft Teams Helper (Renderer).app"
+end
