38599452411b0ea7dc6837428790925c6fe9b6c6
Ben Chatelain <ben@octop.ad>
2021-04-14 23:06:01 -0600

    🐛 Fix fileowner for dirs

--- .config/fish/functions/fileowner.fish
+++ .config/fish/functions/fileowner.fish
@@ -14,7 +14,7 @@ function fileowner \
         else
             # macOS args: -c '%U'
             # stat -c '%U' $file
-            ls -l $file | cut -d' ' -f4
+            ls -ld $file | cut -d' ' -f4
         end
     else if is_linux
         stat --format=%U $file
