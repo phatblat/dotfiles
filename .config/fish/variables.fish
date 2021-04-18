f6087b45abf6f3ee87ee2f1fe7e9b2efffc4c527
Ben Chatelain <ben@octop.ad>
2021-04-18 10:34:36 -0600

    🎨 Clean up path syntax in variables

--- .config/fish/variables.fish
+++ .config/fish/variables.fish
@@ -9,11 +9,11 @@ set --export KERNEL (uname)
 
 # Upper case
 if is_mac
-    set --export ANDROID_HOME ~/Library/Android/sdk
+    set --export ANDROID_HOME $HOME/Library/Android/sdk
     set --export ANDROID_SDK_ROOT $ANDROID_HOME
     set --export ANDROID_NDK_HOME (brew_home)/share/android-ndk
 else if is_linux
-    set --export ANDROID_HOME ~/Android/Sdk
+    set --export ANDROID_HOME $HOME/Android/Sdk
     set --export ANDROID_SDK_ROOT $ANDROID_HOME
     set --export ANDROID_NDK_HOME
 end
@@ -31,13 +31,13 @@ set --export HOMEBREW_BAT (command --search bat)
 set --export HOMEBREW_CLEANUP_MAX_AGE_DAYS 30
 set --export ICLOUD_HOME $HOME"/Library/Mobile Documents"
 set --export ICLOUD_DRIVE $ICLOUD_HOME"/com~apple~CloudDocs"
-set --export JABBA_HOME ~/.jabba
+set --export JABBA_HOME $HOME/.jabba
 set --export LANG en_US.UTF-8
 set --export LANGUAGE en_US.UTF-8
 set --export LC_ALL en_US.UTF-8
 set --export LC_CTYPE en_US.UTF-8
 set --export OPENSSL_PATH (brew_home openssl)/bin/openssl
-set --export PING_IDENTITY_DEVOPS_HOME /Users/phatblat/dev/ping/devops
+set --export PING_IDENTITY_DEVOPS_HOME $HOME/dev/ping/devops
 set --export PING_IDENTITY_DEVOPS_REGISTRY docker.io/pingidentity
 set --export PING_IDENTITY_DEVOPS_TAG edge
 set --export PING_IDENTITY_ACCEPT_EULA Y
@@ -51,7 +51,7 @@ set --export LSCOLORS ExFxBxDxCxegedabagacad
 # Lower case
 set --export github_user phatblat
 set --export powerline_enabled 0
-set --global --export sdkman_prefix ~/.sdkman
+set --global --export sdkman_prefix $HOME/.sdkman
 
 # Fix spacing for emoji and ambiguous characters
 set -xg fish_emoji_width 2
@@ -103,8 +103,7 @@ set --global fish_user_paths \
 
 # PATH
 set --export --global PATH \
-    ./bin \
-    ~/bin \
+    $HOME/bin \
     (brew_home)/bin \
     (brew_home curl)/bin \
     (brew_home python)/libexec/bin \
@@ -120,13 +119,13 @@ if test -d (brew_home python)/libexec/bin
         (brew_home python)/libexec/bin
 end
 
-set --export --global SWIFT_TOOLCHAIN ~/src/swift/current
+set --export --global SWIFT_TOOLCHAIN $HOME/src/swift/current
 if test -d "$SWIFT_TOOLCHAIN"
     set --export --global PATH $PATH \
         $SWIFT_TOOLCHAIN/usr/bin
 end
 
-set --export --global SWIFTENV_ROOT ~/.swiftenv
+set --export --global SWIFTENV_ROOT $HOME/.swiftenv
 set --export --global PATH $SWIFTENV_ROOT/bin $PATH
 if which swiftenv > /dev/null
     status --is-interactive
@@ -141,7 +140,7 @@ if test -d "$ANDROID_HOME"
         $PATH
 end
 
-set --export --global CHROME_DEPOT_TOOLS ~/dev/chromium/depot_tools
+set --export --global CHROME_DEPOT_TOOLS $HOME/dev/chromium/depot_tools
 if test -d "$CHROME_DEPOT_TOOLS"
     set --export --global PATH $PATH \
         $CHROME_DEPOT_TOOLS
@@ -151,15 +150,15 @@ end
 # https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md#budspencer
 if test (uname -s) = "Darwin"
     if is_coreutils
-        set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-        set -gx PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+        set --export --global PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+        set --export --global PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
     end
 end
 
 # MongoDB Realm
-set --export --global MONGODB_HOME ~/dev/mongodb/current
+set --export --global MONGODB_HOME $HOME/dev/mongodb/current
 if test -d "$MONGODB_HOME"
-    set -xg PATH \
+    set --export --global PATH \
         "$MONGODB_HOME/bin" \
         $PATH
 end
