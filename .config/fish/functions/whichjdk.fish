2b8b9c94339e59c312cd10fb4e8ad5548d1d4d2e
Ben Chatelain <ben@octop.ad>
2021-04-19 21:25:56 -0600

    ♻️ Add support for SDKman to whichjdk

--- .config/fish/functions/whichjdk.fish
+++ .config/fish/functions/whichjdk.fish
@@ -1,7 +1,14 @@
 # Displays path to the current JDK.
 function whichjdk
     if is_mac
-        /usr/libexec/java_home $argv
+        echo JAVA_HOME: $JAVA_HOME
+
+        if string match --entire --quiet /Library/Java/JavaVirtualMachines $JAVA_HOME
+            # System JVMs
+            /usr/libexec/java_home $argv
+        end
+
+        java -version
     else if is_linux
         java -version
     end
