39402f03dc487a8045d5be3ca5f3b279e1bbb012
Ben Chatelain <ben@octop.ad>
2021-05-12 14:03:45 -0600

    🔥 Remove fish prompt and title overrides

--- .config/fish/functions/fish_mode_prompt.fish
+++
@@ -1,6 +0,0 @@
-function fish_mode_prompt \
-    --description 'Displays the current VI mode'
-
-    # To reuse the mode indicator use this function instead
-    # fish_default_mode_prompt
-end
--- .config/fish/functions/fish_postexec.fish
+++
@@ -1,9 +0,0 @@
-# Custom function for fish_postexec event
-function fish_postexec --on-event fish_postexec
-    # Export CMD_DURATION so that powerline can access it
-    set --export CMD_DURATION $CMD_DURATION
-
-    # DISABLED:
-    # Switch back to vi command (default) mode after each terminal command.
-    # fish_vi_key_bindings insert
-end
