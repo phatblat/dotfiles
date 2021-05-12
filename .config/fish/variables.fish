82ecc0db7a23ba8a562b68c8356691ed99f6e1cc
Ben Chatelain <ben@octop.ad>
2021-05-12 14:02:36 -0600

    🐠🎨 Switch theme to bobthefish

--- .config/fish/variables.fish
+++ .config/fish/variables.fish
@@ -58,40 +58,55 @@ set -xg fish_emoji_width 2
 set -xg fish_ambiguous_width 2
 
 # OMF bobthefish theme options
-# https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md#bobthefish
+# https://github.com/oh-my-fish/theme-bobthefish#readme
 set -g theme_display_git yes
 set -g theme_display_git_dirty yes
 set -g theme_display_git_untracked yes
 set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose yes
 set -g theme_display_git_stashed_verbose yes
-set -g theme_display_git_master_branch yes
+set -g theme_display_git_default_branch yes
+set -g theme_git_default_branches master main
 set -g theme_git_worktree_support yes
+set -g theme_use_abbreviated_branch_name yes
 set -g theme_display_vagrant yes
-set -g theme_display_docker_machine no
+set -g theme_display_docker_machine yes
 set -g theme_display_k8s_context yes
+set -g theme_display_k8s_namespace yes
 set -g theme_display_hg yes
-set -g theme_display_virtualenv no
-set -g theme_display_ruby no
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby yes
 set -g theme_display_user ssh
 set -g theme_display_hostname ssh
 set -g theme_display_vi yes
-set -g theme_display_nvm yes
-set -g theme_display_date no
+set -g theme_display_node yes
+set -g theme_display_date yes
 set -g theme_display_cmd_duration yes
 set -g theme_title_display_process yes
 set -g theme_title_display_path no
 set -g theme_title_use_abbreviated_path no
 set -g theme_date_format "+%a %H:%M"
+# set -g theme_date_timezone America/Los_Angeles
 set -g theme_avoid_ambiguous_glyphs yes
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
 set -g default_user phatblat
-set -g theme_color_scheme gruvbox
+# Color options
+# dark. The default bobthefish theme.
+# light. A lighter version of the default theme.
+# solarized (or solarized-dark), solarized-light. Dark and light variants of Solarized.
+# base16 (or base16-dark), base16-light. Dark and light variants of the default Base16 theme.
+# zenburn. An adaptation of Zenburn.
+# gruvbox. An adaptation of gruvbox.
+# dracula. An adaptation of dracula.
+# nord. An adaptation of nord.
+set -g theme_color_scheme dracula
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor no
+set -g theme_newline_prompt '$ '
 
 # OMF danger theme options
 # https://github.com/oh-my-fish/theme-dangerous#readme
