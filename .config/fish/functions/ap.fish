f0d35813b1d69742e9e1287e488194b0a35f1c0c
Ben Chatelain <ben@octop.ad>
2021-06-21 14:12:16 -0600

    ⏳ Add toggle_wait calls to ap

--- .config/fish/functions/ap.fish
+++ .config/fish/functions/ap.fish
@@ -1,5 +1,7 @@
 function ap \
     --description='Selectively add some of the modifications in the work tree to the git staging area.'
 
+    toggle_wait on
     git add --patch $argv
+    toggle_wait off
 end
