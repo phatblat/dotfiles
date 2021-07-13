db6ada403908bfa1a70acca8f63154830a6fa4ca
Ben Chatelain <ben@octop.ad>
2021-07-13 10:15:32 -0600

    🖥 Add clone to masd

--- .config/fish/functions/masd.fish
+++ .config/fish/functions/masd.fish
@@ -1,4 +1,18 @@
 function masd \
-        --description='Quick nav to mas dir'
-    nav ~/dev/mas-cli/mas
+    --description='Quick nav to mas dir'
+
+    set -l repo_url git@github.com:mas-cli/mas.git
+    set -l base_dir ~/dev/mas-cli
+    set -l local_dir $base_dir/mas
+
+    if test -d $base_dir/mas
+        rmdir $base_dir/mas ^| echo /dev/null
+    end
+
+    # Create parent directories
+    createdirs $base_dir
+
+    clone_or_pull $local_dir $repo_url
+
+    nav $local_dir
 end
