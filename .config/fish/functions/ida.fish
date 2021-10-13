21f3c92061eadac224bd17a4c68b94634127ce99
Ben Chatelain <ben@octop.ad>
2021-10-12 23:18:43 -0600

    ✨ Add ida command

---
+++ .config/fish/functions/ida.fish
@@ -0,0 +1,12 @@
+function ida \
+    --description='Lauch IDA with elevated priveleges'
+
+    set -l ida_path "/Applications/IDA Pro 7.5"
+
+    if not test -d "$ida_path"
+        error 'IDA is not installed'
+        return 1
+    end
+
+    sudo $ida_path/idabin/ida64 $argv
+end
