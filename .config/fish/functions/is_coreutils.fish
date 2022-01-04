e9b6d3353449a90e3bffa782319374e0ece91f96
Ben Chatelain <ben@octop.ad>
2022-01-04 08:19:08 -0700

    🚩 Fix is_coreutils to false

--- .config/fish/functions/is_coreutils.fish
+++ .config/fish/functions/is_coreutils.fish
@@ -1,5 +1,6 @@
 function is_coreutils \
     --description='Tests whether coreutils is installed'
 
-    brew list coreutils >/dev/null 2>&1
+    # brew list coreutils >/dev/null 2>&1
+    return 1
 end
