161f4e0ee3847f8f99156470f90be803c573a92d
Ben Chatelain <ben@octop.ad>
2022-01-08 16:40:38 -0700

    ♻️ Only upstall npm packages as needed

--- .config/fish/functions/🕸_npm.fish
+++ .config/fish/functions/🕸_npm.fish
@@ -3,26 +3,16 @@
 # Sequencing
 # - After: brew (installed with node)
 function 🕸_npm \
-    --description='Installs and updates npm modules.'
+    --description='Installs and updates npm packages.'
 
-    echo "🕸 NPM"
+    echo "🕸 NPM - https://www.npmjs.com/"
     echo
 
-    # Ensure NPM is installed.
-    if not type -q npm
-        error "NPM is not installed."
-        return 1
-    end
-
-    # Verify the user owns the node_modules dir.
-    # Package location - /Users/phatblat/.nvm/versions/node/v17.3.0
-    set -l global_modules (npm get prefix)
-    if test $USER != (fileowner $global_modules)
-        if status is-login
-            echo "You must be the owner of "$global_modules" to run this command."
-        end
-        return 1
-    end
+    # --------------------------------------------------------------------------
+    #
+    # Configuration
+    #
+    # --------------------------------------------------------------------------
 
     set -l global_packages \
         atlas-connect \
@@ -44,31 +34,116 @@ function 🕸_npm \
         ts2kt \
         ubolt
 
-    set -l installed_packages (npm list --global --depth=0)
-    # Example output:
-    # /usr/local/lib
-    # ├── fast-cli@0.2.1
-    # ├── n@2.1.7
-    # ├── npm@4.5.0
-    # └── ralio@0.6.1
-    # Manual installation check
-    # string match "*ralio*" $installed_packages[5]
-
-    echo "Updating NPM"
-    npm install npm@latest --global
-
-    # Uninstall
-    npm uninstall --global \
+    set -l uninstall \
         ralio \
         ngrok \
         realm-object-server \
         tslint
 
-    # Update global packages using npm-check-updates
-    ncu -g
+    # --------------------------------------------------------------------------
+    #
+    # Preflight Checks
+    #
+    # --------------------------------------------------------------------------
 
-    # TODO: Refactor to only install missing packages
-    npm install --global $global_packages
+    # Ensure NPM is installed.
+    if not type -q npm
+        error "NPM is not installed."
+        return 1
+    end
+
+    # Verify the user owns the node_modules dir.
+    # Package location - /Users/phatblat/.nvm/versions/node/v17.3.0
+    set -l global_modules (npm get prefix)
+    if test $USER != (fileowner $global_modules)
+        if status is-login
+            echo "You must be the owner of "$global_modules" to run this command."
+        end
+        return 1
+    end
+
+    # --------------------------------------------------------------------------
+    #
+    # self-update
+    #
+    # --------------------------------------------------------------------------
+
+    echo "⬆️ Updating NPM"
+    npm install npm@latest --global
+
+    # --------------------------------------------------------------------------
+    #
+    # Uninstall
+    #
+    # --------------------------------------------------------------------------
+
+    # Uninstall unwanted packages
+    set -l to_uninstall
+    for package in $uninstall
+        if contains $package $installed_packages
+            set to_uninstall $to_uninstall $package
+        end
+    end
+    if test -n "$to_uninstall"
+        echo 🗑️  Uninstalling $to_uninstall
+        npm uninstall --global $to_uninstall
+    end
+
+    # --------------------------------------------------------------------------
+    #
+    # Upstall
+    #
+    # --------------------------------------------------------------------------
+
+    set -l outdated_json (npm outdated --global --json)
+    if test ! "$outdated_json" = "{}"
+        set -l outdated_packages (\
+            eval $outdated_json \
+            jq --raw-output 'keys | .[] | "\(.)"'\
+        )
+
+        echo
+        echo "👵🏻 Outdated: $outdated_packages"
+
+        # npm outdated --global
+
+        # Update global packages using npm-check-updates
+        # ncu --global
+
+        for package in $outdated_packages
+            npm update --global $package
+        end
+    end
+
+    # List only top-level dependencies
+    set -l installed_packages (\
+        npm list --global --depth=0 --json | \
+        jq --raw-output '.dependencies | keys | .[] | "\(.)"'\
+    )
+
+    echo
+    echo "📦 Installed packages: $installed_packages"
+
+    # Install new packages not already installed.
+    set -l not_installed
+    for package in $global_packages
+        if not contains $package $installed_packages
+            set not_installed $not_installed $package
+        end
+    end
+    if test -n "$not_installed"
+        for package in $not_installed
+            echo
+            echo 🆕  Installing: $package
+            npm install --global $package
+        end
+    end
+
+    # --------------------------------------------------------------------------
+    #
+    # Post Install
+    #
+    # --------------------------------------------------------------------------
 
     echo
     echo 👩🏻‍⚕️  Doctor
