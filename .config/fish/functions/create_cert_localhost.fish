92b7ccfdd49855a367c6e99053f48a2e7e381341
Ben Chatelain <ben@octop.ad>
2022-01-10 19:48:04 -0700

    🚧 Add create_cert_localhost

---
+++ .config/fish/functions/create_cert_localhost.fish
@@ -0,0 +1,18 @@
+# https://stackoverflow.com/questions/8169999/how-can-i-create-a-self-signed-cert-for-localhost
+function create_cert_localhost \
+    --description='Create a self-signed certificate for localhost'
+
+    echo "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" |\
+    openssl req -x509 \
+        -out localhost.crt \
+        -keyout localhost.key \
+        -newkey rsa:2048 \
+        -nodes \
+        -sha256 \
+        -subj '/CN=localhost' \
+        -extensions EXT \
+        -config
+
+        # https://www.humankode.com/ssl/create-a-selfsigned-certificate-for-nginx-in-5-minutes
+        # sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config localhost.conf
+end
