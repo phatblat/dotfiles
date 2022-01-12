84b16ba6a43bbeada7478dcaaa45d7a3b641cf38
Ben Chatelain <ben@octop.ad>
2022-01-11 17:06:17 -0700

    🔇 Prevent fish shell errors when workspace not found

--- .config/fish/functions/xc.fish
+++ .config/fish/functions/xc.fish
@@ -2,17 +2,29 @@ function xc \
     --description='Xcode wrapper function'
 
     if test -f Package.swift
+        echo "Opening swift package"
         open Package.swift $argv
         return
     end
 
-    if test -d *.xcworkspace
-        open *.xcworkspace $argv
+    # Ignore workspaces inside xcodeproj bundle
+    set -l workspaces (string match --entire --invert project.xcworkspace **.xcworkspace)
+    list $workspaces | column -c 1
+    if test -d $workspaces[1]
+        echo "Opening first workspace"
+        open $workspaces[1] $argv
         return
     end
 
-    if test -d *.xcodeproj
-        open *.xcodeproj $argv
+    # Ignore CocoaPods projects
+    set -l projects (string match --entire --invert Pods.xcodeproj **.xcodeproj)
+    list $projects | column -c 1
+    if test -d $projects[1]
+        echo "Opening first project"
+        open $projects[1] $argv
         return
     end
+
+    error "No Xcode projects found in the current directory."
+    return 1
 end
