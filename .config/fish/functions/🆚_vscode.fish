cd0a30f8e8337c6aa7be117c1ea855170b8ef28f
Ben Chatelain <ben@octop.ad>
2022-01-30 16:46:01 -0700

    ➕🆚 Add extensions used on tredecim

--- .config/fish/functions/🆚_vscode.fish
+++ .config/fish/functions/🆚_vscode.fish
@@ -13,80 +13,139 @@ function 🆚_vscode \
     set -l extensions \
         ahmadawais.shades-of-purple \
         Alan.stylus \
-        DSnake.java-debug \
-        ahmadawais.shades-of-purple \
         Apptorium.teacode-vsc-helper \
+        bencoleman.armview \
         bierner.markdown-preview-github-styles \
+        bungcip.better-toml \
         castwide.solargraph \
         christian-kohler.npm-intellisense \
+        cmstead.js-codeformer \
         cmstead.jsrefactor \
         codezombiech.gitignore \
         DavidAnson.vscode-markdownlint \
         dbaeumer.jshint \
         dbaeumer.vscode-eslint \
+        deerawan.vscode-dash \
         dkundel.vscode-npm-source \
+        dnicolson.binary-plist \
         donjayamanne.githistory \
+        DSnake.java-debug \
         eamodio.gitlens \
         EditorConfig.EditorConfig \
         eg2.tslint \
         eg2.vscode-npm-script \
+        Equinusocio.vsc-community-material-theme \
         Equinusocio.vsc-material-theme \
+        equinusocio.vsc-material-theme-icons \
+        esafirm.kotlin-formatter \
+        esbenp.prettier-vscode \
         fallenwood.vimL \
         felipecaputo.git-project-manager \
         felixrieseberg.vsc-travis-ci-status \
         fisheva.eva-theme \
         fivepointseven.node-version \
+        flesler.url-encode \
         fmoronzirfas.open-in-marked \
         formulahendry.auto-close-tag \
         formulahendry.auto-rename-tag \
         formulahendry.code-runner \
         formulahendry.terminal \
         georgewfraser.vscode-javac \
+        fwcd.kotlin \
+        golang.go \
         HookyQR.beautify \
         humao.rest-client \
+        jakearl.search-editor-apply-changes \
         jamesmaj.easy-icons \
         joelday.docthis \
+        joshuapoehls.json-escaper \
+        jtavin.ldif \
+        JuanBlanco.solidity \
+        jvitorfrancisco.theme-acid-purple \
         Kasik96.swift \
         kevinkyang.auto-comment-blocks \
         kevinmcgowan.TypeScriptImport \
         kisstkondoros.vscode-codemetrics \
         kumar-harsh.graphql-for-vscode \
+        kozet.purple-night \
         leizongmin.node-module-intellisense \
         lunaryorn.fish-ide \
         mathiasfrohlich.Kotlin \
         miramac.vscode-exec-node \
+        matklad.rust-analyzer \
+        mechatroner.rainbow-csv \
+        mhcpnl.xcodestrings \
+        mikestead.dotenv \
         mitaki28.vscode-clang \
         mkaufman.HTMLHint \
         mohsen1.prettify-json \
+        mongodb.mongodb-vscode \
         monokai.theme-monokai-pro-vscode \
+        ms-azure-devops.azure-pipelines \
+        ms-azuretools.vscode-apimanagement \
+        ms-azuretools.vscode-azureappservice \
         ms-azuretools.vscode-azurefunctions \
+        ms-azuretools.vscode-azureresourcegroups \
+        ms-azuretools.vscode-azurestorage \
+        ms-azuretools.vscode-azurevirtualmachines \
+        ms-azuretools.vscode-bicep \
+        ms-azuretools.vscode-cosmosdb \
         ms-azuretools.vscode-docker \
+        ms-dotnettools.csharp \
+        ms-dotnettools.vscode-dotnet-runtime \
+        ms-kubernetes-tools.vscode-aks-tools \
+        ms-kubernetes-tools.vscode-kubernetes-tools \
         ms-python.python \
+        ms-python.vscode-pylance \
+        ms-toolsai.jupyter \
+        ms-toolsai.jupyter-keymap \
+        ms-toolsai.jupyter-renderers \
+        ms-toolsai.vscode-ai \
+        ms-toolsai.vscode-ai-remote \
+        ms-vscode-remote.remote-containers \
         ms-vscode.azure-account \
+        ms-vscode.azurecli \
         ms-vscode.cpptools \
         ms-vscode.js-atom-grammar \
         ms-vscode.typescript-javascript-grammar \
+        ms-vscode.vscode-node-azure-pack \
         ms-vsliveshare.vsliveshare \
         ms-vsliveshare.vsliveshare-audio \
+        msazurermtools.azurerm-vscode-tools \
         msjsdiag.debugger-for-chrome \
         naumovs.node-modules-resolve \
         nepaul.editorconfiggenerator \
+        nhcarrigan.vscode-purple \
         nodesource.vscode-for-node-js-development-pack \
+        Perkovec.emoji \
+        phplasma.csv-to-table \
         PKief.material-icon-theme \
         pkosta2005.heroku-command \
         prashaantt.node-tdd \
         rbbit.typescript-hero \
+        redhat.fabric8-analytics \
         redhat.java \
+        redhat.vscode-xml \
+        redhat.vscode-yaml \
+        ritwickdey.LiveServer \
+        rust-lang.rust \
         secanis.jenkinsfile-support \
         Shan.code-settings-sync \
         shanoor.vscode-nginx \
         sidneys1.gitconfig \
         skyapps.fish-vscode \
+        sohibe.java-generate-setters-getters \
         stevencl.addDocComments \
+        stkb.rewrap \
         sysoev.language-stylus \
         sysoev.vscode-open-in-github \
+        TabNine.tabnine-vscode \
+        techer.open-in-browser \
         timonwong.shellcheck \
+        Trunk.io \
+        tushortz.java-imports-snippets \
         twxs.cmake \
+        vadimcn.vscode-lldb \
         VisualStudioExptTeam.vscodeintellicode \
         vscjava.vscode-java-debug \
         vscjava.vscode-java-dependency \
