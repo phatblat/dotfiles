67cfe4e585d9a10b3d63bfe7f35ba8fcaedfa636
Ben Chatelain <ben@octop.ad>
2022-02-02 15:16:13 -0700

    ✨ Add unmount function

---
+++ .config/fish/functions/unmount.fish
@@ -0,0 +1,14 @@
+function unmount \
+    --description='Unmounts a drive' \
+    --argument-names drive_path
+
+    if test -z $drive_path
+        error 'Usage: unmount drive_path'
+        return 1
+    else if test ! -d $drive_path
+        error "Not found: $drive_path"
+        return 2
+    end
+
+    hdiutil unmount $drive_path
+end
