b3a20e5402117ff7938c14a97111250e90bbd23e
Ben Chatelain <ben@octop.ad>
2022-02-12 18:02:22 -0700

    🔥 Remove dup copy of omf sdk function

--- .config/fish/functions/sdk.fish
+++
@@ -1,8 +0,0 @@
-# Copied from omf. Using from there gets sourced before config/variables, yielding error:
-# > bash: /bin/sdkman-init.sh: No such file or directory
-# Defined in /Users/phatblat/.local/share/omf/pkg/sdk/functions/sdk.fish @ line 1
-function sdk \
-        --wraps=sdk \
-        --description 'Software Development Kit Manager'
-    bash -c "source $sdkman_prefix/bin/sdkman-init.sh && sdk $argv"
-end
