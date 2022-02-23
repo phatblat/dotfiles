1273f2cd9733020d0333e80a91e954e3c8874e93
Ben Chatelain <ben@octop.ad>
2022-02-22 23:41:15 -0700

    ✨ Add tmdelete & tmsnapshots

---
+++ .config/fish/functions/tmdelete.fish
@@ -0,0 +1,12 @@
+# https://appleinsider.com/articles/21/06/26/how-to-delete-time-machine-local-snapshots-in-macos#how-to-delete-all-time-machine-local-snapshots
+function tmdelete \
+    --description='Delete Time Machine stapshot' \
+    --argument-names date_time
+
+    if test -z $date_time
+        error 'Usage: tmdelete date_time'
+        return 1
+    end
+
+    sudo tmutil deletelocalsnapshots $date_time
+end
---
+++ .config/fish/functions/tmsnapshots.fish
@@ -0,0 +1,5 @@
+function tmsnapshots \
+    --description='List Time Machine Snapshots'
+
+    tmutil listlocalsnapshots /
+end
