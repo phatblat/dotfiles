93b65df707064ad4515ef9639d3451609f1b479c
Ben Chatelain <ben@octop.ad>
2022-03-05 00:48:50 -0700

    ✨ Add findup function

---
+++ .config/fish/functions/findup.fish
@@ -0,0 +1,21 @@
+function findup \
+    --description='Recursively searches up the directory tree looking for files.' \
+    --argument-names pattern
+
+    if test -z $pattern
+        error "Usage: findup pattern"
+        return 1
+    end
+
+    # Start in the current directory
+    set -l path (realpath .)
+
+    # Stop at the root directory
+    while not string match --quiet / $path
+        # Print full path if file found
+        stat -qf '%N' $path/$pattern
+
+        # Recurse using parent directory
+        set path (realpath (dirname $path))
+    end
+end
