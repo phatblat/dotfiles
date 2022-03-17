3522e42f2742e364c763a94f674b80245a725264
Ben Chatelain <ben@octop.ad>
2022-03-17 10:46:08 -0600

    🖥 Replace fzf_key_bindings symlink with wrapper function

--- .config/fish/functions/fzf_key_bindings.fish
+++ .config/fish/functions/fzf_key_bindings.fish
@@ -1,1 +1,4 @@
-/opt/homebrew/opt/fzf/shell/key-bindings.fish
+function fzf_key_bindings \
+    --description="Wrapper for fzf key binding script that accounts for mac differences in homebrew root."
+    eval (brew_home fzf)/shell/key-bindings.fish
+end
