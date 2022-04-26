f565a55a8c09c559f6773610415b7cee2ca170ff
Ben Chatelain <ben@octop.ad>
2022-04-26 12:59:55 -0600

    ✨ Add xamarin_version function

---
+++ .config/fish/functions/xamarin_version.fish
@@ -0,0 +1,8 @@
+function xamarin_version \
+    --description='Determines the current version Xamarin'
+
+    set --local xamarin_ios /Library//Frameworks/Xamarin.iOS.framework/Versions
+
+    echo -n "Xamarin.iOS: "
+    command ls -1 $xamarin_ios | head -1
+end
