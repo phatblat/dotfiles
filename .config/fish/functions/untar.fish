82920b2ae98c29b1490bd3caafe01705fbd1fa22
Ben Chatelain <ben@octop.ad>
2022-05-06 10:41:08 -0600

    ✨ Create expansion dir in untar function

--- .config/fish/functions/untar.fish
+++ .config/fish/functions/untar.fish
@@ -1,13 +1,25 @@
 function untar --wraps='tar' \
         --description='Extracts a tarball.' \
-        --argument file
+        --argument file_name
 
-    if test -z $file 2>/dev/null
-        echo "Usage: untar file.tar.gz"
+    if test -z $file_name 2>/dev/null
+        error "Usage: untar file.tar.gz"
         return 1
+    else if not test -f $file_name
+        error "File not found: $file_name"
+        return 2
     end
 
-    # TODO: iterate files when no args, prompting before extract
+    set --local dir_name (file_base $file_name)
+    if test -d $dir_name
+        error "Extraction directory already exists: $dir_name"
+        return 3
+    end
 
-    tar -zxvf $file
+    command mkdir $dir_name
+
+    # -xzvf
+    tar --extract --gunzip --verbose \
+        --file $file_name \
+        --directory $dir_name
 end
