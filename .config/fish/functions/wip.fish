b6584acb65576bccd278057437815c8201d7c39b
Ben Chatelain <ben@octop.ad>
2022-05-06 09:49:11 -0600

    ✨ Add wip function

---
+++ .config/fish/functions/wip.fish
@@ -0,0 +1,11 @@
+function wip \
+    --description='Commits WIP work'
+
+    if git_repo_clean
+        error 'There is nothing to commit. The working copy is clean.'
+        return 1
+    end
+
+    git add --all
+    git commit -am "🚧 WIP"
+end
