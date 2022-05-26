41b01d240710683a4ab3440b43ebdb00d4a80efd
Ben Chatelain <ben@octop.ad>
2022-05-25 19:17:27 -0600

    🔥 Remove lock file in gradle_kill

--- .config/fish/functions/gradle_kill.fish
+++ .config/fish/functions/gradle_kill.fish
@@ -5,4 +5,9 @@ function gradle_kill \
     psgrep gradle | cut -w -f 2 | while read pid
         kill -9 $pid
     end
+
+    set -l lockfile ~/.gradle/caches/journal-1/journal-1.lock
+    if test -f lockfile; then
+        rm -v $lockfile
+    end
 end
