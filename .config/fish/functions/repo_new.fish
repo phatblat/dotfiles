e8487cf95d37bffd57aab5236824f19813914089
Ben Chatelain <ben@octop.ad>
2022-07-11 09:58:43 -0600

    ✨ Add repo_new function

---
+++ .config/fish/functions/repo_new.fish
@@ -0,0 +1,13 @@
+function repo_new \
+    --description='Creates a new GitHub repo using the local dir as the root' \
+    --argument-names repo_name
+
+    if test -z $repo_name
+        echo 'Usage: repo_new [repo_name]'
+        return 1
+    end
+
+    gh repo create $repo_name --public --source=.
+    pborigin
+    pp
+end
