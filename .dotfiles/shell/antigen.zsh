9013081c299fb2452afe1609a6a5b1b524a29151
Ben Chatelain <ben@octop.ad>
2022-07-22 15:03:33 -0600

    💀 Disable antigen

--- .dotfiles/shell/antigen.zsh
+++ .dotfiles/shell/antigen.zsh
@@ -19,16 +19,16 @@
 # mkdir -p ${_ANTIGEN_CACHE_PATH}/highlighters
 
 # Start up Antigen
-source "$(brew --prefix)/share/antigen/antigen.zsh"
-antigen use oh-my-zsh
+#source "$(brew --prefix)/share/antigen/antigen.zsh"
+#antigen use oh-my-zsh
 
 # Override oh-my-zsh aliases
-unalias d  2>/dev/null && alias d='git diff'
-unalias po 2>/dev/null && alias po='pod outdated --no-repo-update'
+#unalias d  2>/dev/null && alias d='git diff'
+#unalias po 2>/dev/null && alias po='pod outdated --no-repo-update'
 
 # Antigen Bundles
 # antigen bundle common-aliases
-antigen bundle phatblat/powerline-shell --branch=custom
+#antigen bundle phatblat/powerline-shell --branch=custom
 # antigen bundle robbyrussell/oh-my-zsh plugins/ruby
 
 # Antigen Themes
@@ -36,6 +36,6 @@ antigen bundle phatblat/powerline-shell --branch=custom
 
 # Add zsh-syntax-highlighting as last bundle fixes "zsh-syntax-highlighting: failed loading highlighters, exiting."
 # https://github.com/zsh-users/zsh-syntax-highlighting/blob/0a9b347483ae653e95ed7ccb147a0db3644b6384/INSTALL.md#antigen
-antigen bundle zsh-users/zsh-syntax-highlighting
+#antigen bundle zsh-users/zsh-syntax-highlighting
 
-antigen apply
+#antigen apply
