79fd3e72f55d7a89eefff53a5ce294fefb39a43d
Ben Chatelain <phatblat@users.noreply.github.com>
2022-09-09 15:47:27 -0600

    Switch SSH key generation to use ed25519

--- .config/fish/functions/sshnewkey.fish
+++ .config/fish/functions/sshnewkey.fish
@@ -1,14 +1,15 @@
-# Generates a new private RSA SSH key at ~/.ssh/id_rsa with an optional comment.
-# Comment defaults to USER@hostname.
-function sshnewkey --argument-names comment
+function sshnewkey \
+    --description 'Generates a new private SSH key at ~/.ssh/id_ed25519 with an optional comment. Comment defaults to USER@hostname'
+    --argument-names comment
+
     if test -z $comment
         set comment "$USER@"(hostname)
     end
 
-    set -l keyfile ~/.ssh/id_rsa
+    set -l keyfile ~/.ssh/id_ed25519
 
     ssh-keygen \
-        -t rsa \
+        -t ed25519 \
         -C $comment \
         -f $keyfile
 end
