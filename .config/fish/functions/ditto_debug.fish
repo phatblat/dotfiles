0af5a7d44ce78e64f50287d5ed932e1d5c0c3beb
Ben Chatelain <ben@octop.ad>
2022-10-07 11:50:09 -0600

    ✨ Add ditto_debug function

---
+++ .config/fish/functions/ditto_debug.fish
@@ -0,0 +1,22 @@
+function ditto_debug \
+    --description='Toggles the DITTO_DEBUG flag.' \
+    --argument-names state
+
+    if test -z $state
+        echo 'Usage: ditto_debug [on/off]'
+        return 1
+    end
+
+
+    switch $state
+        case on ON On
+            set --global --export DITTO_DEBUG 1
+            echo " (DITTO_DEBUG=1)"
+        case off OFF Off
+            set --erase DITTO_DEBUG
+            echo " (DITTO_DEBUG removed)"
+        case '*'
+            echo "Usage: ditto_debug [on|off]"
+            return 1
+    end
+end
--- .config/fish/functions/todo.fish
+++ .config/fish/functions/todo.fish
@@ -1,4 +1,5 @@
-# Edit the rebase todo file (~/.git/rebase-merge/git-rebase-todo)
-function todo
+function todo \
+    --description="Edit the rebase todo file (~/.git/rebase-merge/git-rebase-todo)"
+
     git rebase --edit-todo
 end
