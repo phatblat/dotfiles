25b0423402651b5dbc82f9ad46206145c9e06508
Ben Chatelain <ben@octop.ad>
2022-11-15 16:35:24 -0700

    ✨ Add cherry-pick and am to skip function

--- .config/fish/functions/skip.fish
+++ .config/fish/functions/skip.fish
@@ -1,4 +1,7 @@
-# Skip the current commit in a git rebase.
-function skip
-    git rebase --skip $argv
+function skip \
+    --description='Skip the current commit in a git rebase/cherry-pick/am.'
+
+    git rebase --skip 2>/dev/null
+    or git cherry-pick --skip 2>/dev/null
+    or git am --skip 2>/dev/null
 end
