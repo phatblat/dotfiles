8914c2527339b7068e798ee21408e4040d034f45
Ben Chatelain <ben@octop.ad>
2022-12-12 12:33:02 -0700

    👔 Change devices function to use xctrace

--- .config/fish/functions/devices.fish
+++ .config/fish/functions/devices.fish
@@ -1,5 +1,4 @@
-# List devices (simulators and connected hardware) that Xcode knows about.
 function devices \
     --description='List simulators and connected devices.'
-    xcrun simctl list devices $argv
+    xcrun xctrace list devices $argv
 end
