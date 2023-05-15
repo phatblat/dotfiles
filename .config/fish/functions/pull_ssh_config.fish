274b36a4cc4a829e556caefa681ba9b4ed92e68a
FirstName LastName <you@example.com>
2023-05-15 17:22:56 -0600

    ✨ Add pull_ssh_config function

---
+++ .config/fish/functions/pull_ssh_config.fish
@@ -0,0 +1,10 @@
+function pull_ssh_config \
+    --description='Copies SSH config to local'
+
+    if test phatmini = (hostname)
+        echo 'pull_ssh_config should be run on a remote host'
+        return 1
+    end
+
+    scp phatmini.c4:.ssh/config ~/.ssh/config
+end
