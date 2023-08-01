812536d4289e39f81e6158dd89069bd6bd0bf6eb
Ben Chatelain <ben@octop.ad>
2023-08-01 17:45:00 -0600

    ✨ Add gem_pristine function

---
+++ .config/fish/functions/gem_pristine.fish
@@ -0,0 +1,10 @@
+function gem_pristine \
+    --description='Runs the pristine command for all gems'
+
+    # Fix gem write permissions
+    # ERROR:  While executing gem ... (Errno::EACCES)
+    #     Permission denied @ rb_sysopen - /opt/homebrew/Cellar/ruby/3.2.2_1/lib/ruby/gems/3.2.0/specifications/matrix-0.4.2.gemspec
+    chmod u+w /opt/homebrew/Cellar/ruby/3.2.2_1/lib/ruby/gems/3.2.0/specifications/*
+
+    gem pristine --all
+end
