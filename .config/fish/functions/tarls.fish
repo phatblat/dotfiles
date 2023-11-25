74205c41287a626381e730f52108cbe09e027a95
FirstName LastName <you@example.com>
2023-11-25 16:05:27 -0700

    ✨ Add tarls function

---
+++ .config/fish/functions/tarls.fish
@@ -0,0 +1,11 @@
+function tarls \
+    --description='List contents of a tarball' \
+    --argument-names file_name
+
+    if test -z $file_name
+        echo 'Usage: tarls [file_name]'
+        return 1
+    end
+
+    tar -ztvf $file_name
+end
