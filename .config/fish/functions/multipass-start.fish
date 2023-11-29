61a4c2f18f1adbccc6035519859429ba7f03f84f
Ben Chatelain <ben@octop.ad>
2023-11-29 14:39:27 -0700

    ✨ Add multipass_start function

---
+++ .config/fish/functions/multipass-start.fish
@@ -0,0 +1,6 @@
+function multipass-start \
+    --description='Starts the multipass service and GUI app'
+
+    sudo launchctl load -w /Library/LaunchDaemons/com.canonical.multipassd.plist
+    open -a Multipass
+end
