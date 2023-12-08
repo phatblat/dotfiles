a7c47b53109055f6a4884fcb45030926051179c6
FirstName LastName <you@example.com>
2023-12-08 14:11:27 -0700

    ❄️ Set hostname to DTO-A017

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -40,12 +40,12 @@
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+    # $ darwin-rebuild build --flake .#DTO-A017
+    darwinConfigurations."DTO-A017" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."simple".pkgs;
+    darwinPackages = self.darwinConfigurations."DTO-A017".pkgs;
   };
 }
