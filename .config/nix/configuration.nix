72863f0f60c9eb0e8efb4300f146d43402590075
FirstName LastName <you@example.com>
2023-12-09 18:43:52 -0700

    ❄️ Set experimental-features

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -20,10 +20,12 @@
   nix.configureBuildUsers = true;
 
   # Enable experimental nix command and flakes
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
   # nix.package = pkgs.nixUnstable;
   nix.extraOptions = ''
     auto-optimise-store = true
-    experimental-features = nix-command flakes
   '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
     extra-platforms = x86_64-darwin aarch64-darwin
   '';
--- .config/nix/darwin.nix
+++ .config/nix/darwin.nix
@@ -13,8 +13,6 @@
   services.karabiner-elements.enable = true;
   # nix.package = pkgs.nix;
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
