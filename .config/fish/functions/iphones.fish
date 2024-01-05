23cd6ff7e601019d6841876a9f8df5e64c717a11
Ben Chatelain <ben@octop.ad>
2024-01-04 20:27:05 -0700

    ✨ Add iphones function

---
+++ .config/fish/functions/iphones.fish
@@ -0,0 +1,7 @@
+function iphones \
+    --description='Show connected iOS devices'
+
+
+    system_profiler SPUSBDataType | \
+      sed -n -E -e '/(iPhone|iPad)/,/Serial/s/ *Serial Number: *(.+)/\1/p'
+end
