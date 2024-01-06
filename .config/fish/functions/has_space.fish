c49965062cff5b9dd0db8140abeee04d9fbbe692
Ben Chatelain <ben@octop.ad>
2024-01-06 11:07:01 -0700

    ✨ Aedd has_space function

---
+++ .config/fish/functions/has_space.fish
@@ -0,0 +1,8 @@
+function has_space \
+    --description='Tests whether string continas a space' \
+    --argument-names string
+
+    if string match --entire --invert ' ' "$string"
+        return 1
+    end
+end
