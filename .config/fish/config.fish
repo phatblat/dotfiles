d2033af662642fcc47772657ee9433b962188cb5
Ben Chatelain <ben@octop.ad>
2024-01-06 10:33:29 -0700

    ☕️ Install openjdk-17 via mise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -33,8 +33,8 @@ if type --query direnv
 end
 
 if type --query mise
-    eval "$(mise activate fish)"
-    eval "$(mise hook-env --shell=fish)"
+    eval (mise activate fish)
+    eval (mise hook-env --shell=fish)
 end
 
 # GUI and items requiring a user
