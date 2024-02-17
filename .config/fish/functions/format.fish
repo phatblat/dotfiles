ac3550308c98d3cab83aa58ad4a2fd1d7c088383
Ben Chatelain <ben@octop.ad>
2024-02-17 10:11:47 -0700

    ✨ Add format function

---
+++ .config/fish/functions/format.fish
@@ -0,0 +1,19 @@
+function format \
+    --description='Format files' \
+    --argument-names type
+
+    if test -z $type
+        echo 'Usage: format [type]'
+        return 1
+    end
+
+
+    switch $type
+        case fish
+            # Format fish files
+            fish_format .config/fish/functions/*.fish
+        case '*'
+            echo "Unsupported file type: $type"
+            return 1
+    end
+end
