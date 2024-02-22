a8d52f00ec00466c5005c3c6465747fe23b45241
Ben Chatelain <ben@octop.ad>
2024-02-21 21:02:34 -0700

    ✨ Add aws_id function

---
+++ .config/fish/functions/aws_id.fish
@@ -0,0 +1,11 @@
+function aws_id \
+    --description='aws_id' \
+    --argument-names profile
+
+    if test -z $profile
+        set profile "--profile $profile"
+    end
+
+    aws sts get-caller-identity \
+        --no-cli-pager
+end
