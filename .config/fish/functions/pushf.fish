23a408c5a5376b6659c2dd8bb19af8c0810c6ef5
Ben Chatelain <ben@octop.ad>
2024-02-26 14:37:10 -0700

    👔 Change pushf function to use --force-with-lease

--- .config/fish/functions/pushf.fish
+++ .config/fish/functions/pushf.fish
@@ -1,3 +1,4 @@
 function pushf --description 'Force a git push.'
-    push --force $argv
+    # "force-with-lease": You assume you took the lease on the ref when you fetched to decide what the rebased history should be, and you can push back only if the lease has not been broken.
+    push --force-with-lease $argv
 end
