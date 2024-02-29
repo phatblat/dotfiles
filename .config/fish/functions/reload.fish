c99241c4119b9b2f7294fd605326517d916e180d
Ben Chatelain <ben@octop.ad>
2024-02-29 12:37:02 -0700

    🔈 Log message when reloading shell or function

--- .config/fish/functions/reload.fish
+++ .config/fish/functions/reload.fish
@@ -6,13 +6,16 @@ function reload \
     set -l file ~/.config/fish/functions/$function_name.fish
 
     if test -z $function_name
+        echo Reloading fish shell
         source ~/.config/fish/config.fish
         return
     else if test -e $file
+        echo Reloading fish function $function_name
         source $file
         if test $status -ne 0
             return $status
         end
+
         # TODO: Show diff of function (memory vs file system)
         # func $function_name
     else
