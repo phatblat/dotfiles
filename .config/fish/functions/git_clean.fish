7406e7889adb5b36fe2f1f109c31116245817109
Ben Chatelain <ben@octop.ad>
2024-03-08 09:30:58 -0700

    🥅 Return error instead of cleaning HOME dir

--- .config/fish/functions/git_clean.fish
+++ .config/fish/functions/git_clean.fish
@@ -1,4 +1,8 @@
 function git_clean \
     --description 'Clean non-tracked files from the working tree'
+    if test $PWD = $HOME
+        error Refusing to clean user home
+        return 1
+    end
     git clean -xffd
 end
