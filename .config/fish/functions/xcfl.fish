11aad6f379992cbdf6c4d9732de7c94b1dae63a4
Ben Chatelain <ben@octop.ad>
2024-03-09 07:42:19 -0700

    ✨ Add xcfl function

---
+++ .config/fish/functions/xcfl.fish
@@ -0,0 +1,13 @@
+function xcfl \
+    --description "Runs Xcode first launch if necessary or forced" \
+    --argument-names force
+
+    if begin
+            ! xcodebuild -checkFirstLaunchStatus
+            or test "$force" = --force
+        end
+        echo Running Xcode first launch
+        # install packages and agree to the licens -runFirstLaunch install packages and agree to the licens
+        xcodebuild -runFirstLaunch -verbose
+    end
+end
