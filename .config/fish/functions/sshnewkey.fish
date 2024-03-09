6b4e00359f50327abdb971d57672d0f493fdeedd
Ben Chatelain <ben@octop.ad>
2024-03-09 16:45:16 -0700

    🩹 Fix syntax error in sshnewkey function

--- .config/fish/functions/sshnewkey.fish
+++ .config/fish/functions/sshnewkey.fish
@@ -1,5 +1,5 @@
 function sshnewkey \
-    --description 'Generates a new private SSH key at ~/.ssh/id_ed25519 with an optional comment. Comment defaults to USER@hostname'
+    --description 'Generates a new private SSH key at ~/.ssh/id_ed25519 with an optional comment. Comment defaults to USER@hostname' \
     --argument-names comment
 
     if test -z $comment
