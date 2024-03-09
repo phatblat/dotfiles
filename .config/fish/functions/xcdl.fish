2252e6c27bbb4c4bb72a6241b7687d8552fc44ee
Ben Chatelain <ben@octop.ad>
2024-03-09 08:11:59 -0700

    ✨ Add xcdl function

---
+++ .config/fish/functions/xcdl.fish
@@ -0,0 +1,25 @@
+function xcdl \
+    --description "Downloads platform tools for Xcode" \
+    --argument-names platform
+
+    echo Xcode (xcv)
+
+    # https://developer.apple.com/documentation/xcode/installing-additional-simulator-runtimes#Install-and-manage-Simulator-runtimes-from-the-command-line
+
+    switch $platform
+        case all
+            # -downloadAllPlatforms download all platforms
+            xcodebuild -downloadAllPlatforms
+        case ios iOS
+            # -downloadPlatform NAME download the platform NAME
+            xcodebuild -downloadPlatform iOS
+        case tvos tvOS
+            xcodebuild -downloadPlatform tvOS
+        case visionos visionOS
+            xcodebuild -downloadPlatform visionOS
+        case watchos watchOS
+            xcodebuild -downloadPlatform watchOS
+        case '*' list
+            echo available platforms: ios tvos visionos watchos
+    end
+end
