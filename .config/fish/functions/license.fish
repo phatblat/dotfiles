b4bdcf1665ac926d464fce36067b51d45be7d768
Ben Chatelain <ben@octop.ad>
2024-04-07 09:32:55 -0600

    📄 Switch license function to use ISC license
    Removed extension from filename

--- .config/fish/functions/license.fish
+++ .config/fish/functions/license.fish
@@ -1,38 +1,26 @@
 function license \
-    --description 'Writes out LICENSE.md file, adds link to readme and commits changes.'
+    --description 'Writes out LICENSE file, adds link to readme and commits changes.'
 
     set -l year (date "+%Y")
-    echo -n "The MIT License (MIT)
+
+    # Write LICENSE file
+    echo -n "ISC License
 
 Copyright ©️ $year Ben Chatelain
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-" >LICENSE.md
+THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+" >LICENSE
 
     # Append license info to README.md
     echo -n "
 ## 📄 License
 
-This repo is licensed under the MIT License. See the [LICENSE](LICENSE.md) file for rights and limitations.
+This repo is licensed under the ISC License. See the [LICENSE](LICENSE) file for rights and limitations.
 " >>README.md
 
     # Commit the changes
-    git add LICENSE.md README.md
+    git add LICENSE README.md
     git commit -m '📄 Add license to README.md'
 end
