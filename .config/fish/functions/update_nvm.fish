55fea1c227d8b1ca525127ad34da96d9ebdcf922
Ben Chatelain <ben@octop.ad>
2024-09-03 13:56:16 -0600

    ✨ Dynamically fetch the latest NVM version

--- .config/fish/functions/update_nvm.fish
+++ .config/fish/functions/update_nvm.fish
@@ -3,8 +3,11 @@ function update_nvm \
     --argument-names nvm_version
 
     if test -z $nvm_version
-        set nvm_version 0.40.1
+        # Fetch the latest version from GitHub
+        set nvm_version (curl --silent https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
     end
 
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v$nvm_version/install.sh | bash
+    echo "Upstalling NVM version $nvm_version"
+
+    curl --silent https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh | bash
 end
