9299f0d121d4be02d5093b7f11462a91b1631eb7
Ben Chatelain <ben@octop.ad>
2024-10-22 17:18:09 -0600

    👔 Delete more of the gradle cache

--- .config/fish/functions/gradle_cache_clean.fish
+++ .config/fish/functions/gradle_cache_clean.fish
@@ -1,5 +1,29 @@
 function gradle_cache_clean \
     --description 'Cleans the gradle cache'
 
-    trash ~/.gradle/caches
+    # not init.d/ wrappers/ or gradle.properties
+    set --local folders \
+                    7.5.1 \
+                    android \
+                    build-scan-data \
+                    buildOutputCleanup \
+                    caches \
+                    daemon \
+                    jdks \
+                    kotlin-profile \
+                    native \
+                    notifications \
+                    undefined-build \
+                    vcs-1 \
+                    workers \
+                    android.lock \
+                    file-system.probe
+
+    for folder in $folders
+        echo $folder
+        if test -z "$folder"
+            error "$folder is empty"
+        end
+        rm -rf ~/.gradle/$folder
+    end
 end
