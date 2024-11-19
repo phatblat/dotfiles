896bb062f54d336185b6bf76e9a91683a306e935
Ben Chatelain <ben@octop.ad>
2024-11-19 11:54:32 -0700

    🐟 Fish shell config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,74 +3,64 @@
 # Dotfiles
 #
 
+# Warpify subshells
+# https://docs.warp.dev/features/subshells
+# printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "fish" }}\x9c'
+
 # resource limits
 ulimit --file-descriptor-count 4096
 
-# shell state debug logging
-if status is-login
-    echo is-login
-end
+# Fish logo
 if status is-interactive
-    echo is-interactive
-end
-
-# Disable the default welcome greeting
-set --global --export fish_greeting
-
-if is_mac
-    # A lot of things check for XDG_CONFIG_HOME which isn't defined by default
-    # on macOS. This may speed up scripts slightly.
-    set --export --global XDG_CONFIG_HOME "$HOME/.config"
-
-    if is_arm
-        set --export --global BREW_HOME /opt/homebrew
-    else
-        set --export --global BREW_HOME /usr/local
+    if type -q neofetch
+        neofetch --config ~/.config/neofetch/config.conf
+    else if type -q fish_logo
+        fish_logo brblue cyan green
     end
-else if is_linux
-    set --export --global BREW_HOME /home/linuxbrew/.linuxbrew
 end
 
-set --export --global --prepend --path PATH \
-    /opt/homebrew/bin
+# Variables
+source ~/.config/fish/variables.fish
+
+if type -q direnv
+    # Directory-based variables
+    eval (direnv hook fish)
+end
 
 # GUI and items requiring a user
 if status is-interactive
-    warpify
+    # Set up RAM disk for Xcode DerivedData
+    if is_phatmini
+        # derived_data quiet
+    end
 
-    if type --query fastfetch
-        fastfetch --config ~/.config/fastfetch/config.jsonc
-    else if type --query fish_logo
-        fish_logo
+    # Set up RAM disk for Cargo
+    if is_phatmini
+        # cargo_target quiet
     end
 
     if command --query starship
         starship init fish | source
     end
-
     if command --query zoxide
         zoxide init fish | source
     end
 
-    if type --query thefuck
+    # Use custom autoloaded functions
+    # reload fish_mode_prompt
+    # reload fish_right_prompt
+
+    # Event Hooks
+    # reload fish_postexec
+
+    if type -q thefuck
         # The Fuck
         eval (thefuck --alias | tr \n ';')
     end
+
+    # 1Password CLI plugin
+    source ~/.config/op/plugins.sh
 end
 
-
-# Environment Variables - set once on login
-# or set if bash was the login shell and fish was opened as a subshell.
-if status is-login; or is_bash_login
-    source ~/.config/fish/variables.fish
-
-    if type --query direnv
-        # Directory-based variables
-        eval (direnv hook fish)
-    end
-
-    if type --query mise
-        eval (mise activate fish)
-        #eval (mise hook-env --shell=fish)
-    end
-end
+# Created by `pipx` on 2023-08-29 02:06:37
+set PATH $PATH /Users/phatblat/.local/bin
