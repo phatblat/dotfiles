d465ca4e2789e5d1d85f87b9637bb201e09737be
Ben Chatelain <ben@octop.ad>
2024-11-19 11:51:53 -0700

    🐠 OMF plugins

--- .config/fish/functions/🐠_omf.fish
+++ .config/fish/functions/🐠_omf.fish
@@ -9,39 +9,21 @@ function 🐠_omf \
     echo "🐠 oh-my-fish - https://github.com/oh-my-fish/oh-my-fish"
     echo
 
-    set --local remote_install_script \
-        https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install
-    set --local local_install_script ~/tmp/omf_install
+    set -l repo_url git@github.com:oh-my-fish/oh-my-fish.git
+    set -l base_dir ~/dev/shell/fish
+    set -l local_dir $base_dir/oh-my-fish
 
-    # https://github.com/oh-my-fish/oh-my-fish#installation
-    curl $remote_install_script >$local_install_script
+    # Create parent directories
+    createdirs $base_dir
+
+    clone_or_pull $local_dir $repo_url
 
     # Install omf if necessary
     if not functions --query omf
-        # Usage: install [options]
-        #   Install Oh My Fish
-
-        # Options:
-        #   --channel= <channel >Download a specific release channel, either stable or dev (default is "stable").
-        #   --check Do a system readiness check without installing.
-        #   --config= <path >Put config in a specific path (default is /Users/phatblat/.config/omf).
-        #   --help, -h Show this help message.
-        #   --noninteractive Disable interactive questions (assume no, use with --yes to assume yes).
-        #   --offline[=<path>] Offline install, optionally specifying a tar or directory to use.
-        #   --path= <path >Use a specific install path (default is /Users/phatblat/.local/share/omf).
-        #   --uninstall Uninstall existing installation instead of installing.
-        #   --verbose Enable verbose debugging statements for the installer.
-        #   , -y Assume yes for interactive questions.
-        fish $local_install_script \
-            --config=~/.config/omf \
-            --path=~/.local/share/omf \
-            --verbose \
-            --yes
-    end
-
-    if not functions --query omf
-        error "omf not installed"
-        return 1
+        pushd $local_dir
+        bin/install --offline
+        # fish install --path=~/.local/share/omf --config=~/.config/omf
+        popd
     end
 
     # --------------------------------------------------------------------------
