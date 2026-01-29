c39ef5815ca9b5b5eadd6be929bff58f26f06129
Ben Chatelain <ben@octop.ad>
2026-01-29 08:34:54 -0700

    refactor(nix): pin nixpkgs to ee889ba
    last working revision for xcodes on aarch64-darwin (2025-12-31)

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -6,8 +6,10 @@
   };
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Pinned to last working revision for xcodes on aarch64-darwin (2025-12-31).
+    # Swift 5.10.1 build crashes during bootstrapping on nixos-unstable due to
+    # upstream compiler segfault. See: https://github.com/NixOS/nixpkgs/issues/353261
+    nixpkgs.url = "github:nixos/nixpkgs/ee889ba3c108edae7bb38a33a0fa33f318aa09e9";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
