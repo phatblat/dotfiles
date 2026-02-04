504dae613953b45895f9bb9a63445086a17701ef
Ben Chatelain <ben@octop.ad>
2026-02-04 12:39:54 -0700

    refactor(nix): switch back to nixos-unstable
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/ee889ba' (2025-12-30)
      → 'github:nixos/nixpkgs/e6eae2e' (2026-02-03)

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -6,10 +6,13 @@
   };
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     # Pinned to last working revision for xcodes on aarch64-darwin (2025-12-31).
     # Swift 5.10.1 build crashes during bootstrapping on nixos-unstable due to
     # upstream compiler segfault. See: https://github.com/NixOS/nixpkgs/issues/353261
-    nixpkgs.url = "github:nixos/nixpkgs/ee889ba3c108edae7bb38a33a0fa33f318aa09e9";
+    # nixpkgs.url = "github:nixos/nixpkgs/ee889ba3c108edae7bb38a33a0fa33f318aa09e9";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
