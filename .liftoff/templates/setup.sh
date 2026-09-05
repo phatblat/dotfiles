dac8013ffd5578c561b1ad3da285b4cda2f99ccc
Ben Chatelain <ben@octop.ad>
2026-09-05 12:32:29 -0400

    chore: remove dead liftoff config
    Liftoff (thoughtbot's iOS Xcode project generator) is unused: not
    installed, absent from Brewfile, and referenced by no script or shell
    function. Remove the last-touched-2016 template residue and drop the
    two orphaned thoughtbot/formulae entries from Brewfile.lock.json.
    
    Co-Authored-By: oh-my-pi <omp@can.ac>

--- .liftoff/templates/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-bundle install
-<% if enable_settings && dependency_manager_enabled?("cocoapods") %>
-pod install
-<% end %>
-<% if dependency_manager_enabled?("carthage") %>
-carthage update
-<% end %>
